23729235bbe5cf265b912caf9942cc3caddf1bbb
Alan Tammet <alan.tammet@solita.fi>
2019-07-31 10:31:34 +0000

    OS branching

--- app/project-registry/dev.sh
+++ app/project-registry/dev.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-echo "Starting PostgREST for project registry local dev db"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "Starting PostgREST for project registry local dev db"
+	docker run --rm -p 3000:3000 \
+	-e PGRST_DB_URI="postgres://teis@host.docker.internal:5432/teis" \
+	-e PGRST_DB_ANON_ROLE="teis" \
+	-e PGRST_DB_SCHEMA="projects" \
+	postgrest/postgrest
 
-docker run --rm -p 3000:3000 \
-  -e PGRST_DB_URI="postgres://teis@host.docker.internal:5432/teis" \
-  -e PGRST_DB_ANON_ROLE="teis" \
-  -e PGRST_DB_SCHEMA="projects" \
-  postgrest/postgrest
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+	echo "Starting PostgREST for project registry local dev db"
+	docker run -d --rm --net=host -p 3000:3000 \
+        -e PGRST_DB_URI="postgres://teis@127.0.0.1:5432/teis" \
+        -e PGRST_DB_ANON_ROLE="teis" \
+        -e PGRST_DB_SCHEMA="projects" \
+        postgrest/postgrest
+
+else
+        echo "OS not supported"
+fi
