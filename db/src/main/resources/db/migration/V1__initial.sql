5726e7000eff0e26001ff0cdc6016d9fbb5a285e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-01 10:22:38 +0300

    Add project geometry

--- db/src/main/resources/db/migration/V1__initial.sql
+++ db/src/main/resources/db/migration/V1__initial.sql
@@ -136,6 +136,21 @@ INSERT INTO projects.projectgroup (name, description, phase) VALUES
   ('Proof of concept planning projects','This is the 1st test project group for PoC planning projects', 1),
   ('Implementation projects','The 2nd test project group for implementation', 2);
 
-INSERT INTO projects.project (name,description,state,phase,projectgroup_id) VALUES
- ('Plan AWS setup project','Project for planning the AWS setup', 2, 1, 1),
- ('Implement PoC', 'Project for implementing the PoC', 3,2,2);
+INSERT INTO projects.project (name,description,state,phase,projectgroup_id,geometry) VALUES
+ ('Plan AWS setup project','Project for planning the AWS setup', 2, 1, 1, st_setsrid(st_geomfromgeojson('{"type": "LineString", "coordinates": [[23.73046875,58.78528524510292],[25.147705078125,59.67328836837126],[27.0263671875,58.842174856190105],[23.697509765625,58.722598828043374]]}'), 4326)),
+ ('Implement PoC', 'Project for implementing the PoC', 3,2,2, st_setsrid(st_geomfromgeojson('{"type": "LineString", "coordinates": [ [25.51025390625,59.48414789449185], [24.906005859375,59.37239142233717],[25.762939453125,59.338792483494494],[25.015869140625,59.265880628258095],[25.751953125,59.24341475839977],[25.059814453125,59.12522577945005],[25.718994140624996,59.09138238455909]]}'), 4326));
+
+
+CREATE FUNCTION projects.mvt_projectgroup_projects(id INT, xmin NUMERIC, ymin NUMERIC, xmax NUMERIC, ymax NUMERIC)
+RETURNS bytea
+AS $$
+SELECT ST_AsMVT(tile) AS mvt
+  FROM (SELECT CONCAT(p.name) AS tooltip,
+               p.id,
+               ST_AsMVTGeom(p.geometry,
+                            st_makebox2d(st_makepoint($2, ymin),
+                                         st_makepoint($4, ymax)),
+                            4096, NULL, false)
+          FROM projects.project p
+         WHERE p.projectgroup_id = id) AS tile;
+$$ LANGUAGE SQL STABLE;
