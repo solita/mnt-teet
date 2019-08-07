659df05a9e8c411de29dba86e8ad89445b9e33be
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-07 12:30:38 +0300

    Update build script

--- app/frontend/build.sh
+++ app/frontend/build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-clj -m figwheel.main -O advanced -bo prod
-
-
-
-cp $WD/resources/public/index.html index.html
-cp $WD/target/public/cljs-out/prod-main.js main.js
-cp $WD/resources/public/language/en.edn language/en.edn
-cp $WD/resources/public/language/et.edn language/et.edn
-cp $WD/resources/public/config.json.tpl config.json.tpl
+clojure -m figwheel.main -O advanced -bo prod
