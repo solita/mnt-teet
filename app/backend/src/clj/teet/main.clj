0e3d6c921b98afe498e48b52c8fd7a01eb70cc2e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-16 14:22:08 +0300

    Change port to 3000

--- app/backend/src/clj/teet/main.clj
+++ app/backend/src/clj/teet/main.clj
@@ -11,28 +11,29 @@
 (def server nil)
 
 (defn start []
-  (alter-var-root
-   #'server
-   (fn [_]
-     (log/info "Starting TEET service in port 4000")
-     (server/run-server
-      (params/wrap-params
-       (routes
-        (tara-routes/tara-routes (tara-endpoint/discover "https://tara-test.ria.ee/oidc")
-                                 {:client-id (System/getenv "TARA_CLIENT_ID")
-                                  :client-secret (System/getenv "TARA_CLIENT_SECRET")
-                                  :base-url (System/getenv "BASE_URL")
-                                  :on-error #(do
-                                               (log/error "TARA auth error" %)
-                                               {:status 500
-                                                :body "Error processing TARA auth"})
-                                  :on-success #(do
-                                                 (log/info "TARA auth success, token: " %)
-                                                 {:status 302
-                                                  :headers {"Location" (System/getenv "BASE_URL")}})})
-        (routes/teet-routes)))
-      {:ip "0.0.0.0"
-       :port 4000}))))
+  (let [port 3000]
+    (alter-var-root
+     #'server
+     (fn [_]
+       (log/info "Starting TEET service in port " port)
+       (server/run-server
+        (params/wrap-params
+         (routes
+          (tara-routes/tara-routes (tara-endpoint/discover "https://tara-test.ria.ee/oidc")
+                                   {:client-id (System/getenv "TARA_CLIENT_ID")
+                                    :client-secret (System/getenv "TARA_CLIENT_SECRET")
+                                    :base-url (System/getenv "BASE_URL")
+                                    :on-error #(do
+                                                 (log/error "TARA auth error" %)
+                                                 {:status 500
+                                                  :body "Error processing TARA auth"})
+                                    :on-success #(do
+                                                   (log/info "TARA auth success, token: " %)
+                                                   {:status 302
+                                                    :headers {"Location" (System/getenv "BASE_URL")}})})
+          (routes/teet-routes)))
+        {:ip "0.0.0.0"
+         :port port})))))
 
 (defn stop []
   (server))
