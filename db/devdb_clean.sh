ac338095557cc899d8937954fecd148b40d17c87
Ville Outamaa <ville.outamaa@solita.fi>
2019-08-19 10:48:17 +0300

    Update devdb_clean.sh

--- db/devdb_clean.sh
+++ db/devdb_clean.sh
@@ -11,11 +11,16 @@ function check_psql {
 check_psql
 
 echo "Dropping and recreating teis database."
-psql -h localhost -U postgres -c "DROP DATABASE teet;"
-psql -h localhost -U postgres -c "DROP ROLE authenticator;"
-psql -h localhost -U postgres -c "DROP ROLE teet_anon;"
-psql -h localhost -U postgres -c "DROP ROLE teet_user;"
+psql -h localhost -U postgres -c "DROP DATABASE IF EXISTS teet;"
+psql -h localhost -U postgres -c "DROP ROLE IF EXISTS authenticator;"
+psql -h localhost -U postgres -c "DROP ROLE IF EXISTS teet_anon;"
+psql -h localhost -U postgres -c "DROP ROLE IF EXISTS teet_user;"
+psql -h localhost -U postgres -c "DROP ROLE IF EXISTS teet;"
+psql -h localhost -U postgres -c "CREATE ROLE teet WITH LOGIN SUPERUSER;"
 psql -h localhost -U postgres -c "CREATE DATABASE teet OWNER teet;"
 
 echo "Running migrations"
 mvn flyway:migrate
+
+echo "Adding all privileges in schema teet to teet_anon."
+psql -h localhost -U teet -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA teet TO teet_anon;"
