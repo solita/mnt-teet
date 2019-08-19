bb5c2ee91e652f049793c34c8c31a5d6ca2dc98d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-19 15:26:05 +0300

    Use build info edn file

--- app/backend/src/clj/teet/util/build_info.clj
+++ app/backend/src/clj/teet/util/build_info.clj
@@ -1,6 +1,10 @@
 (ns teet.util.build-info
-  (:require [clojure.java.shell :as sh]
-            [clojure.string :as str]))
+  (:require [clojure.java.io :as io]))
+
+(defonce build-info (delay (-> "build-info.edn"
+                               io/resource
+                               slurp
+                               read-string)))
 
 (defmacro git-commit []
-  (str/trim (:out (sh/sh "git" "rev-parse" "HEAD"))))
+  (:git-commit @build-info))
