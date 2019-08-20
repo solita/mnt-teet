c914bfedf6c37c8193992fe3cc82a7ebc8cf2fda
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-20 10:27:54 +0300

    Add simple item view to project listing table

--- app/frontend/src/cljs/teet/projects/projects_view.cljs
+++ app/frontend/src/cljs/teet/projects/projects_view.cljs
@@ -7,7 +7,8 @@
             [teet.search.search-interface :as search-interface]
             [teet.ui.icons :as icons]
             [teet.login.login-paths :as login-paths]
-            [teet.projects.project-form :as project-form]))
+            [teet.projects.project-form :as project-form]
+            [postgrest-ui.components.item-view :as postgrest-item-view]))
 
 (defmethod search-interface/format-search-result "project" [{:keys [id label]}]
   {:icon [icons/file-folder-open]
@@ -25,7 +26,17 @@
     :state (get-in app [:projects :listing])
     :set-state! #(e! (projects-controller/->SetListingState %))
     :table "thk_project_search"
-    :select ["id" "name" "estimated_duration" "road_nr" "km_range"]}])
+    :select ["id" "name" "estimated_duration" "road_nr" "km_range"]
+    :drawer (fn [{:strs [id]}]
+              [postgrest-item-view/item-view
+               {:endpoint (get-in app [:config :api-url])
+                :token (get-in app login-paths/token)
+                :table "thk_project"
+                :select ["name" "estimated_duration"
+                         "road_nr" "km_range" "carriageway"
+                         "procurement_no"]}
+               id])}
+   ])
 
 (defn projects-page [e! app]
   [projects-listing e! app])
