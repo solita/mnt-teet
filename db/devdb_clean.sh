561059be0b06a8585a9b5b24214a4eb69b9b9111
Ville Outamaa <ville.outamaa@solita.fi>
2019-08-20 16:37:46 +0300

    Grant privileges to teet_user in devdb_clean.sh

--- db/devdb_clean.sh
+++ db/devdb_clean.sh
@@ -11,6 +11,7 @@ function check_psql {
 check_psql
 
 PSQL="psql -h localhost -U postgres -c"
+PSQL_TEET="psql -h localhost -U teet -c"
 
 echo "Dropping and recreating teis database."
 $PSQL "DROP DATABASE IF EXISTS teet;"
@@ -26,4 +27,9 @@ echo "Running migrations"
 mvn flyway:migrate
 
 echo "Adding all privileges in schema teet to teet_anon."
-psql -h localhost -U teet -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA teet TO teet_anon;"
+$PSQL_TEET "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA teet TO teet_anon;"
+
+echo "Adding all privileges in schema teet to teet_anon."
+$PSQL_TEET "GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA teet TO teet_user;"
+
+echo "Remember to import THK projects with teet.thk.thk-import/run-test-import!"
