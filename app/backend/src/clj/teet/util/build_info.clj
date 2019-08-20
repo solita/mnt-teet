9efdbac0c79e6ddba40c23a9558e1d2762d0c4ec
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-20 12:31:18 +0300

    Don't crash if build-info.edn is not found

--- app/backend/src/clj/teet/util/build_info.clj
+++ app/backend/src/clj/teet/util/build_info.clj
@@ -1,10 +1,11 @@
 (ns teet.util.build-info
   (:require [clojure.java.io :as io]))
 
-(defonce build-info (delay (-> "build-info.edn"
-                               io/resource
-                               slurp
-                               read-string)))
+(defonce build-info (delay
+                      (some-> "build-info.edn"
+                              io/resource
+                              slurp
+                              read-string)))
 
 (defmacro git-commit []
   (:git-commit @build-info))
