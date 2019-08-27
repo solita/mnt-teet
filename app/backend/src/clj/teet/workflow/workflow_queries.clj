3cad038a6738098e9c8c53855e95d1c731fae793
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-27 20:03:43 +0300

    Formatting

--- app/backend/src/clj/teet/workflow/workflow_queries.clj
+++ app/backend/src/clj/teet/workflow/workflow_queries.clj
@@ -2,14 +2,11 @@
   (:require [teet.db-api.core :as db-api]))
 
 (defmethod db-api/query :fetch-workflow [db {:keys [id]}]
-  {:query `[:find
-            (pull ?e [:workflow/name
-                      {:workflow/phases
-                       [:db/id
-                        :phase/name
-                        {:phase/tasks [:db/id :task/status]}]}])
+  {:query '[:find (pull ?e [:workflow/name
+                            {:workflow/phases
+                             [:db/id
+                              :phase/name
+                              {:phase/tasks [:db/id :task/status]}]}])
             :in $ ?id
-            :where
-            [?e :db/id ?id]
-            [?e :workflow/name _]]
+            :where [?e :db/id ?id]]
    :args [db id]})
