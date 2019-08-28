f88836c1dc5a8bd0b5a34f18872fd989f7ee97a8
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-28 10:14:01 +0300

    Package frontend artifacts

--- app/frontend/resources.sh
+++ app/frontend/resources.sh
@@ -7,3 +7,10 @@ curl -o resources/public/material-ui.production.min.js https://unpkg.com/@materi
 # FIXME: do a shasum check here
 
 cp target/public/cljs-out/prod-main.js resources/public/main.js
+
+echo "Package artifact"
+
+COMMIT=`git rev-parse HEAD`
+cd resources/public
+zip -r ../../../../frontend-$COMMIT.zip *
+cd ../..
