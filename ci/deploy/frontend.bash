503a9a9a6f805e4907234b29974a5785e6c6b26b
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-29 12:59:40 +0300

    Log commit used

--- ci/deploy/frontend.bash
+++ ci/deploy/frontend.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Fetching frontend build
-aws s3 cp s3://teet-build/frontend/frontend-$COMMIT.zip frontend.zip
+echo "Fetching frontend build: $COMMIT"
+aws s3 cp s3://teet-build/teet-frontend/frontend-$COMMIT.zip frontend.zip
 mkdir frontend
 cd frontend
 unzip ../frontend.zip
