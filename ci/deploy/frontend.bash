1226256a8f6ebb68632759e151e951ad6da31368
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-29 13:10:47 +0300

    Clear bucket of old version files before sync

--- ci/deploy/frontend.bash
+++ ci/deploy/frontend.bash
@@ -5,4 +5,5 @@ mkdir frontend
 cd frontend
 unzip ../frontend.zip
 cd ..
-aws s3 sync frontend s3://teet-dev-public --acl public-read --delete
+aws s3 rm s3://teet-dev-public --recursive
+aws s3 sync frontend s3://teet-dev-public --acl public-read
