5babde990b8d9da3aeadc210419412f8a5de7f49
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-30 08:42:47 +0300

    Check specs for queries/commands

--- app/backend/src/clj/teet/db_api/db_api_handlers.clj
+++ app/backend/src/clj/teet/db_api/db_api_handlers.clj
@@ -4,10 +4,12 @@
             [teet.environment :as environment]
             [teet.transit :as transit]
             [teet.db-api.core :as db-api]
+            [clojure.spec.alpha :as s]
 
             ;; Require all namespaces that provide queries/commands
             [teet.workflow.workflow-queries]
-            [teet.workflow.workflow-commands]))
+            [teet.workflow.workflow-commands]
+            [taoensso.timbre :as log]))
 
 
 (defn- request [handler-fn]
@@ -17,13 +19,21 @@
           request-payload (transit/transit-request req)]
       (transit/transit-response (handler-fn ctx request-payload)))))
 
+(defn- check-spec [spec data]
+  (if (nil? (s/get-spec spec))
+    (do (log/warn "No spec for " spec ", every query and command should have a spec!")
+        nil)
+    (when-let [problems (s/explain-data spec data)]
+      (merge {:error "Spec validation failed"} problems))))
+
 (def query-handler
   "Ring handler to invoke a named Datomic query.
   Takes input as transit, invokes the query and returns
   the result as transit."
   (request
    (fn [ctx query-def]
-     (let [query-result (db-api/query ctx query-def)]
+     (let [query-result (or (check-spec (:query/name query-def) query-def)
+                            (db-api/query ctx query-def))]
        (if (and (map? query-result)
                 (contains? query-result :query)
                 (contains? query-result :args))
@@ -36,4 +46,5 @@
   the result as transit."
   (request
    (fn [ctx command-def]
-     (db-api/command! ctx command-def))))
+     (or (check-spec (:command/name command-def) command-def)
+         (db-api/command! ctx command-def)))))
--- app/backend/src/clj/teet/workflow/workflow_commands.clj
+++ app/backend/src/clj/teet/workflow/workflow_commands.clj
@@ -1,8 +1,14 @@
 (ns teet.workflow.workflow-commands
   "Commands for workflows, phases and tasks"
   (:require [teet.db-api.core :as db-api]
-            [datomic.client.api :as d]))
+            [datomic.client.api :as d]
+            [clojure.spec.alpha :as s]
+            [teet.workflow.workflow-specs]))
 
-(defmethod db-api/command! :workflow/create-workflow [conn {:keys [thk-project-id
-                                                                   phases]}]
-  (d/transact ))
+
+(defmethod db-api/command! :workflow/create-project-workflow [{conn :conn} project-workflow]
+  ;; FIXME: automatically validate spec based on command
+  (when-not (s/valid? :workflow/project-workflow project-workflow)
+    (throw (ex-info "Project workflow is not valid"
+                    {:explain (s/explain-data :workflow/project-workflow project-workflow)})))
+  (d/transact conn {:tx-data [project-workflow]}))
