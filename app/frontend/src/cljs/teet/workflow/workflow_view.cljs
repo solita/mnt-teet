da69ce5ee0491e0bc4bf029acd4842f2006e0cb3
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-30 11:48:02 +0300

    Use itemlist

--- app/frontend/src/cljs/teet/workflow/workflow_view.cljs
+++ app/frontend/src/cljs/teet/workflow/workflow_view.cljs
@@ -1,11 +1,21 @@
 (ns teet.workflow.workflow-view
-  (:require [teet.workflow.workflow-controller :as workflow-controller]))
+  (:require [teet.workflow.workflow-controller :as workflow-controller]
+            [teet.ui.itemlist :as itemlist]
+            [teet.ui.format :as format]))
 
 
 (defn workflow-page
   "Main workflow page"
-  [e! workflow]
-  [:div (pr-str workflow)])
+  [e! {:workflow/keys [phases] :as workflow}]
+  [:<>
+   (for [phase phases]
+     ^{:key (:db/id phase)}
+     [itemlist/ProgressList
+      {:title (:phase/name phase) :subtitle (some-> phase :phase/due-date format/date)}
+      (for [{:task/keys [status name] :as task} (:phase/tasks phase)]
+        {:status status
+         :name name})])
+   [:div (pr-str workflow)]])
 
 (defn workflow-page-and-title [e! {params :params :as app}]
   (let [id (params :workflow)
