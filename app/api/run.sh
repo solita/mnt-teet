e98ccff8970ff7d8ae9214eea70360ee020e0937
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-09-04 12:23:25 +0300

    Use secret from parameter store

--- app/api/run.sh
+++ app/api/run.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-JWKS_URL="https://cognito-idp.$COGNITO_REGION.amazonaws.com/$COGNITO_POOL_ID/.well-known/jwks.json"
-echo Downloading JWKS from $JWKS_URL
-
-curl -o /tmp/postgrest-key.jwks $JWKS_URL
-
 echo STARTING PostgREST
 postgrest /etc/postgrest.conf
