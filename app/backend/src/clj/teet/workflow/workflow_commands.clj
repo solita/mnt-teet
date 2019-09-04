703a336d77c76fff383756634d89c4dc7385d8f5
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-09-04 16:45:14 +0300

    Update task command

--- app/backend/src/clj/teet/workflow/workflow_commands.clj
+++ app/backend/src/clj/teet/workflow/workflow_commands.clj
@@ -3,8 +3,18 @@
   (:require [teet.db-api.core :as db-api]
             [datomic.client.api :as d]
             [clojure.spec.alpha :as s]
-            [teet.workflow.workflow-specs]))
+            [teet.workflow.workflow-specs]
+            [taoensso.timbre :as log]))
 
 
 (defmethod db-api/command! :workflow/create-project-workflow [{conn :conn} project-workflow]
   (select-keys (d/transact conn {:tx-data [project-workflow]}) [:tempids]))
+
+(defmethod db-api/command! :workflow/update-task [{conn :conn} task]
+  (select-keys (d/transact conn {:tx-data [task]}) [:tempids]))
+
+(defmethod db-api/command! :workflow/add-task-to-phase [{conn :conn} {phase-id :phase-id
+                                                                      task :task}]
+  (select-keys (d/transact conn {:tx-data [{:db/id phase-id
+                                            :phase/tasks [task]}]}) [:tempids])
+  )
