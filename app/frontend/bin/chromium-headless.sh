3348f91d7ea1fba98cafcea3e22524e39dbbc056
Ville Outamaa <ville.outamaa@solita.fi>
2019-09-05 08:29:24 +0300

    TEET-44 Remove check for chromium

--- app/frontend/bin/chromium-headless.sh
+++ app/frontend/bin/chromium-headless.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-function check_chromium {
-    if ! [ -x "$(command -v "$CHROMIUM_BROWSER")" ]; then
-        echo "chromium not found, make sure it is in PATH" >&2
-        exit 1
-    fi
-}
-
-check_chromium
-
 "$CHROMIUM_BROWSER" --headless --disable-gpu --repl "$@"
