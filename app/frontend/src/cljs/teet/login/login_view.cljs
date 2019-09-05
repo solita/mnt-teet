54e8f8ef06566f5a5115fd60c56ce019967d7c68
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-09-05 11:51:23 +0300

    Simpler demo login page

--- app/frontend/src/cljs/teet/login/login_view.cljs
+++ app/frontend/src/cljs/teet/login/login_view.cljs
@@ -1,66 +1,54 @@
 (ns teet.login.login-view
   "Login page"
   (:require [teet.login.login-controller :as login-controller]
-            [teet.ui.material-ui :refer [Select Container Card CardHeader CardContent TextField Grid Button]]
+            [teet.ui.material-ui :refer [Container Card CardHeader CardContent Button
+                                         Typography]]
             [teet.ui.layout :as layout]
             [teet.ui.itemlist]
             [teet.localization :refer [tr]]
             [teet.ui.icons :as icons]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [reagent.core :as r]
+            [teet.ui.typography :as typography]))
 
 
-(def mock-users-map {:consultant "Carla Consultant (ACME Road Consulting, Ltd.)"
-                     :design-manager "Danny Design-Manager (Maanteeamet)"
-                     :boss "Benjamin Boss (Maanteeamet, department head)"})
+(def mock-users [{:user/id #uuid "4c8ec140-4bd8-403b-866f-d2d5db9bdf74"
+                  :user/given-name "Danny"
+                  :user/family-name "Design-Manager"
+                  :user/email "danny.design-manager@example.com"
+                  :user/organization "Maanteeamet"}
+
+                 {:user/id #uuid "ccbedb7b-ab30-405c-b389-292cdfe85271"
+                  :user/given-name "Carla"
+                  :user/family-name "Consultant"
+                  :user/email "carla.consultant@example.com"
+                  :user/organization "ACME Road Consulting, Ltd."}
+
+                 {:user/id #uuid "fa8af5b7-df45-41ba-93d0-603c543c880d"
+                  :user/given-name "Benjamin"
+                  :user/family-name "Boss"
+                  :user/email "benjamin.boss@example.com"
+                  :user/organization "Maanteeamet"}])
+
 
 (defn login-page [e! {login :login}]
-  [Container {:maxWidth "xs"}
-   [Card
-    [CardHeader {:title "Maanteeamet TEET"}]
-    [CardContent
-     [layout/column {:content-style {:padding-bottom "2em"}}
-      [teet.ui.itemlist/LinkList
-       {:title "Available users"}
-       [{:link "/select-user/carla-consultant@acme"
-         :name "Carla Consultant (ACME Road Consulting, Ltd.)"}
-        {:link "/select-user/danny.design-manager@maanteamet"
-         :name "Danny Design-Manager (Maanteeamet)"}]
-       (fn on-click-handler [evt]
-         (.preventDefault evt)
-         (let [href (-> evt .-target .-href)
-               role (last (clojure.string/split href "/"))]         
-           (js/console.log "selected role is" role)           
-           (e! (login-controller/->UpdateLoginForm
-                (assoc (:form login)
-                       :username role)))
-           ))]
-      #_[teet.ui.select/select-with-action {:items (vec (keys mock-users-map))
-                                          :item-label mock-users-map
-                                          :name "Select user"
-                                          
-                                          :on-select #(e! (login-controller/->UpdateLoginForm
-                                                           (do
-                                                             (js/console.log (str "username: " (pr-str  (-> % .-target .-value))))
-                                                             (assoc  (:form login) :username (-> % .-target .-value))
-                                                             )))                                       
-                                          }]
-      [TextField {:style {:width "300px"}
-                  :label (tr [:login :username])
-                  :value (or (:username (:form login)) "")
-                  :on-change #(e! (login-controller/->UpdateLoginForm (assoc (:form login)
-                                                                             :username (-> % .-target .-value))))}]
-
-      [TextField {:style {:width "300px"}
-                  :label (tr [:login :password])
-                  :value (or (:password (:form login)) "")
-                  :type "password"
-                  :on-change #(e! (login-controller/->UpdateLoginForm (assoc (:form login)
-                                                                             :password (-> % .-target .-value))))
-                  :on-key-down #(when (= "Enter" (.-key %))
-                                  (e! (login-controller/->Login)))}]
-
-      [layout/row {}
-       [Button {:color "primary"
-                :on-click #(e! (login-controller/->Login))}
-        (tr [:login :login])]]]]]])
-
+  [Container {:maxWidth "sm"}
+   [typography/Heading1 "Maanteeamet TEET"]
+   [layout/column {:content-style {:padding-bottom "2em"}}
+    [typography/Heading2 "Login with demo user"]
+    (doall
+     (for [{:user/keys [given-name family-name organization email] :as user} mock-users]
+       ^{:key email}
+       [:span
+        [Card
+         [CardHeader {:title (str given-name " " family-name)
+                      :action (r/as-element
+                               [Button {:color "primary"
+                                        :on-click #(log/info "Login: " user)}
+                                "Login"])}]
+         [CardContent
+          [Typography
+           "Name: " given-name " " family-name [:br]
+           "Organization: " organization [:br]
+           "Email: " email]]]
+        [:br]]))]])
