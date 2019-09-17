758633af180bad5bf0ce353c2d8d00af5b88a845
Erno Kuusela <erno-laptop!erno>
2019-09-17 21:02:25 +0300

    S3 multipart upload experiment WIP
    Currently hitting a CORS error from the server side

--- app/frontend/src/cljs/teet/document/document_controller.cljs
+++ app/frontend/src/cljs/teet/document/document_controller.cljs
@@ -46,12 +46,57 @@
                              (assoc doc :progress progress))))
                        docs)))))
 
-(defmethod tuck.effect/process-effect ::upload! [e! {:keys [file document url app-path]}]
+
+;; non-multipart version
+#_(defmethod tuck.effect/process-effect ::upload! [e! {:keys [file document url app-path]}]
   (-> (js/fetch url #js {:method "PUT" :body file})
       (.then (fn [^js/Response resp]
                (e! (->UpdateDocumentProgress app-path document nil))
                (when-not (.-ok resp)
                  (log/warn "Upload failed: " (.-status resp) (.-statusText resp)))))))
+
+(defn completion-xmldoc [part-etags]
+  (str "<CompleteMultipartUpload>\n"
+       (clojure.string/join
+              (map #(str "  <Part> <PartNumber>" %1 "</PartNumber> <ETag>" %2 "</ETag> </Part>\n")
+                   (range (count part-etags))
+                   part-etags))
+       "</CompleteMultipartUpload>\n"))
+
+
+(defmethod tuck.effect/process-effect ::upload! [e! {:keys [file document url app-path]}]
+  ;; you can upload any number of parts. the ones before the last one have to be at least 5mb long.
+  ;; you save the returned etags of the uploaded parts.
+  ;; at the final complete-multipart phase, s3 will cat the parts together when you provide the upload id and the etags.
+  (log/info "multipart upload starting")
+  (let [init-multipart! #(js/fetch (str (first (clojure.string/split url "?")) "?uploads") #js {:method "POST"})        
+        upload-part! #(js/fetch (str url "&partNumber=" 1 "&uploadId=" (.text %))
+                                #js {:method "PUT" :body file})
+        part-etags (atom [])
+        complete-multipart! #(js/fetch (str url "&uploadId=" (.text %))
+                                       #js {:method "POST" :body (completion-xmldoc @part-etags)})]
+
+    (->
+     (init-multipart!)
+     (.then (fn [^js/Response init-resp]
+              (if (.-ok init-resp)
+                (do 
+                  (log/info "multipart upload init got ok response, body should contain upload id:" (.text init-resp))
+                  (->  (upload-part! init-resp)
+                       (.then (fn [^js/Response upload-resp]
+                                (if (.-ok upload-resp)
+                                  (do 
+                                    (log/info "part 1 upload response text:" (.text upload-resp))
+                                    (log/info "part 1 etag:" (.get (.-headers upload-resp) "ETag"))
+                                    (swap! part-etags conj (.get (.-headers upload-resp) "ETag"))
+                                    (-> (complete-multipart! upload-resp)
+                                        (.then (fn [^js/Response complete-resp]
+                                                 (e! (->UpdateDocumentProgress app-path document nil))
+                                                 (if (.-ok complete-resp)
+                                                   (log/info "status for complete-multipart call:" (.-statusText complete-resp))
+                                                   (log/warn "Upload failed: " (.-status complete-resp) (.-statusText complete-resp)))))))
+                                  (log/info "part upload response not ok"))))))
+                (log/info "multipart upload init failed. statustext:" (.statusText init-resp))))))))
 
 (defmethod tuck.effect/process-effect :upload-documents [e! {:keys [files task-id project-id app-path]}]
   (assert project-id "Must specify THK project id with :project-id key")
