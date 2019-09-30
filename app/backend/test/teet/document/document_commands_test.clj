b3c18aaf90882675a4cd46161efd883397c88b3e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-09-30 13:06:51 +0300

    Fix document commands test

--- app/backend/test/teet/document/document_commands_test.clj
+++ app/backend/test/teet/document/document_commands_test.clj
@@ -4,14 +4,14 @@
 
 (deftest validate-document
   (testing "too large files are invalid"
-    (is (= (:error (document-commands/validate-document {:document/type "image/png"
-                                                         :document/size (* 1024 1024 1024)}))
+    (is (= (:error (document-commands/validate-file {:file/type "image/png"
+                                                     :file/size (* 1024 1024 1024)}))
            :file-too-large)))
   (testing "files of illegal type are invalid"
-    (is (= (:error (document-commands/validate-document {:document/type "application/nonsense"
-                                                         :document/size 1024}))
+    (is (= (:error (document-commands/validate-file {:file/type "application/nonsense"
+                                                     :file/size 1024}))
            :file-type-not-allowed)))
   (testing "other files are valid"
-    (is (nil? (document-commands/validate-document
-               {:document/type (rand-nth (vec document-commands/upload-allowed-file-types))
-                :document/size (rand (inc document-commands/upload-max-file-size))})))))
+    (is (nil? (document-commands/validate-file
+               {:file/type (rand-nth (vec document-commands/upload-allowed-file-types))
+                :file/size (rand (inc document-commands/upload-max-file-size))})))))
