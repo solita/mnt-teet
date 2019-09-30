39d6e99c3edaa1cd766a751bdcfb62aa6fb532c2
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-09-30 15:30:05 +0300

    Wrap middlewares in ion handlers

--- app/backend/src/clj/teet/db_api/db_api_ion.clj
+++ app/backend/src/clj/teet/db_api/db_api_ion.clj
@@ -6,10 +6,24 @@
             [teet.environment :as environment]
             [taoensso.timbre :as log]
             [taoensso.timbre :as timbre]
-            [datomic.ion.cast :as cast]))
+            [datomic.ion.cast :as cast]
+            [ring.middleware.params :as params]
+            [ring.middleware.cookies :as cookies]
+            [ring.middleware.session :as session]))
 
-(def db-api-query (apigw/ionize db-api-handlers/query-handler))
-(def db-api-command (apigw/ionize db-api-handlers/command-handler))
+(defn- wrap-middleware [handler]
+  (-> handler
+      params/wrap-params
+      cookies/wrap-cookies
+      ;; FIXME: we don't need session yet (with TARA login, add it)
+      ))
+
+(def db-api-query (-> db-api-handlers/query-handler
+                      wrap-middleware
+                      apigw/ionize))
+(def db-api-command (-> db-api-handlers/command-handler
+                        wrap-middleware
+                        apigw/ionize))
 
 (environment/init-ion-config! (ion/get-env))
 
