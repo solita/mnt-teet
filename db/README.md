9a6c8d72a592afbca66c41105df4bfef9a686136
Ville Outamaa <ville.outamaa@solita.fi>
2019-10-04 12:51:44 +0300

    TEET-113 Import restriction data dump in devdb_clean.sh

--- db/README.md
+++ db/README.md
@@ -8,17 +8,12 @@ pom.xml contains a template for running the flyway migrations.
 
 * Install PostgreSQL with PostGIS extension locally (or run with docker)
 
-* Run commands in psql:
-
-> CREATE ROLE teet WITH LOGIN SUPERUSER;
-> CREATE DATABASE teet OWNER teet;
-
-
-* Run migrations: mvn flyway:migrate
+* Run `devdb_clean.sh`
 
 ## GIS data dump
 
 * This is loaded from a private S3 bucket. If you don't have AWS SDK creds set up yet: clone a dev sdk user in the IAM AWS console & set up your AWS CLI creds so that you can sucessfully run es `aws s3 ls  s3://teet-dev-files`
+* NOTE that the dump is imported when running `devdb_clean.sh`!
 
 
 ## Maa-amet restrictions data dump
@@ -27,3 +22,4 @@ pom.xml contains a template for running the flyway migrations.
 * Download and unzip "Kitsenduste mõjualad GPKG"
 * run `ogr2ogr -f "PostgreSQL" PG:"host=localhost user=teet dbname=teet" KITSENDUSED.gpkg -lco schema=restrictions -lco spatial_index=yes` to import the restrictions tables (takes a while)
 * run SELECT public.ensure_restrictions_indexes();
+* NOTE that the restriction data dump is also imported when running `devdb_clean.sh`!
--- db/devdb_clean.sh
+++ db/devdb_clean.sh
@@ -8,6 +8,27 @@ function check_psql {
     fi
 }
 
+function check_curl {
+    if ! [ -x "$(command -v curl)" ]; then
+        echo "curl not found, make sure it is in PATH" >&2
+        exit 1
+    fi
+}
+
+function check_unzip {
+    if ! [ -x "$(command -v unzip)" ]; then
+        echo "unzip not found, make sure it is in PATH" >&2
+        exit 1
+    fi
+}
+
+function check_ogr2ogr {
+    if ! [ -x "$(command -v ogr2ogr)" ]; then
+        echo "ogr2ogr not found, make sure it is in PATH" >&2
+        exit 1
+    fi
+}
+
 check_psql
 
 ARGS="-h localhost -U postgres"
@@ -39,3 +60,15 @@ $PSQL_TEET "GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA teet TO
 
 echo "Importing THK projects with teet.thk.thk-import/run-test-import!"
 aws s3 cp s3://teet-dev-files/db/THK_export.csv - | (cd ../app/backend && clj -m teet.thk.thk-import)
+
+echo "Importing Maa-amet restrictions data dump"
+check_curl
+check_unzip
+check_ogr2ogr
+curl "https://geoportaal.maaamet.ee/docs/KPO/KITSENDUSED_GPKG.zip" -o temp.zip
+unzip temp.zip
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=teet dbname=teet" KITSENDUSED.gpkg -lco schema=restrictions -lco spatial_index=yes
+rm KITSENDUSED.gpkg
+rm temp.zip
+
+$PSQL_TEET "SELECT public.ensure_restrictions_indexes();"
