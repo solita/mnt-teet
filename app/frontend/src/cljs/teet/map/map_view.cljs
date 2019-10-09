f6753b8c2e3b0fcc3fd895312d98863fbbcc6ec9
jkorpelin <joni.korpelin@solita.fi>
2019-10-09 10:45:14 +0300

    Fixed map layer toggle not working

--- app/frontend/src/cljs/teet/map/map_view.cljs
+++ app/frontend/src/cljs/teet/map/map_view.cljs
@@ -57,13 +57,13 @@
                                                                (e! (map-controller/->LayerToggle category layer)))}])}]]))]]])))
 
 (defn map-layer-controls
-  [e! _map-layers _map-controls]
+  [e! _map-layers {:keys [_open?]} _map-controls]
   (r/create-class
     {:component-did-mount
      (fn [_]
        (e! (map-controller/->FetchMapLayers)))
      :reagent-render
-     (fn [e! map-layers {:keys [open?] :as map-controls}]
+     (fn [e! map-layers {:keys [open?]} map-controls]
        [:div
         (when open?
           [ClickAwayListener {:on-click-away #(e! (map-controller/->CloseMapControls))}
