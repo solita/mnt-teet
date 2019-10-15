87c510b3938c4b26814f57468884d4c5ed234755
Ville Outamaa <ville.outamaa@solita.fi>
2019-10-15 11:54:18 +0300

    Try to fix CircleCI frontend tests

--- app/frontend/bin/chromium-headless.sh
+++ app/frontend/bin/chromium-headless.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-"$CHROMIUM_BROWSER" --headless --no-sandbox --disable-gpu --repl "$@"
+if [ -z "$CHROMIUM_BROWSER" ]; then
+    "$CHROMIUM_BROWSER" --headless --no-sandbox --disable-gpu --repl "$@"
+elif [ -z "$CHROME" ]; then
+    "$CHROME" "$@"
+fi
