09d1f67cb5e27e58e09a4a3bb51caec42866af34
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-10-30 13:13:12 +0200

    Require admin role to execute :admin/create-user

--- app/backend/src/clj/teet/admin/admin_commands.clj
+++ app/backend/src/clj/teet/admin/admin_commands.clj
@@ -1,6 +1,7 @@
 (ns teet.admin.admin-commands
   (:require [teet.db-api.core :as db-api]
-            [datomic.client.api :as d]))
+            [datomic.client.api :as d]
+            [teet.user.user-roles :as user-roles]))
 
 (defmethod db-api/command! :admin/create-user [{conn :conn} {:user/keys [id person-id roles]}]
   (d/transact conn
@@ -8,3 +9,6 @@
                           :user/person-id person-id
                           :user/roles roles}]})
   :ok)
+
+(defmethod db-api/command-authorization :admin/create-user [{user :user} _]
+  (user-roles/require-role user :admin))
