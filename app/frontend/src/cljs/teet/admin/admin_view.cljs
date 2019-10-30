383d8de23cb0cf1ae6574902af4f927adde5c397
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-10-30 13:11:36 +0200

    Add preliminary listing of users to admin page

--- app/frontend/src/cljs/teet/admin/admin_view.cljs
+++ app/frontend/src/cljs/teet/admin/admin_view.cljs
@@ -1,7 +1,25 @@
 (ns teet.admin.admin-view
   "Admin user interface"
   (:require [teet.admin.admin-controller :as admin-controller]
-            [teet.user.user-controller :as user-controller]))
+            [teet.user.user-controller :as user-controller]
+            [teet.ui.material-ui :refer [Table TableHead TableBody TableRow TableCell]]
+            [clojure.string :as str]))
 
 (defn admin-page [e! app users]
-  [:div "this is the admin page!"])
+  [:div "this is the admin page!"
+   [Table {}
+    [TableHead {}
+     [TableRow
+      [TableCell {} "person id"]
+      [TableCell {} "given name"]
+      [TableCell {} "family name"]
+      [TableCell {} "roles"]]]
+    [TableBody {}
+     (doall
+      (for [{:user/keys [person-id given-name family-name roles id]} users]
+        ^{:key id}
+        [TableRow {}
+         [TableCell {} person-id]
+         [TableCell {} given-name]
+         [TableCell {} family-name]
+         [TableCell {} (str/join ", " (map name roles))]]))]]])
--- app/frontend/src/cljs/teet/ui/material_ui.cljs
+++ app/frontend/src/cljs/teet/ui/material_ui.cljs
@@ -43,7 +43,7 @@
 (define-mui-components Tabs Tab TabPanel)
 
 ;; Table
-(define-mui-components Table TableRow TableHead TableCell TableSortLabel)
+(define-mui-components Table TableRow TableHead TableCell TableSortLabel TableBody)
 
 ;; Transitions
 (define-mui-components Zoom Fade)
