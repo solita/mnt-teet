fd5e80db74bb99dba76d439571648d22b3677178
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-10-30 14:55:27 +0200

    Add working user creationg

--- app/frontend/src/cljs/teet/admin/admin_controller.cljs
+++ app/frontend/src/cljs/teet/admin/admin_controller.cljs
@@ -1,2 +1,36 @@
 (ns teet.admin.admin-controller
-  (:require [tuck.core :as t]))
+  (:require [tuck.core :as t]
+            [teet.common.common-controller :as common-controller]))
+
+(defrecord CreateUser [])
+(defrecord UpdateUserForm [form-data])
+(defrecord SaveUser [])
+(defrecord SaveUserResponse [response])
+(defrecord CancelUser [])
+
+(extend-protocol t/Event
+  CreateUser
+  (process-event [_ app]
+    (assoc-in app [:admin :create-user] {}))
+
+  UpdateUserForm
+  (process-event [{form-data :form-data} app]
+    (update-in app [:admin :create-user] merge form-data))
+
+  SaveUser
+  (process-event [_ app]
+    (t/fx app
+          {:tuck.effect/type :command!
+           :command :admin/create-user
+           :payload (get-in app [:admin :create-user])
+           :result-event ->SaveUserResponse}))
+
+  SaveUserResponse
+  (process-event [_ app]
+    (-> app
+        (update :admin dissoc :create-user)
+        common-controller/refresh-page))
+
+  CancelUser
+  (process-event [_ app]
+    (update app :admin dissoc :create-user)))
--- app/frontend/src/cljs/teet/admin/admin_view.cljs
+++ app/frontend/src/cljs/teet/admin/admin_view.cljs
@@ -2,18 +2,33 @@
   "Admin user interface"
   (:require [teet.admin.admin-controller :as admin-controller]
             [teet.user.user-controller :as user-controller]
-            [teet.ui.material-ui :refer [Table TableHead TableBody TableRow TableCell]]
-            [clojure.string :as str]))
+            [teet.ui.material-ui :refer [Table TableHead TableBody TableRow TableCell TextField]]
+            [teet.ui.buttons :as buttons]
+            [clojure.string :as str]
+            [teet.localization :refer [tr]]
+            [teet.ui.form :as form]
+            [teet.ui.typography :as typography]))
 
-(defn admin-page [e! app users]
-  [:div "this is the admin page!"
+(defn- create-user-form [e! form]
+  [:div
+   [typography/Heading3 (tr [:admin :create-user])]
+   [form/form {:e! e!
+               :on-change-event admin-controller/->UpdateUserForm
+               :save-event admin-controller/->SaveUser
+               :cancel-event admin-controller/->CancelUser
+               :value form}
+    ^{:attribute :user/person-id}
+    [TextField {:variant "outlined"}]]])
+
+(defn admin-page [e! {admin :admin} users]
+  [:div
    [Table {}
     [TableHead {}
      [TableRow
-      [TableCell {} "person id"]
-      [TableCell {} "given name"]
-      [TableCell {} "family name"]
-      [TableCell {} "roles"]]]
+      [TableCell {} (tr [:fields :user/person-id])]
+      [TableCell {} (tr [:fields :user/given-name])]
+      [TableCell {} (tr [:fields :user/family-name])]
+      [TableCell {} (tr [:fields :user/roles])]]]
     [TableBody {}
      (doall
       (for [{:user/keys [person-id given-name family-name roles id]} users]
@@ -22,4 +37,8 @@
          [TableCell {} person-id]
          [TableCell {} given-name]
          [TableCell {} family-name]
-         [TableCell {} (str/join ", " (map name roles))]]))]]])
+         [TableCell {} (str/join ", " (map name roles))]]))]]
+   (if-let [form (:create-user admin)]
+     [create-user-form e! form]
+     [buttons/button-primary {:on-click (e! admin-controller/->CreateUser)}
+      (tr [:admin :create-user])])])
