b9ec9ee5105de79ac4d6527d0448dabe9bf7c830
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-10-30 13:10:53 +0200

    Return maps for all user data

--- app/backend/src/clj/teet/admin/admin_queries.clj
+++ app/backend/src/clj/teet/admin/admin_queries.clj
@@ -5,7 +5,8 @@
 (defmethod db-api/query :admin/list-users [{db :db} _]
   {:query '[:find (pull ?e [*])
             :where [?e :user/id _]]
-   :args [db]})
+   :args [db]
+   :result-fn (partial mapv first)})
 
 (defmethod db-api/query-authorization :admin/list-users [{user :user} _]
   (user-roles/require-role user :admin))
