5819a3b14deddaf929648080bb9ad569df78bd55
jkorpelin <joni.korpelin@solita.fi>
2019-11-08 13:16:50 +0200

    New pin style for map icon pin

--- app/frontend/src/cljs/teet/map/map_features.cljs
+++ app/frontend/src/cljs/teet/map/map_features.cljs
@@ -5,16 +5,49 @@
             [ol.style.Icon]
             [ol.style.Stroke]
             [ol.style.Fill]
-            [ol.render.Feature]))
+            [ol.render.Feature]
+            [teet.theme.theme-colors :as theme-colors]))
+
+(def ^:const map-pin "M9.96587 0.5C15.221 0.5 19.5 4.78348 19.5 9.96587C19.5 12.7235 17.9724 15.4076 15.9208 18.0187C14.9006 19.3172 13.7685 20.5764 12.6603 21.802C12.611 21.8565 12.5618 21.911 12.5126 21.9653C11.6179 22.9546 10.7407 23.9244 9.9694 24.8638C9.1882 23.8963 8.29237 22.8969 7.37848 21.8774L7.31238 21.8036C6.21334 20.5775 5.08749 19.3183 4.07125 18.0195C2.02771 15.4079 0.5 12.7237 0.5 9.96587C0.5 4.78126 4.78126 0.5 9.96587 0.5Z")
+(def ^:const map-pin-height 26)
+(def ^:const map-pin-width 20)
+
+(def project-pin-icon
+  (memoize
+    (fn [w h fill stroke center-fill]
+      (let [canvas (.createElement js/document "canvas")
+            scaled-w (/ w map-pin-width)
+            scaled-h (/ h map-pin-height)]
+        (set! (.-width canvas) w)
+        (set! (.-height canvas) h)
+        (let [ctx (.getContext canvas "2d")]
+          (.scale ctx scaled-w scaled-h)
+          (when fill
+            (set! (.-strokeStyle ctx) stroke)
+            (set! (.-fillStyle ctx) fill)
+            ;; draw the map pin
+            (let [p (js/Path2D. map-pin)]
+              (.stroke ctx p)
+              (.fill ctx p)))
+          ;; draw the center white circle in the pin
+          (when center-fill
+            (set! (.-strokeStyle ctx) stroke)
+            (set! (.-fillStyle ctx) center-fill)
+            (.arc ctx 10 10 4 0 (* 2 js/Math.PI))
+            (.stroke ctx)
+            (.fill ctx)))
+        canvas))))
+
+
 
 (defn road-line-style [color ^ol.render.Feature _feature res]
   (let [line-width (+ 3 (min 5 (int (/ 200 res))))]
     ;; Show project road geometry line
     (ol.style.Style.
-     #js {:stroke (ol.style.Stroke. #js {:color color
-                                         :width line-width
-                                         :lineCap "butt"})
-          :zIndex 2})))
+      #js {:stroke (ol.style.Stroke. #js {:color color
+                                          :width line-width
+                                          :lineCap "butt"})
+           :zIndex 2})))
 
 (def ^{:doc "Show project geometry as the road line."} project-line-style
   (partial road-line-style "blue"))
@@ -65,33 +98,38 @@
   "Show restriction geometrys as area. Restrictions are all (multi)polygons."
   [^ol.render.Feature feature _res]
   (ol.style.Style.
-   #js {:stroke (ol.style.Stroke. #js {:color "rgba(255,0,0,90)"
-                                       :width 2})
-        :fill (ol.style.Fill. #js {:color (restriction-fill feature "rgba(200,50,50, 0.20)")})}))
+    #js {:stroke (ol.style.Stroke. #js {:color "rgba(255,0,0,90)"
+                                        :width 2})
+         :fill (ol.style.Fill. #js {:color (restriction-fill feature "rgba(200,50,50, 0.20)")})}))
 
 (defn project-pin-style
   "Show project centroid as a pin icon."
   [^ol.render.Feature _feature _res]
   (ol.style.Style.
-   #js {:image (ol.style.Icon.
-                #js {:src "/img/pin.png"
-                     :anchor #js [0.5 1.0]})}))
+    #js {:image (ol.style.Icon.
+                  #js {:img (project-pin-icon map-pin-width
+                                              map-pin-height
+                                              theme-colors/primary
+                                              theme-colors/blue-dark
+                                              theme-colors/white)
+                       :imgSize #js [24 30]
+                       :anchor #js [0.5 1.0]})}))
 
 (defn crosshair-pin-style
   [^ol.render.Feature _feature _res]
   (ol.style.Style.
-   #js {:image (ol.style.Icon.
-                #js {:src "/img/crosshair.png"
-                     :anchor #js [0.5 0.5]})}))
+    #js {:image (ol.style.Icon.
+                  #js {:src "/img/crosshair.png"
+                       :anchor #js [0.5 0.5]})}))
 
 (defn cadastral-unit-style
   "Show cadastral unit."
   [^ol.render.Feature feature _res]
   (let [selected? (.get feature "selected")]
     (ol.style.Style.
-     #js {:stroke (ol.style.Stroke. #js {:color "rgba(40,40,255,0.90)"
-                                         :width 2})
-          :fill (ol.style.Fill. #js {:cursor :pointer
-                                     :color (if selected?
-                                              "rgba(0,0,255,1)"
-                                              "rgba(40,40,255, 0.20)")})})))
+      #js {:stroke (ol.style.Stroke. #js {:color "rgba(40,40,255,0.90)"
+                                          :width 2})
+           :fill (ol.style.Fill. #js {:cursor :pointer
+                                      :color (if selected?
+                                               "rgba(0,0,255,1)"
+                                               "rgba(40,40,255, 0.20)")})})))
