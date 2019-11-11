6d48a317c550e385fbe366656658eb9dce719518
Ville Outamaa <ville.outamaa@solita.fi>
2019-11-11 13:07:02 +0200

    TEET-249 Small fixes to THK integration

--- app/backend/src/clj/teet/thk/thk_integration_ion.clj
+++ app/backend/src/clj/teet/thk/thk_integration_ion.clj
@@ -4,22 +4,28 @@
             [cheshire.core :as cheshire]
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]
+            [clojure.stacktrace :as stacktrace]
             [clojure.string :as str]
             [teet.log :as log]))
 
 (def test-event
-  {:input "{\"Records\":[{\"s3\":{\"bucket\":{\"name\":\"teet-dev-csv-import\"},\"object\":{\"key\":\"thk/unprocessed/hep.csv\"}}}]}"})
+  {:input "{\"Records\":[{\"s3\":{\"bucket\":{\"name\":\"teet-dev-csv-import\"},\"object\":{\"key\":\"thk/unprocessed/not_hep.csv\"}}}]}"})
 
 (def processed-directory "thk/processed/")
 (def error-directory "thk/error/")
 (def log-directory "thk/log/")
 
+(defn- stack-trace [e]
+  (with-out-str (stacktrace/print-stack-trace e 10)))
+
 (defn ctx-exception [ctx message & [e]]
   (ex-info message
-           (assoc ctx :error (str message
-                                  (when e
-                                    (str ": "
-                                         (.getMessage e)))))))
+           (assoc ctx :error {:message (str message
+                                            (when e
+                                              (str ": "
+                                                   (.getMessage e))))
+                              :stack-trace (when e
+                                             (stack-trace e))})))
 
 (defn- decode-input [{:keys [event] :as ctx}]
   (try
@@ -55,18 +61,20 @@
     (assoc ctx :csv
            (-> file
                (io/reader :encoding "UTF-8")
-               (csv/read-csv :separator \,)))
+               (csv/read-csv :separator \;)))
     (catch Exception e
       (throw (ctx-exception ctx "Failed to parse CSV file" e))))) ;; TODO: THK import uses ; as separator
 
-(defn- csv->updates [ctx]
+(defn- csv->updates [{:keys [csv] :as ctx}]
   ;; TODO: create an update data structure that describes necessary updates
-  ctx)
+  (if (empty? csv)
+    ;; TODO: this is just a placeholder for actual parsing failures
+    (throw (ctx-exception ctx "Failed to parse updates from CSV data"))
+    ctx))
 
 (defn- run-updates [ctx]
   ;; TODO: update the projects either in Datomic or Postgres
-  (throw (ctx-exception ctx "Failed to run updates"))
-  )
+  ctx)
 
 (defn- move-file [bucket old-key new-key]
   (s3/copy-object bucket old-key bucket new-key)
@@ -76,29 +84,41 @@
   (str directory
        (-> file-key (str/split #"/") last)))
 
+(defn- add-suffix [file-key suffix]
+  (str file-key suffix))
+
 (defn- move-file-to-processed [{{:keys [bucket file-key]} :s3 :as ctx}]
   (try
-    (let [processed-file-key (change-directory file-key processed-directory)]
+    (let [processed-file-key (-> file-key
+                                 (change-directory processed-directory)
+                                 (add-suffix (str "." (System/currentTimeMillis))))]
       (move-file bucket file-key processed-file-key))
     (catch Exception e
       (throw (ctx-exception ctx "Failed to move file to processed directory" e)))))
 
-(defn- move-file-to-error [{{:keys [bucket file-key]} :s3}]
-  (let [processed-file-key (change-directory file-key error-directory)]
+(defn- move-file-to-error [{{:keys [bucket file-key]} :s3 :as ctx}]
+  (let [processed-file-key (-> file-key
+                               (change-directory error-directory)
+                               (add-suffix (str "." (System/currentTimeMillis))))]
     (move-file bucket file-key processed-file-key)))
 
 (defn- write-error-to-log [{{:keys [bucket file-key]} :s3
-                            error :error}]
-  (log/info bucket " " file-key " " error)
+                            {:keys [message stack-trace]}  :error}]
   (s3/put-object :bucket-name bucket
-                 :key (change-directory file-key log-directory)
-                 :input-stream (io/input-stream (.getBytes (str error)))))
+                 :key (-> file-key
+                          (change-directory log-directory)
+                          (add-suffix (str (System/currentTimeMillis)
+                                           ".error.txt")))
+                 :input-stream (io/input-stream (.getBytes (str message
+                                                                "\n"
+                                                                stack-trace)))))
 
 (defn- on-error [{:keys [error] :as ctx}]
   ;; TODO: Metrics?
   (log/error error)
   (move-file-to-error ctx)
-  (write-error-to-log ctx))
+  (write-error-to-log ctx)
+  nil)
 
 (defn process-thk-file
   [event]
@@ -115,3 +135,4 @@
                  {:input result}))
     (catch Exception e
       (on-error (ex-data e)))))
+>
