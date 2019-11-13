db742b366179a2297318d2f516a4bf6f756ce08f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-11-13 14:46:10 +0200

    Add project model and query all listing columns

--- app/backend/src/clj/teet/project/project_queries.clj
+++ app/backend/src/clj/teet/project/project_queries.clj
@@ -1,9 +1,11 @@
 (ns teet.project.project-queries
-  (:require [teet.db-api.core :as db-api]))
+  (:require [teet.db-api.core :as db-api]
+            [teet.project.project-model :as project-model]))
 
 
 (defmethod db-api/query :thk.project/listing [{db :db} args]
-  {:query '[:find (pull ?e [:thk.project/name :thk.project/id])
+  {:query '[:find (pull ?e columns)
+            :in $ columns
             :where [?e :thk.project/id _]]
-   :args [db]
+   :args [db project-model/project-listing-columns]
    :result-fn (partial mapv first)})
---
+++ app/common/src/cljc/teet/project/project_model.cljc
@@ -0,0 +1,12 @@
+(ns teet.project.project-model
+  "Common project datamodel metadata.")
+
+(def project-listing-columns
+  [:thk.project/id
+   :thk.project/name
+   :thk.project/road-nr
+   :thk.project/start-m
+   :thk.project/end-m
+   :thk.project/carriageway
+   :thk.project/estimated-start-date
+   :thk.project/estimated-end-date])
