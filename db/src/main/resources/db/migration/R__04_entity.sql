7f69a4b0e85a6dcbb70fcac194c7731b1bf50c25
Ville Outamaa <ville.outamaa@solita.fi>
2019-11-14 14:08:52 +0200

    Add teet.geojson_entity_cluster_pins

--- db/src/main/resources/db/migration/R__04_entity.sql
+++ db/src/main/resources/db/migration/R__04_entity.sql
@@ -33,7 +33,7 @@ FROM (SELECT e.tooltip,
             300)) tile;
 $$ LANGUAGE SQL STABLE SECURITY DEFINER;
 
-CREATE FUNCTION teet.geojson_entities(ids BIGINT[])
+CREATE OR REPLACE FUNCTION teet.geojson_entities(ids BIGINT[])
     RETURNS TEXT
 AS $$
 SELECT row_to_json(fc)::TEXT
@@ -48,7 +48,7 @@ FROM (SELECT 'FeatureCollection' as type,
             WHERE e.id = ANY(ids)) f) fc;
 $$ LANGUAGE SQL STABLE SECURITY DEFINER;
 
-CREATE FUNCTION teet.geojson_entities(type entity_type)
+CREATE OR REPLACE FUNCTION teet.geojson_entities(type entity_type)
     RETURNS TEXT
 AS $$
 SELECT row_to_json(fc)::TEXT
@@ -77,10 +77,29 @@ FROM (SELECT 'FeatureCollection' as type,
             WHERE e.type = type AND e.geometry IS NOT NULL AND NOT ST_isempty(e.geometry)) f) fc;
 $$ LANGUAGE SQL STABLE SECURITY DEFINER;
 
+CREATE OR REPLACE FUNCTION teet.geojson_entity_cluster_pins(type entity_type, cluster_distance NUMERIC)
+RETURNS TEXT
+AS $$
+WITH clusters AS (
+     SELECT unnest(st_clusterwithin(st_centroid(e.geometry), cluster_distance)) geom
+     FROM teet.entity e
+     WHERE NOT ST_IsEmpty(e.geometry) AND e.type = type
+)
+SELECT row_to_json(fc)::TEXT
+FROM (SELECT 'FeatureCollection' as type,
+             array_to_json(array_agg(f)) AS features
+      FROM (SELECT 'Feature' as type,
+                   ST_AsGeoJSON(st_centroid(c.geom))::json AS geometry,
+                   json_build_object('count', ST_NumGeometries(c.geom))
+           AS properties
+           FROM clusters c) f) fc;
+$$ LANGUAGE SQL STABLE SECURITY DEFINER;
+
 
 GRANT EXECUTE ON FUNCTION teet.geojson_entity_pins TO teet_user;
 
-GRANT EXECUTE ON FUNCTION teet.mvt_entities TO teet_user;
-GRANT EXECUTE ON FUNCTION teet.mvt_entities TO teet_backend;
-GRANT EXECUTE ON FUNCTION teet.geojson_entities TO teet_user;
-GRANT EXECUTE ON FUNCTION teet.geojson_entities TO teet_backend;
+GRANT EXECUTE ON FUNCTION teet.mvt_entities(BIGINT[], NUMERIC, NUMERIC, NUMERIC, NUMERIC) TO teet_user;
+GRANT EXECUTE ON FUNCTION teet.mvt_entities(type entity_type, NUMERIC, NUMERIC, NUMERIC, NUMERIC) TO teet_user;
+GRANT EXECUTE ON FUNCTION teet.geojson_entities(BIGINT[]) TO teet_user;
+GRANT EXECUTE ON FUNCTION teet.geojson_entities(type entity_type) TO teet_user;
+GRANT EXECUTE ON FUNCTION teet.geojson_entity_cluster_pins TO teet_user;
