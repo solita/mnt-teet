9ac82c68a66825b8232787749300d86fb8646ae0
jkorpelin <joni.korpelin@solita.fi>
2019-11-18 10:38:30 +0200

    fixed build.sh unescaped character

--- app/frontend/build.sh
+++ app/frontend/build.sh
@@ -7,4 +7,4 @@ branch=`git branch | grep "*" | cut -f2 -d' '`
 githash=`git rev-parse HEAD`
 buildtime=`date "+%d.%m.%Y %H:%M:%S"`
 
-echo "window.teet_branch = \"${branch}\"; window.teet_githash = \"${githash}\"; window.teet_buildtime = \"${buildtime};" > target/public/cljs-out/version-info.js
+echo "window.teet_branch = \"${branch}\"; window.teet_githash = \"${githash}\"; window.teet_buildtime = \"${buildtime}\";" > target/public/cljs-out/version-info.js
