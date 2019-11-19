0fcd7b4889e32041263601b63ae684f333521a0e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-11-19 08:35:45 +0200

    Create two test lifecycles when initializing a projet

--- app/backend/src/clj/teet/project/project_commands.clj
+++ app/backend/src/clj/teet/project/project_commands.clj
@@ -4,20 +4,31 @@
             [clojure.string :as str]))
 
 
-(defmethod db-api/command! :thk.project/initialize! [{conn :conn} {:thk.project/keys [id owner custom-name]}]
-  (let [project (d/pull (d/db conn) [:thk.project/estimated-start-date :thk.project/estimated-end-date]
-                        [:thk.project/id id])]
+(defmethod db-api/command! :thk.project/initialize!
+  [{conn :conn}
+   {:thk.project/keys [id owner custom-name]}]
+  (let [{:thk.project/keys [estimated-start-date estimated-end-date]}
+        (d/pull (d/db conn) [:thk.project/estimated-start-date :thk.project/estimated-end-date]
+                [:thk.project/id id])]
     (d/transact
      conn
      {:tx-data [(merge {:thk.project/id id
                         :thk.project/owner [:user/id (:user/id owner)]
 
-                        ;; FIXME: these should be received from THK, now create a design lifecycle
-                        ;; that has the same date range as project
-                        :thk.project/lifecycles [{:db/id "design-lifecycle"
-                                                  :thk.lifecycle/type [:db/ident :thk.lifecycle-type/design]
-                                                  :thk.lifecycle/estimated-start-date (:thk.project/estimated-start-date project)
-                                                  :thk.lifecycle/estimated-end-date (:thk.project/estimated-end-date project)}]}
+                        ;; FIXME: these should be received from THK, now create a design/construction
+                        ;; lifecycle that covers the project date range
+                        :thk.project/lifecycles
+                        (let [start-ms (.getTime estimated-start-date)
+                              end-ms (.getTime estimated-end-date)
+                              halfway-date (java.util.Date. (+ start-ms (/ (- end-ms start-ms) 2)))]
+                          [{:db/id "design-lifecycle"
+                            :thk.lifecycle/type [:db/ident :thk.lifecycle-type/design]
+                            :thk.lifecycle/estimated-start-date estimated-start-date
+                            :thk.lifecycle/estimated-end-date halfway-date}
+                           {:db/id "construction-lifecycle"
+                            :thk.lifecycle/type [:db/ident :thk.lifecycle-type/construction]
+                            :thk.lifecycle/estimated-start-date halfway-date
+                            :thk.lifecycle/estimated-end-date estimated-end-date}])}
                        (when-not (str/blank? custom-name)
                          {:thk.project/custom-name custom-name}))]}))
   :ok)
