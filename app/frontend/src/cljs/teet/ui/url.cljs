a98217c73ff591e5a28af3181e8b7d7836aa379e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-11-21 09:35:02 +0200

    Add hash parsing regex

--- app/frontend/src/cljs/teet/ui/url.cljs
+++ app/frontend/src/cljs/teet/ui/url.cljs
@@ -10,3 +10,22 @@
                          (map (fn [[param-name param-value]]
                                 (str (name param-name) "=" (js/encodeURIComponent (str param-value))))
                               (partition 2 param-names-and-values)))))
+
+(def path-and-params-pattern
+  "Regex pattern to split URL hash into path and parameters."
+  #"([^\?]+)(\?.+)?$")
+
+
+(def param-name-and-value-pattern
+  "Regex pattern to extract parameter names and values."
+  #"([\w-]+)=([^&]*)")
+
+(defn- parse-hash [hash]
+  (let [[_ path params-part] (re-matches path-and-params-pattern hash)
+        params (when params-part
+                 (re-seq param-name-and-value-pattern params-part))]
+    {:path path
+     :params (into {}
+                   (map (fn [[_ param-name param-value]]
+                          [(keyword param-name) param-value]))
+                   params)}))
