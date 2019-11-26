8c324d128ce2d5c569d2d5cfcb1b594f2d399344
alan.tammet <53471718+alnt-solita@users.noreply.github.com>
2019-11-26 15:43:50 +0200

    Parameter Store variables for FE CI script

--- ci/deploy/frontend.bash
+++ ci/deploy/frontend.bash
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
+TEET_DIR_BUILD=`aws ssm get-parameters --names "/teet/s3/builddir"  --query 'Parameters[*].Value' --output text`
+TEET_DIR_PUBLIC=`aws ssm get-parameters --names "/teet/s3/publicdir"  --query 'Parameters[*].Value' --output text`
+
 echo "Fetching frontend build: $COMMIT"
-aws s3 cp s3://teet-build/teet-frontend/frontend-$COMMIT.zip frontend.zip
+aws s3 cp s3://$TEET_DIR_BUILD/teet-frontend/frontend-$COMMIT.zip frontend.zip
 mkdir frontend
 cd frontend
 unzip ../frontend.zip
@@ -12,5 +15,5 @@ mv js/main.js js/main-$COMMIT.js
 sed -i -e "s/main.js/main-$COMMIT.js/g" index.html
 
 cd ..
-aws s3 rm s3://teet-dev-public --recursive
-aws s3 sync frontend s3://teet-dev-public --acl public-read
+aws s3 rm s3://$TEET_DIR_PUBLIC --recursive
+aws s3 sync frontend s3://$TEET_DIR_PUBLIC --acl public-read
