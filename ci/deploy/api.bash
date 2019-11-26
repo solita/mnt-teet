6036e943a327d30fbc011e974e8fd2f5bd06e28a
alan.tammet <53471718+alnt-solita@users.noreply.github.com>
2019-11-26 14:41:21 +0200

    Parameter Store variables from SSM

--- ci/deploy/api.bash
+++ ci/deploy/api.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-aws ecs update-service --cluster teet-dev --service teet-api-test --force-new-deployment
+TEET_CLUSTER_NAME=`aws ssm get-parameters --names "/teet/ecs/cluster_name"  --query 'Parameters[*].Value' --output text`
+TEET_SERVICE_NAME=`aws ssm get-parameters --names "/teet/ecs/service_name"  --query 'Parameters[*].Value' --output text`
+
+aws ecs update-service --cluster $TEET_CLUSTER_NAME --service $TEET_SERVICE_NAME --force-new-deployment
