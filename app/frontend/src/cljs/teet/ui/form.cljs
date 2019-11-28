a8e2afb59c3f4595b36cc08a620bda2aab521a89
jkorpelin <joni.korpelin@solita.fi>
2019-11-28 15:37:42 +0200

    Moved class to overlaying div so the grid component background doesn't bleed over the grid because -margins

--- app/frontend/src/cljs/teet/ui/form.cljs
+++ app/frontend/src/cljs/teet/ui/form.cljs
@@ -134,31 +134,31 @@
                     :display :flex
                     :flex-direction :column
                     :justify-content :space-between}}
-     [Grid {:container true
-            :class class
-            :spacing spacing}
-      (util/with-keys
-        (map (fn [field]
-               (assert (vector? field) "Field must be a hiccup vector")
-               (assert (map? (second field)) "First argument to field must be an options map")
-               (let [{:keys [xs lg md attribute adornment]} (meta field)
-                     value (get value attribute (default-value (first field)))
-                     opts {:value value
-                           :on-change (r/partial update-attribute-fn attribute)
-                           :label (tr [:fields attribute])
-                           :error (boolean (@invalid-attributes attribute))
-                           :required (boolean (required-fields attribute))}]
-                 [Grid (merge {:item true :xs (or xs 12)}
-                              (when lg
-                                {:lg lg})
-                              (when md
-                                {:md md}))
-                  (add-validation
-                   (update field 1 merge opts)
-                   validate-attribute-fn attribute value)
-                  (when adornment
-                    adornment)]))
-             fields))]
+     [:div {:class  class}
+      [Grid {:container true
+             :spacing   spacing}
+       (util/with-keys
+         (map (fn [field]
+                (assert (vector? field) "Field must be a hiccup vector")
+                (assert (map? (second field)) "First argument to field must be an options map")
+                (let [{:keys [xs lg md attribute adornment]} (meta field)
+                      value (get value attribute (default-value (first field)))
+                      opts {:value     value
+                            :on-change (r/partial update-attribute-fn attribute)
+                            :label     (tr [:fields attribute])
+                            :error     (boolean (@invalid-attributes attribute))
+                            :required  (boolean (required-fields attribute))}]
+                  [Grid (merge {:item true :xs (or xs 12)}
+                               (when lg
+                                 {:lg lg})
+                               (when md
+                                 {:md md}))
+                   (add-validation
+                     (update field 1 merge opts)
+                     validate-attribute-fn attribute value)
+                   (when adornment
+                     adornment)]))
+              fields))]]
      (when (or cancel-event save-event)
        [footer {:cancel (when cancel-event
                           (r/partial e! (cancel-event)))
