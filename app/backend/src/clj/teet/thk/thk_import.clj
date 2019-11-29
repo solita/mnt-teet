653351cdfb5b17658a1fe7ed546816e3b4d966a2
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-11-29 13:00:25 +0200

    Import based on new CSV format

--- app/backend/src/clj/teet/thk/thk_import.clj
+++ app/backend/src/clj/teet/thk/thk_import.clj
@@ -11,18 +11,13 @@
 (defn parse-thk-export-csv [input]
   (with-open [raw-input-stream (io/input-stream input)
               input-stream (BOMInputStream. raw-input-stream)]
-    (let [[headers _ignore-extra-header & rows]
+    (let [[headers & rows]
           (-> input-stream
               (io/reader :encoding "UTF-8")
               (csv/read-csv :separator \;))]
-      ;; There are multiple rows per project. One for each financial year.
-      ;; For now we only use the first one
-      ;; TODO: model financial years in datomic
-      (map first
-           (vals
-             (group-by
-               #(get % "PlanObject.Id")
-               (doall (map #(zipmap headers %) rows))))))))
+      ;; There are multiple rows per project. One for each project lifecycle phase.
+      (group-by #(get % "ob_id")
+                (map #(zipmap headers %) rows)))))
 
 (defn blank? [s]
   (or (not s)
@@ -54,31 +49,51 @@
   (when km
     (int (* 1000 km))))
 
-(defn project-datomic-attributes [csv-prj]
-  (without-nils {:thk.project/id                   (csv-prj "PlanObject.Id")
-                 :thk.project/road-nr              (->int (csv-prj "PlanObject.RoadNr"))
-                 :thk.project/bridge-nr            (->int (csv-prj "PlanObject.BridgeNr"))
+(defn project-datomic-attributes [[project-id lifecycle-phases]]
+  (let [prj (first lifecycle-phases)
+        phase-est-starts (map (comp ->date #(get % "est_start")) lifecycle-phases)
+        phase-est-ends (map (comp ->date #(get % "est_end")) lifecycle-phases)
 
-                 :thk.project/start-m              (km->m (->num (csv-prj "PlanObject.KmStart")))
-                 :thk.project/end-m                (km->m (->num (csv-prj "PlanObject.KmEnd")))
-                 :thk.project/carriageway          (->int (csv-prj "PlanObject.Carriageway"))
+        project-est-start (first (sort phase-est-starts))
+        project-est-end (last (sort phase-est-ends))]
+    (without-nils
+     {:db/id project-id
+      :thk.project/id project-id
+      :thk.project/road-nr              (->int (prj "roadnr"))
+      :thk.project/bridge-nr            (->int (prj "bridgenr"))
+      :thk.project/start-m              (km->m (->num (prj "kmstart")))
+      :thk.project/end-m                (km->m (->num (prj "kmend")))
+      :thk.project/carriageway          (->int (prj "carriageway"))
+      :thk.project/name                 (prj "objectname")
+      :thk.project/estimated-start-date project-est-start
+      :thk.project/estimated-end-date   project-est-end
 
-                 :thk.project/name                 (csv-prj "PlanObject.ObjectName")
+      :thk.project/lifecycles
+      (into []
+            (for [{:strs [est_start est_end phase]} lifecycle-phases
+                  ;; PENDING: should come from CSV field
+                  :let [id (str project-id "-" phase)]]
+              {:db/id id
+               :thk.lifecycle/id id
+               :thk.lifecycle/type
+               (case phase
+                 "ehitus" :thk.lifecycle-type/design
+                 "projekteerimine" :thk.lifecycle-type/construction)
+               :thk.lifecycle/estimated-start-date (->date est_start)
+               :thk.lifecycle/estimated-end-date (->date est_end)}))})))
 
-                 :thk.project/procurement-nr       (csv-prj "Procurement.ProcurementNo")
-                 :thk.project/procurement-id       (->int (csv-prj "Procurement.ID"))
-                 :thk.project/estimated-start-date (->date (csv-prj "Activity.EstStart"))
-                 :thk.project/estimated-end-date   (->date (csv-prj "Activity.EstEnd"))}))
+(defn teet-project? [[_ [p1 & _]]]
+  (and p1
+       (not (blank? (p1 "kmstart")))
+       (not (excluded-project-types (p1 "thk_grupp")))))
 
-(defn teet-project? [csv-prj]
-  (and (not (blank? (csv-prj "PlanObject.KmStart")))
-       (not (excluded-project-types (csv-prj "PlanObject.PlanGroupFK")))))
+(defn- thk-project-tx [url projects-csv]
+  (into [{:db/id "datomic.tx"
+          :integration/source-uri url}]
+        (for [prj projects-csv
+              :when (teet-project? prj)]
+          (project-datomic-attributes prj))))
 
 (defn import-thk-projects! [connection url projects]
-  (d/transact
-   connection
-   {:tx-data (into [{:db/id "datomic.tx"
-                     :integration/source-uri url}]
-                   (for [prj projects
-                         :when (teet-project? prj)]
-                     (project-datomic-attributes prj)))}))
+  (d/transact connection
+              {:tx-data (thk-project-tx url projects)}))
