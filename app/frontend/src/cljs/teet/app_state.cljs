7cb64151fdfad06ba816ab2f97f143ca83a3b811
jkorpelin <joni.korpelin@solita.fi>
2019-12-12 12:51:00 +0200

    road-buffer-meters default-value added to app state

--- app/frontend/src/cljs/teet/app_state.cljs
+++ app/frontend/src/cljs/teet/app_state.cljs
@@ -1,5 +1,7 @@
 (ns teet.app-state
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [teet.road.road-model :as road-model]))
 
-(defonce app (r/atom {:config {}
-                      :navigation {:open? true}}))
+(defonce app (r/atom {:config     {}
+                      :navigation {:open? true}
+                      :map        {:road-buffer-meters road-model/default-road-buffer-meters}}))
