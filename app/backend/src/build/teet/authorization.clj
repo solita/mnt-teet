0b76836e40c9a0c4669a9a66c7826f98adf0e94f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-12-18 15:37:41 +0200

    Add main entrypoint to process authz excel

--- app/backend/src/build/teet/authorization.clj
+++ app/backend/src/build/teet/authorization.clj
@@ -4,8 +4,9 @@
 
 (defn- keywordize [s]
   (-> s
+      str/trim
       str/lower-case
-      (str/replace #" " "-")
+      (str/replace #" +" "-")
       keyword))
 
 (defn- role->column-mapping [sheet]
@@ -34,22 +35,35 @@
         role->column (role->column-mapping sheet)]
 
     ;; Read all authorization rows (skip 4 header rows)
-    (for [row (drop 4 (sheet/row-seq sheet))
-          :when (not (str/blank? (sheet/read-cell (first (sheet/cell-seq row)))))
-          :let [[section functionality & _] (map sheet/read-cell
-                                                 (sheet/cell-seq row))
-                row-num (inc (.getRowNum row))]]
-      {:name (keyword (str/lower-case section)
-                      (name (keywordize functionality)))
-       :role-permissions (reduce-kv
-                          (fn [perms role column]
-                            (if-let [permission (->> sheet
-                                                     (sheet/select-cell (str column row-num))
-                                                     sheet/read-cell
-                                                     permission-type)]
-                              (assoc perms
-                                     role
-                                       permission)
-                              perms))
-                          {}
-                          role->column)})))
+    (into
+     {}
+     (for [row (drop 4 (sheet/row-seq sheet))
+           :when (not (str/blank? (sheet/read-cell (first (sheet/cell-seq row)))))
+           :let [[section functionality & _] (map sheet/read-cell
+                                                  (sheet/cell-seq row))
+                 row-num (inc (.getRowNum row))]]
+       [(keyword (-> section str/trim str/lower-case)
+                 (name (keywordize functionality)))
+        (reduce-kv
+         (fn [perms role column]
+           (if-let [permission (->> sheet
+                                    (sheet/select-cell (str column row-num))
+                                    sheet/read-cell
+                                    permission-type)]
+             (assoc perms
+                    role
+                    permission)
+             perms))
+         {}
+         role->column)]))))
+
+(defn- write-authorization-edn-from-sheet! [sheet-path]
+  (->> sheet-path
+       get-authorizations-from-sheet
+       (spit "resources/authorization.edn")))
+
+(defn -main [& [sheet-path]]
+  (if sheet-path
+    (do (write-authorization-edn-from-sheet! sheet-path)
+        (println "Authorization edn file successfully written!"))
+    (println "Please provide authorization sheet path as argument.")))
