cb5ac6f83df4d24a342eb4ad849fa41d7d33d206
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-12-19 10:58:22 +0200

    Add authorization info

--- app/frontend/build.sh
+++ app/frontend/build.sh
@@ -8,3 +8,6 @@ githash=`git rev-parse HEAD`
 buildtime=`date "+%d.%m.%Y %H:%M:%S"`
 
 echo "window.teet_branch = \"${branch}\"; window.teet_githash = \"${githash}\"; window.teet_buildtime = \"${buildtime}\";" > target/public/cljs-out/version-info.js
+
+AUTHZ=`cat ../backend/resources/authorization.edn`
+echo "window.teet_authz = \"${AUTHZ}\";" > target/public/cljs-out/authorization-info.js
