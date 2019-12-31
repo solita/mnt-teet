e17c3f1cd241d374832e887d1ccad90b476ec4b7
jkorpelin <joni.korpelin@solita.fi>
2019-12-31 13:24:50 +0200

    New style for checkboxlists checkbox

--- app/frontend/src/cljs/teet/theme/itemlist_styles.cljs
+++ app/frontend/src/cljs/teet/theme/itemlist_styles.cljs
@@ -1,21 +1,27 @@
-(ns teet.theme.itemlist-theme
+(ns teet.theme.itemlist-styles
   (:require [teet.theme.theme-colors :as theme-colors]))
 
 (defn heading
   [variant]
-  {:margin-bottom "1rem"
-   :display :flex
+  {:margin-bottom   "1rem"
+   :display         :flex
    :justify-content :space-between
-   :align-items :flex-end})
+   :align-items     :flex-end})
 
 (defn heading-action []
   {:float :right})
 
 (defn checkbox-list-contents
   []
-  {:padding "0.5rem 1rem"
-   ;:background-color theme-colors/gray-lightest
+  {:padding       "0.5rem 1rem"
    :border-bottom (str "1px solid " theme-colors/gray-lighter)})
+
+(defn checkbox-container
+  []
+  ^{:pseudo {:hover {:background-color theme-colors/gray-lightest}}}
+  {:transition "background-color 0.2s ease-in-out"
+   :padding-left "45px"
+   :width        "100%"})
 
 (defn checkbox-label
   [selected?]
