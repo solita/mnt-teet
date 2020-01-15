58a710907bf82b348540cf93e18b6be7e2aebef4
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-01-15 12:52:03 +0200

    Check branch from GH before starting build&deploy

--- ci/scripts/build-and-deploy.bash
+++ ci/scripts/build-and-deploy.bash
@@ -12,8 +12,14 @@ echo "ENV: $TEET_ENV"
 echo "Enter branch name to deploy:"
 read VERSION
 
-echo "Building $VERSION"
-exit;
+LATEST_COMMIT=`git ls-remote https://github.com/solita/mnt-teet | grep refs/heads/$VERSION | cut -f1`
+
+if [ -z "$LATEST_COMMIT" ]; then
+   echo "Unable to determine latest commit for branch $VERSION, exiting."
+   exit 1;
+fi
+
+echo "Building and deploying version $VERSION that has latest commit in GitHub $LATEST_COMMIT"
 
 AWS_DEFAULT_REGION=us-east-1 $DIR/codebuild teet-datomic-ion $VERSION
 $DIR/codebuild teet-frontend $VERSION
