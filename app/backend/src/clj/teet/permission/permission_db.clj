361a12377b6186cbfcb01e7d902dc7bc98bb5d08
jkorpelin <joni.korpelin@solita.fi>
2020-02-03 16:20:27 +0200

    db fns for working with permissions

---
+++ app/backend/src/clj/teet/permission/permission_db.clj
@@ -0,0 +1,43 @@
+(ns teet.permission.permission-db
+  "Datomic queries related to project user permissions"
+  (:require [datomic.client.api :as d])
+  (:import (java.util Date)))
+
+(defn valid-project-permissions
+  "User and project are either db/ids or lookup refs."
+  ([db project]
+   (valid-project-permissions db project (Date.)))
+  ([db project time]
+   (mapv first
+         (d/q '[:find (pull ?perm [:permission/role :db/id :meta/created-at
+                                   {:user/_permissions [:db/id :user/person-id :user/family-name :user/given-name :user/email]}])
+                :in $ ?project ?time
+                :where
+                [?perm :permission/projects ?project]
+                [?perm :permission/valid-from ?time-from]
+                [(get-else $ ?perm :permission/valid-until ?time) ?time-until]
+                [(<= ?time-from ?time)]
+                [(<= ?time ?time-until)]]
+              db
+              project
+              time))))
+
+(defn user-permission-for-project
+  "User and project are either db/ids or lookup refs."
+  ([db user project]
+   (user-permission-for-project db user project (Date.)))
+  ([db user project time]
+   (d/q '[:find (pull ?p [*])
+          :in $ ?user ?proj ?time
+          :where
+          [?user :user/permissions ?p]
+          [?p :permission/projects ?proj]
+          [?p :permission/valid-from ?time-from]
+          [(get-else $ ?p :permission/valid-until ?time) ?time-until]
+          [(<= ?time-from ?time)]
+          [(<= ?time ?time-until)]]
+        db
+        user
+        project
+        time)))
+
