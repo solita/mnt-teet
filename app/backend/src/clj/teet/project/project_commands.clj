9cb783be0b20ce09bf5dad8f07f214e6ed852b00
jkorpelin <joni.korpelin@solita.fi>
2020-02-07 12:29:21 +0200

    Added checks for valid activity names to activity commands

--- app/backend/src/clj/teet/project/project_commands.clj
+++ app/backend/src/clj/teet/project/project_commands.clj
@@ -23,37 +23,37 @@
                                    [:thk.project/owner :thk.project/estimated-start-date :thk.project/estimated-end-date]
                                    [:thk.project/id id])]
     (if (project-model/initialized? project-in-datomic)
-      (db-api/fail! {:error :project-already-initialized
-                     :msg (str "Project " id " is already initialized")
+      (db-api/fail! {:error  :project-already-initialized
+                     :msg    (str "Project " id " is already initialized")
                      :status 409})
       (let [{db :db-after}
             (d/transact
-             conn
-             {:tx-data [(merge {:thk.project/id id
-                                :thk.project/owner [:user/id (:user/id owner)]}
-                               (when-not (str/blank? project-name)
-                                 {:thk.project/project-name project-name})
-                               (when manager
-                                 {:thk.project/manager [:user/id (:user/id manager)]})
-                               (when custom-start-m
-                                 {:thk.project/custom-start-m custom-start-m})
-                               (when custom-end-m
-                                 {:thk.project/custom-end-m custom-end-m})
-                               (when m-range-change-reason
-                                 {:thk.project/m-range-change-reason m-range-change-reason})
-                               (when related-restrictions
-                                 {:thk.project/related-restrictions related-restrictions})
-                               (when related-cadastral-units
-                                 {:thk.project/related-cadastral-units related-cadastral-units})
-                               (modification-meta user))]})]
+              conn
+              {:tx-data [(merge {:thk.project/id    id
+                                 :thk.project/owner [:user/id (:user/id owner)]}
+                                (when-not (str/blank? project-name)
+                                  {:thk.project/project-name project-name})
+                                (when manager
+                                  {:thk.project/manager [:user/id (:user/id manager)]})
+                                (when custom-start-m
+                                  {:thk.project/custom-start-m custom-start-m})
+                                (when custom-end-m
+                                  {:thk.project/custom-end-m custom-end-m})
+                                (when m-range-change-reason
+                                  {:thk.project/m-range-change-reason m-range-change-reason})
+                                (when related-restrictions
+                                  {:thk.project/related-restrictions related-restrictions})
+                                (when related-cadastral-units
+                                  {:thk.project/related-cadastral-units related-cadastral-units})
+                                (modification-meta user))]})]
         (project-geometry/update-project-geometries!
-         (environment/config-map {:api-url [:api-url]
-                                  :api-shared-secret [:auth :jwt-secret]})
-         [(d/pull db '[:db/id :thk.project/name
-                       :thk.project/road-nr :thk.project/carriageway
-                       :thk.project/start-m :thk.project/end-m
-                       :thk.project/custom-start-m :thk.project/custom-end-m]
-                  [:thk.project/id id])]))))
+          (environment/config-map {:api-url           [:api-url]
+                                   :api-shared-secret [:auth :jwt-secret]})
+          [(d/pull db '[:db/id :thk.project/name
+                        :thk.project/road-nr :thk.project/carriageway
+                        :thk.project/start-m :thk.project/end-m
+                        :thk.project/custom-start-m :thk.project/custom-end-m]
+                   [:thk.project/id id])]))))
   :ok)
 
 (defmethod db-api/command! :project/skip-project-setup
@@ -114,7 +114,7 @@
     user :user}
    {permission-id :permission-id}]
   (d/transact conn
-              {:tx-data [(merge {:db/id               permission-id
+              {:tx-data [(merge {:db/id                  permission-id
                                  :permission/valid-until (Date.)}
                                 (modification-meta user))]})
   :ok)
@@ -132,37 +132,60 @@
           conn
           {:tx-data [{:db/id [:user/id id]
                       :user/permissions
-                             [(merge {:db/id               "new-permission"
-                                      :permission/role     :internal-consultant
-                                      :permission/projects project-id
+                             [(merge {:db/id                 "new-permission"
+                                      :permission/role       :internal-consultant
+                                      :permission/projects   project-id
                                       :permission/valid-from (Date.)}
                                      (creation-meta user))]}]})
         {:success "User added successfully"})
-      (throw (ex-info "User is already added"
-                      {:status 400
-                       :error :permission-already-granted})))))
+      (db-api/fail!
+        {:status 400
+         :msg "User is already added"
+         :error  :permission-already-granted}))))
+
+(defn valid-activity-name?
+  "Check if the activity name is valid for the lifecycle it's being added to"
+  [db {lifecycle-id  :lifecycle-id
+       activity-name :activity/name}]
+  (boolean
+    (seq
+      (d/q '[:find ?lc
+             :in $ ?lc ?act-enum
+             :where
+             [?act-enum :enum/valid-for ?v]
+             [?lc :thk.lifecycle/type ?v]]
+           db
+           lifecycle-id
+           activity-name))))
 
 (defmethod db-api/command! :project/create-activity [{conn :conn
-                                                      user :user} activity]
+                                                      user :user}
+                                                     activity]
   (log/info "ACTIVITY: " activity)
-  (select-keys
-    (d/transact
-      conn
-      {:tx-data [(merge
-                   {:db/id "new-activity"}
-                   (select-keys activity [:activity/name :activity/status
-                                          :activity/estimated-start-date
-                                          :activity/estimated-end-date])
-                   (creation-meta user))
-                 {:db/id                    (:lifecycle-id activity)
-                  :thk.lifecycle/activities ["new-activity"]}]})
-    [:tempids]))
+  (let [lifecycle-dbid (:lifecycle-id activity)]
+    (if (valid-activity-name? (d/db conn) activity)
+      (select-keys
+        (d/transact
+          conn
+          {:tx-data [(merge
+                       {:db/id "new-activity"}
+                       (select-keys activity [:activity/name :activity/status
+                                              :activity/estimated-start-date
+                                              :activity/estimated-end-date])
+                       (creation-meta user))
+                     {:db/id                    lifecycle-dbid
+                      :thk.lifecycle/activities ["new-activity"]}]})
+        [:tempids])
+      (db-api/fail! {:status 400
+                     :error  :bad-request}))))
 
 (defmethod db-api/command! :project/update-activity [{conn :conn
                                                       user :user} activity]
-
-  (select-keys (d/transact conn {:tx-data [(merge activity (modification-meta user))]})
-               [:tempids]))
+  (if (valid-activity-name? (d/db conn) activity)
+    (select-keys (d/transact conn {:tx-data [(merge activity (modification-meta user))]})
+                 [:tempids])
+    (db-api/fail! {:status 400
+                   :error  :bad-request})))
 
 (defmethod db-api/command! :project/update-task [{conn :conn
                                                   user :user} task]
