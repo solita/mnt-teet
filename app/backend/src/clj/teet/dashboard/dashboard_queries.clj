b8cbad13c54b7ffc4105c13a8497310cded1355f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-02-12 15:54:45 +0200

    Wrap without-deleted to tasks

--- app/backend/src/clj/teet/dashboard/dashboard_queries.clj
+++ app/backend/src/clj/teet/dashboard/dashboard_queries.clj
@@ -1,6 +1,8 @@
 (ns teet.dashboard.dashboard-queries
   (:require [teet.db-api.core :refer [defquery]]
-            [datomic.client.api :as d]))
+            [datomic.client.api :as d]
+            [teet.util.datomic :as du]
+            [teet.meta.meta-query :as meta-query]))
 
 (defquery :dashboard/user-dashboard
   {:doc "Get a dashboard for the current user"
@@ -8,23 +10,25 @@
    :args _
    :project-id nil
    :authorization {}}
-  {:tasks (mapv first
-                (d/q '[:find (pull ?e [:db/id
-                                       :task/name :task/description
-                                       :task/type
-                                       {:task/status [*]}
-                                       {:activity/_tasks
-                                        [:activity/name
-                                         :activity/estimated-start-date
-                                         :activity/estimated-end-date
-                                         :activity/actual-start-date
-                                         :activity/actual-end-date
-                                         {:thk.lifecycle/_activities
-                                          [:thk.lifecycle/type
-                                           :thk.lifecycle/estimated-start-date
-                                           :thk.lifecycle/estimated-end-date
-                                           {:thk.project/_lifecycles
-                                            [:thk.project/id :thk.project/name]}]}]}])
-                       :in $ ?user
-                       :where [?e :task/assignee ?user]]
-                     db [:user/id (:user/id user)]))})
+  {:tasks (meta-query/without-deleted
+           db
+           (mapv first
+                 (d/q '[:find (pull ?e [:db/id
+                                        :task/name :task/description
+                                        :task/type
+                                        {:task/status [*]}
+                                        {:activity/_tasks
+                                         [:activity/name
+                                          :activity/estimated-start-date
+                                          :activity/estimated-end-date
+                                          :activity/actual-start-date
+                                          :activity/actual-end-date
+                                          {:thk.lifecycle/_activities
+                                           [:thk.lifecycle/type
+                                            :thk.lifecycle/estimated-start-date
+                                            :thk.lifecycle/estimated-end-date
+                                            {:thk.project/_lifecycles
+                                             [:thk.project/id :thk.project/name]}]}]}])
+                        :in $ ?user
+                        :where [?e :task/assignee ?user]]
+                      db [:user/id (:user/id user)])))})
