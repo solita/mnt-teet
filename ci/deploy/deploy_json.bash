27634fef24bbf586569144a922320574b9a4f023
Ville Outamaa <ville.outamaa@solita.fi>
2020-02-13 21:18:04 +0200

    TEET-347 Wait for db to be up and running before setting status to deployed

--- ci/deploy/deploy_json.bash
+++ ci/deploy/deploy_json.bash
@@ -2,6 +2,8 @@
 set -eu
 
 PUBLICDIR=`aws ssm get-parameters --names "/teet/s3/publicdir" --query "Parameters[0].Value" | tr -d '"'`
+BASEURL=`aws ssm get-parameters --names "/teet/base-url" --query "Parameters[0].Value" | tr -d '"'`
+ENDPOINT="query/?q=%5B%22%5E%20%22%2C%22~%3Aquery%22%2C%22~%3Ateet.system%2Fdb%22%5D"
 
 if [ -z "$PUBLICDIR" ]
 then
@@ -13,6 +15,16 @@ if [ -z "$1" ]
 then
     echo "No status provided"
     exit 1;
+fi
+
+# If setting status to deployed, make sure db is up and running
+if [ $1 == "deployed"]
+then
+    echo "Waiting for DB"
+    until $(curl --output /dev/null --silent --head --fail "$BASEURL$ENDPOINT"); do
+        printf '.'
+        sleep 5
+    done
 fi
 
 echo "{\"commit\":\"$COMMIT\",\"status\":\"$1\",\"timestamp\":\"`date`\"}" > deploy.json
