b4fe1d59bd4d0e6fce91731a6f46b741e6dc0505
Ville Outamaa <ville.outamaa@solita.fi>
2020-02-18 18:30:17 +0200

    TEET-347 Deploy script refinements

--- app/backend/build.sh
+++ app/backend/build.sh
@@ -2,9 +2,9 @@
 
 set -eu
 
-#GIT_COMMIT=`git rev-parse HEAD`
+GIT_COMMIT=`git rev-parse HEAD`
 
-#echo "{:git-commit \"$GIT_COMMIT\"}" > resources/build-info.edn
+echo "{:git-commit \"$GIT_COMMIT\"}" > resources/build-info.edn
 
 #clojure -A:pack -m mach.pack.alpha.jib \
 #        --image-name $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG \
