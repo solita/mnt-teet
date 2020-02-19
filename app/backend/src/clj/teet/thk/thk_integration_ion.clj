5ed9e1bdf3af304e9da509e6acbb96bd434c2ea2
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-02-19 15:27:11 +0200

    Just return a JSON string

--- app/backend/src/clj/teet/thk/thk_integration_ion.clj
+++ app/backend/src/clj/teet/thk/thk_integration_ion.clj
@@ -160,6 +160,6 @@
            export-projects
            csv->file
            write-file-to-s3)
-    {:success true}
+    "{\"success\": true}"
     (catch Exception e
       (log/error "Error exporting projects CSV to S3: " (pr-str (ex-data e))))))
