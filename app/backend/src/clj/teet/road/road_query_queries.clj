cc0f32d3f8ec709f353e5aa7f943c1bae5634f9e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-02-19 15:23:02 +0200

    Add road geometry query with road info

--- app/backend/src/clj/teet/road/road_query_queries.clj
+++ app/backend/src/clj/teet/road/road_query_queries.clj
@@ -30,6 +30,17 @@
   (road-query/fetch-road-geometry (wfs-config)
                                   road carriageway start-m end-m))
 
+(defquery :road/geometry-with-road-info
+  {:doc "Fetch road geometry for road address, along with information about the road."
+   :context _
+   :args {:keys [road carriageway start-m end-m]}
+   :pre [(every? number? [road carriageway start-m end-m])]
+   :project-id nil
+   :authorization {}}
+  (let [config (wfs-config)]
+    {:geometry (road-query/fetch-road-geometry config road carriageway start-m end-m)
+     :road-info (road-query/fetch-road-info config road carriageway)}))
+
 (defquery :road/road-parts-for-coordinate
   {:doc "Fetch road parts for a clicked coordinate"
    :context _
