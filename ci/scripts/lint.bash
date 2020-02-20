7ea553a6b7b8b027868c4aec16d613339a6ac678
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-02-20 17:08:31 +0200

    Upload checks based on clj-kondo

--- ci/scripts/lint.bash
+++ ci/scripts/lint.bash
@@ -2,7 +2,51 @@
 
 export PATH="$PATH:/home/runner/work"
 
-clj-kondo --lint app/backend/src app/frontend/src
+IFS='
+'
+OUTPUT=`clj-kondo --lint app/backend/src app/frontend/src --config .clj-kondo/config.edn`
+
+SHA=`git rev-parse HEAD`
+
+ANNOTATIONS=''
+FIRST=1
+FAILURES=0
+WARNINGS=0
+
+for output in $OUTPUT
+do
+    if [[ $output =~ ^([^:]+):([0-9]+):([0-9]+):\ ([^:]+):\ (.*)$ ]]; then
+        FILE=${BASH_REMATCH[1]}
+        ROW=${BASH_REMATCH[2]}
+        COL=${BASH_REMATCH[3]}
+        TYPE=${BASH_REMATCH[4]}
+        MSG=${BASH_REMATCH[5]}
+        #echo "FILE: $FILE, TYPE: $TYPE, MSG: $MSG"
+        SEP=","
+        if [ $FIRST -eq 1 ]; then
+            SEP=""
+            FIRST=0
+        fi
+        if [ $TYPE == "error" ]; then
+            TYPE="failure"
+            FAILURES=$((FAILURES + 1))
+        fi
+        if [ $TYPE == "warning" ]; then
+            WARNINGS=$((WARNINGS + 1))
+        fi
+        ANNOTATIONS="$ANNOTATIONS$SEP{\"path\":\"$FILE\",\"start_line\":$ROW,\"end_line\":$ROW,\"annotation_level\":\"$TYPE\",\"message\":\"$MSG\"}"
+    fi
+done
+
+BODY="{\"name\": \"lint\", \"head_sha\": \"$SHA\", \"output\": {\"title\":\"Lint output\",\"summary\": \"$FAILURES failures, $WARNINGS warnings\", \"annotations\": [$ANNOTATIONS]}}"
+
+curl -H "Content-Type: application/json" \
+     -H "Accept: application/vnd.github.antiope-preview+json" \
+     -H "Authorization: Bearer $GITHUB_TOKEN" \
+     -X POST \
+     -d "$BODY" \
+     https://api.github.com/repos/$GITHUB_REPOSITORY/check-runs
+
 
 if [ $? -eq 2 ]; then
     exit 0;
