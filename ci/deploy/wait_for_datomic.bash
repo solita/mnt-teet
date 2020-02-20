b22ffb3ba80f159392a132b4efbfe6be5297bcd7
Ville Outamaa <ville.outamaa@solita.fi>
2020-02-20 11:04:55 +0200

    TEET-347 Add debug echo

--- ci/deploy/wait_for_datomic.bash
+++ ci/deploy/wait_for_datomic.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eu
 
+echo "In wait_for_datomic.bash";
+
 BASEURL=`aws ssm get-parameters --names "/teet/base-url" --query "Parameters[0].Value" | tr -d '"'`
 ENDPOINT="query/?q=%5B%22%5E%20%22%2C%22~%3Aquery%22%2C%22~%3Ateet.system%2Fdb%22%5D"
 
