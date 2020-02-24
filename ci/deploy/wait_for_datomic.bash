cf95ebafb5d61ac103c3c6a4cba370113206ce61
Ville Outamaa <ville.outamaa@solita.fi>
2020-02-24 11:06:26 +0200

    TEET-347 Rewrite wait_for_datomic

--- ci/deploy/wait_for_datomic.bash
+++ ci/deploy/wait_for_datomic.bash
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
-set -eu
-
-echo "In wait_for_datomic.bash";
+set -xeu
 
 BASEURL=`aws ssm get-parameters --names "/teet/base-url" --query "Parameters[0].Value" | tr -d '"'`
 ENDPOINT="query/?q=%5B%22%5E%20%22%2C%22~%3Aquery%22%2C%22~%3Ateet.system%2Fdb%22%5D"
 
 while true
 do
-    curl -s -L -w '\n%{http_code}' "$BASEURL$ENDPOINT" | {
-        read CURRENT_COMMIT
-        read STATUS
-    }
-    # TODO: Remove after testing
-    echo "COMMIT: $CODEBUILD_RESOLVED_SOURCE_VERSION"
+    read -r CURRENT_COMMIT STATUS < <(curl -s -L -w ' %{http_code}\n' "$BASEURL$ENDPOINT")
+
     echo "CURRENT_COMMIT: $CURRENT_COMMIT"
     echo "STATUS: $STATUS"
     if [ "$STATUS" == "200" ] && [ "$CODEBUILD_RESOLVED_SOURCE_VERSION" == "$CURRENT_COMMIT" ]; then
-        echo "Matching versions, deploy finished";
+        echo "Matching versions, deploy finished"
         break;
+    else
+        echo "Versions don't match, should wait"
     fi
     sleep 5
 done
