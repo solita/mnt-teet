88d686f27dd76a470d0450ce5db5b5ca4da49449
Ville Outamaa <ville.outamaa@solita.fi>
2020-02-24 11:26:29 +0200

    TEET-347 Remove excess logging

--- ci/deploy/wait_for_datomic.bash
+++ ci/deploy/wait_for_datomic.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -xeu
+set -eu
 
 BASEURL=`aws ssm get-parameters --names "/teet/base-url" --query "Parameters[0].Value" | tr -d '"'`
 ENDPOINT="query/?q=%5B%22%5E%20%22%2C%22~%3Aquery%22%2C%22~%3Ateet.system%2Fdb%22%5D"
