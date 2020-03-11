b90fdf3346ac64395d33e9b1e876a9ad897096b5
jkorpelin <joni.korpelin@solita.fi>
2020-03-11 10:43:57 +0200

    Adds THK project link to project view

--- app/frontend/src/cljs/teet/project/project_info.cljs
+++ app/frontend/src/cljs/teet/project/project_info.cljs
@@ -1,6 +1,7 @@
 (ns teet.project.project-info
   "UI components to show project info based on THK id."
-  (:require [postgrest-ui.components.item-view :as item-view]))
+  (:require [postgrest-ui.components.item-view :as item-view]
+            [clojure.string :as str]))
 
 (defn- get-name [{name "name"}]
   name)
@@ -13,3 +14,14 @@
     :select ["name"]
     :view get-name}
    project-id])
+
+(defn thk-url
+  "Checks the environment based on the hostname and returns THK url for a project"
+  [{:thk.project/keys [id]}]
+  (let [hostname (-> js/window
+                     .-location
+                     .-hostname)
+        test-or-dev? (or (str/includes? hostname "test-teet") (str/includes? hostname "dev-teet"))]
+    (if test-or-dev?
+      (str "http://mnt-test-thk.mts.local/web/#/planobject/" id)
+      (str "http://mnt-thk.mts.local/web/#/planobject/" id))))
