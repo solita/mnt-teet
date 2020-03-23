0d7c53c48e261c51d33db82a0ad3581abcfcb953
jkorpelin <joni.korpelin@solita.fi>
2020-03-23 13:13:10 +0200

    Adds new lazy-comments component using query

--- app/frontend/src/cljs/teet/comments/comments_view.cljs
+++ app/frontend/src/cljs/teet/comments/comments_view.cljs
@@ -6,6 +6,7 @@
             [herb.core :refer [<class]]
             [teet.localization :refer [tr]]
             [teet.ui.itemlist :as itemlist]
+            [teet.ui.query :as query]
             [teet.ui.format :as format]
             [teet.comments.comments-styles :as comments-styles]
             [teet.ui.typography :as typography]
@@ -21,6 +22,60 @@
                             :on-click validate}
     (tr [:comment :save])]])
 
+(defn comment-list
+  [e! _app comments _breacrumbs]
+  [itemlist/ItemList {}
+   (doall
+     (for [{id :db/id
+            :comment/keys [author comment timestamp] :as entity} comments]
+       ^{:key id}
+       [:div
+        [:div {:class [(<class common-styles/space-between-center) (<class common-styles/margin-bottom-1)]}
+         [:span
+          [typography/SectionHeading
+           {:style {:display :inline-block}}
+           [user-info/user-name author]]
+          [typography/GreyText {:style {:display :inline-block
+                                        :margin-left "1rem"}}
+           (format/date timestamp)]]
+         (when-authorized :comment-delete
+                          entity
+                          [buttons/delete-button-with-confirm {:small? true
+                                                               :action (e! comment-controller/->DeleteComment id)}
+                           (tr [:buttons :delete])])]
+        [typography/Paragraph comment]]))])
+
+(defn lazy-comments
+  [{:keys [e! app
+           entity-type
+           entity-id
+           new-comment
+           update-comment-event
+           save-comment-event]}]
+  (let [comments (get-in app [:comments-for-entity entity-id])]
+    (.log js/console "Comments in lazy: " (count comments))
+    [layout/section
+     [query/query {:e! e!
+                   :query :task/fetch-comments              ;; TODO case by entity-type
+                   :args {:db/id entity-id}
+                   :skeleton [:span "skeleton"]
+                   :state-path [:comments-for-entity entity-id]
+                   :state comments
+                   :view comment-list
+                   :refresh (count comments)}]
+     [form/form {:e! e!
+                 :value new-comment
+                 :on-change-event update-comment-event
+                 :save-event save-comment-event
+                 :footer new-comment-footer
+                 :spec :document/new-comment-form}
+      ^{:attribute :comment/comment}
+      [TextField {:rows 4
+                  :multiline true
+                  :InputLabelProps {:shrink true}
+                  :full-width true
+                  :placeholder (tr [:document :new-comment])}]]]))
+
 (defn comments [{:keys [e!
                         new-comment
                         comments
@@ -31,21 +86,7 @@
     [typography/Heading3 (tr [:document :comments])]
     [:span {:class (<class comments-styles/comment-amount)}
      (count comments)]]
-   [itemlist/ItemList {}
-    (doall
-      (for [{id            :db/id
-             :comment/keys [author comment timestamp] :as entity} comments]
-        ^{:key id}
-        [:div
-         [:div {:class (<class common-styles/space-between-center)}
-          [typography/GreyText
-           (format/date timestamp) " – " [user-info/user-name author]]
-          (when-authorized :comment-delete
-                           entity
-                           [buttons/delete-button-with-confirm {:small? true
-                                                                :action (e! comment-controller/->DeleteComment id)}
-                            (tr [:buttons :delete])])]
-         [typography/Paragraph comment]]))]
+   [comment-list e! nil comments nil]
 
    [form/form {:e!              e!
                :value           new-comment
