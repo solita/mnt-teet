c4139819be9c7e9ad1f8e1b3c074cca2b8343134
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-03-24 12:44:17 +0200

    File listing improvements, open file by clicking row

--- app/frontend/src/cljs/teet/file/file_view.cljs
+++ app/frontend/src/cljs/teet/file/file_view.cljs
@@ -1,10 +1,12 @@
 (ns teet.file.file-view
-  (:require [teet.project.project-navigator-view :as project-navigator-view]))
+  (:require [teet.project.project-navigator-view :as project-navigator-view]
+            [teet.project.project-model :as project-model]))
 
-(defn file-page [e! app {file :file :as project} breadcrumbs]
-  [project-navigator-view/project-navigator-with-content
-   {:e! e!
-    :app app
-    :project project
-    :breadcrumbs breadcrumbs}
-   [:div "filen sivu " (pr-str file)]])
+(defn file-page [e! {{file-id :file} :params :as app} project breadcrumbs]
+  (let [file (project-model/file-by-id project file-id)]
+    [project-navigator-view/project-navigator-with-content
+     {:e! e!
+      :app app
+      :project project
+      :breadcrumbs breadcrumbs}
+     [:div "filen sivu " (pr-str file)]]))
