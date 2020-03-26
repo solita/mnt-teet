7445644b3f8a7dddbcb40822555e2721b471b5fd
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-03-26 14:23:18 +0200

    Query all files for task

---
+++ app/backend/src/clj/teet/task/task_db.clj
@@ -0,0 +1,30 @@
+(ns teet.task.task-db
+  "Task related queries"
+  (:require [datomic.client.api :as d]))
+
+
+(defn files-for-task
+  "Returns files for a given task. Returns latest versions of files as vector
+  and adds all previous versions of a files as :versions key."
+  [db task-eid]
+  (let [files (mapv first
+                    (d/q '[:find (pull ?f [* {:file/previous-version [:db/id]}])
+                           :where [?t :task/files ?f]
+                           :in $ ?t] db task-eid))
+        ;; Group files to first versions and next versions
+        {next-versions true
+         first-versions false}
+        (group-by (comp boolean :file/previous-version) files)
+
+        ;; Find next version for given file
+        next-version (fn [file]
+                       (some #(when (= (:db/id file)
+                                       (get-in % [:file/previous-version :db/id])) %)
+                             next-versions))]
+    (vec
+     (for [f first-versions
+           :let [[latest-version & previous-versions]
+                 (reverse
+                  (take-while some? (iterate next-version f)))]]
+       (assoc latest-version
+              :versions previous-versions)))))
