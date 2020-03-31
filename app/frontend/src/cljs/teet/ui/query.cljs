3fb6dc973f2dd2ae4e93797eabe63de2c351120d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-03-31 09:47:01 +0300

    Refetch if args change

--- app/frontend/src/cljs/teet/ui/query.cljs
+++ app/frontend/src/cljs/teet/ui/query.cljs
@@ -5,7 +5,8 @@
             [tuck.core :as t]
             [teet.ui.material-ui :refer [CircularProgress]]
             [teet.common.common-controller :as common-controller]
-            [teet.common.common-styles :as common-styles]))
+            [teet.common.common-styles :as common-styles]
+            [teet.log :as log]))
 
 (defrecord Query [query args state-path])
 (defrecord QueryResult [state-path result])
@@ -33,25 +34,28 @@
 (defn query
   "Component that does a datomic query and shows view with the resulting data."
   [{:keys [e! query args state-path refresh]}]
-  (let [refresh-value (atom refresh)]
+  (let [refresh-value (atom refresh)
+        previous-args (atom args)]
     (e! (->Query query args state-path))
     (r/create-class
-      {:component-will-unmount (e! ->Cleanup state-path)
-       :reagent-render
-       (fn [{:keys [e! query args state-path skeleton view app state refresh breadcrumbs]}]
-         (when (not= @refresh-value refresh)
-           (reset! refresh-value refresh)
-           (e! (->Query query args state-path)))
-         (if state
-           ;; Results loaded, call the view
-           ^{:key "query-result-view"}
-           [view e! app state breadcrumbs]
+     {:component-will-unmount (e! ->Cleanup state-path)
+      :reagent-render
+      (fn [{:keys [e! query args state-path skeleton view app state refresh breadcrumbs]}]
+        (when (or (not= @refresh-value refresh)
+                  (not= @previous-args args))
+          (reset! refresh-value refresh)
+          (reset! previous-args args)
+          (e! (->Query query args state-path)))
+        (if state
+          ;; Results loaded, call the view
+          ^{:key "query-result-view"}
+          [view e! app state breadcrumbs]
 
-           ;; Results not loaded, show skeleton or loading spinner
-           (if skeleton
-             skeleton
-             [:div {:class (<class common-styles/spinner-style)}
-              [CircularProgress]])))})))
+          ;; Results not loaded, show skeleton or loading spinner
+          (if skeleton
+            skeleton
+            [:div {:class (<class common-styles/spinner-style)}
+             [CircularProgress]])))})))
 
 (defn rpc
   "Component that does an PostgREST RPC call and shows view with the resulting data."
