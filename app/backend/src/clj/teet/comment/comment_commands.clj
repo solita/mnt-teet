44945ac6c3206eac9064be25a88350200541368d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-04-06 09:26:23 +0300

    Support attached files in comments

--- app/backend/src/clj/teet/comment/comment_commands.clj
+++ app/backend/src/clj/teet/comment/comment_commands.clj
@@ -7,11 +7,30 @@
             [teet.project.project-db :as project-db])
   (:import (java.util Date)))
 
+(defn- validate-files
+  "Validate comment attachments. They must be created
+  by the same user and they must be images."
+  [db user files]
+  (let [user-created-files
+        (into #{}
+              (map first)
+              (d/q '[:find ?c
+                     :where
+                     [?c :meta/creator ?user]
+                     [?c :file/type ?type]
+                     [(.startsWith ^String ?type "image/")]
+                     :in $ $user [?c ...]]
+                   db
+                   [:user/id (:user/id user)]
+                   files))]
+    (if-not (every? user-created-files files)
+      (db-api/bad-request! "No such file")
+      files)))
 
 (defcommand :comment/create
   {:doc "Create a new comment and add it to an entity"
    :context {:keys [db user]}
-   :payload {:keys [entity-id entity-type comment]}
+   :payload {:keys [entity-id entity-type comment files]}
    :project-id (project-db/entity-project-id db entity-type entity-id)
    :authorization {:task/comment-task {:db/id entity-id}}
    :transact [(merge {:db/id entity-id
@@ -20,7 +39,9 @@
                                :comment/author [:user/id (:user/id user)]
                                :comment/comment comment
                                :comment/timestamp (Date.)}
-                              (creation-meta user))]})]})
+                              (creation-meta user)
+                              (when (seq files)
+                                {:comment/files (validate-files db user files)}))]})]})
 
 (defn- comment-parent-entity [db comment-id]
   (if-let [doc-id (ffirst
