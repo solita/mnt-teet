769b01930fd765c81dd78b65daeec012ab5fe9e2
Erno Kuusela <erno-laptop!erno>
2020-04-06 16:24:45 +0300

    debug cleanup from x-road code (some left in)

--- app/backend/src/clj/teet/integration/x_road.clj
+++ app/backend/src/clj/teet/integration/x_road.clj
@@ -4,10 +4,7 @@
             [clojure.zip]
             [hiccup.core :as hiccup]
             [org.httpkit.client :as htclient]
-            [taoensso.timbre :as log]
-            ;; [clj-http.client :as client]
-            ;; [clj-soap.client :as soap]
-            ))
+            [taoensso.timbre :as log]))
 
 ;; see also https://x-tee.ee/catalogue-data/ee-dev/ee-dev/GOV/70008440/rr/155.wsdl
 
@@ -42,8 +39,8 @@
 
 (defn rr442-parse-name [xml-string]
   (let [xml (xml/parse xml-string)
-        _ (assert (some? xml))
-        _ (def *xx xml)
+        ;; _ (assert (some? xml))
+        ;; _ (def *xx xml)
         zipped-xml (clojure.zip/xml-zip xml)
         avaldaja (z/xml1-> zipped-xml :SOAP-ENV:Envelope :SOAP-ENV:Body :prod:RR442Response :response :Avaldaja)
         fields [:Eesnimi :Perenimi :Isikukood]
@@ -112,9 +109,7 @@
    :katastritunnus (z/xml1-> k-xml :a:KinnistuKatastriyksus :a:katastritunnus z/text)
    :katastriyksuse_aadress (z/xml1-> k-xml :a:KinnistuKatastriyksus :a:katastriyksuse_aadress z/text)
    :pindala (z/xml1-> k-xml :a:KinnistuKatastriyksus :a:pindala z/text)
-   :pindala_yhik (z/xml1-> k-xml :a:KinnistuKatastriyksus :a:pindala_yhik z/text)
-
-   })
+   :pindala_yhik (z/xml1-> k-xml :a:KinnistuKatastriyksus :a:pindala_yhik z/text)})
 
 (defn d-cadastral-units [kdr-xml]
   ;;; kdr = Kinnistu_DetailamdResponse from inside the soap body
@@ -124,13 +119,11 @@
 (defn d-property-owners [kdr-xml]
   ;;; kdr = Kinnistu_DetailamdResponse from inside the soap body
   (let [owner-xml-seq (z/xml-> kdr-xml :jagu2 :a:Jagu2 :a:omandiosad :a:Jagu2.Omandiosa)
-        _ (def *ox owner-xml-seq)
+        ;; _ (def *ox owner-xml-seq)
         oo-get (fn oo-get [ox & path]
                   (apply z/xml1-> (concat [ox] path)))
         isikud-get (fn [ox & path] (apply oo-get ox (concat [:a:isikud] path [z/text])))
-        omandi-get (fn [ox & path] (apply oo-get ox (concat path [z/text])))
-        ;; j2-isikud-get (fn [& path] (apply j2-get (concat [:a:omandiosad :a:Jagu2.Omandiosa :a:isikud] path)))
-        ]
+        omandi-get (fn [ox & path] (apply oo-get ox (concat path [z/text])))]
     {:omandiosad
      (mapv (fn [ox]
              {:isiku_tyyp (isikud-get ox :a:KinnistuIsik :a:isiku_tyyp)
@@ -144,16 +137,14 @@
               :omandiosa_lugeja (omandi-get ox :a:omandiosa_lugeja)
               :omandiosa_nimetaja (omandi-get ox :a:omandiosa_nimetaja)
               :omandiosa_suurus (omandi-get ox :a:omandiosa_suurus)})
-           owner-xml-seq)}) 
-  
-  )
+           owner-xml-seq)}))
 
 
 (defn kinnistu-d-parse-response [xml-string]
   (let [xml (xml/parse (clojure.java.io/input-stream (.getBytes xml-string)))
         zipped-xml (clojure.zip/xml-zip xml)
         d-response (z/xml1-> zipped-xml :s:Envelope :s:Body :Kinnistu_DetailandmedResponse)
-        _ (def *x d-response)
+        ;; _ (def *x d-response)
         ]
     (merge {}
            (d-cadastral-units d-response)
@@ -170,26 +161,6 @@
         body
         (get xml-match 1)))))
 
-#_(defn unpeel-multipart [ht-resp]
-  (let [c-type (:content-type (:headers ht-resp))
-        rx-match (re-find #"multipart/related;.*boundary=\"([^\"]+)\"" c-type)]
-    (if-not rx-match
-      :kek ;; ht-resp
-      ;; else
-      (let [boundary (str "--" (get rx-match 1))
-            b-len (count boundary)
-            body (:body ht-resp)
-            
-            i1 (clojure.string/index-of body boundary)
-            i2 (when (integer? i1)
-                 (clojure.string/index-of body boundary (+ i1 (count boundary))))
-            ;; _ (println i1 i2)
-            part1-header+body (subs body i1 i2)
-            p1-body-offset (clojure.string/index-of part1-header+body "\n\n")
-            ]
-        (subs body i1 i2))
-      )))
-
 (defn perform-kinnistu-d-request [url instance-id reg-nr]
   (let [req (kr-kinnistu-d-request-xml reg-nr instance-id)
         resp-atom (htclient/post url {:body req
@@ -197,8 +168,8 @@
                                       :headers {"Content-Type" "text/xml; charset=UTF-8"}})
         resp (deref resp-atom)]
     ;; (println "req was" req)
-    (def *rq req)
-    (def *rr resp)
+    ;; (def *rq req)
+    ;; (def *rr resp)
     (if (= 200 (:status resp))
       {:status :ok
        :result (kinnistu-d-parse-response (unpeel-multipart resp))
@@ -207,7 +178,7 @@
       {:status :error
        :result resp})))
 
-;; repl
+;; repl notes:
 
 ;; test land reg ids: 233102, 284502, 308104 (last one has empty jag3/jag4)
 ;; (def *r  (perform-kinnistu-d-request "http://localhost:12073" "ee-dev" "308104"))
