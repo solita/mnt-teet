3f12cdc5cd48a2d74a6208eb676808bc89c2e62b
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-04-07 08:43:42 +0300

    Add login timeout

--- ci/browser-tests/cypress/integration/login.spec.js
+++ ci/browser-tests/cypress/integration/login.spec.js
@@ -2,15 +2,21 @@ context('Login', () => {
     beforeEach(() => {
         // do any before setup here
         cy.visit("https://dev-teet.solitacloud.fi/")
+
+
     })
 
     it("dummy login as benjamin boss should work", () => {
 
+        cy.server()
+        cy.route('POST', 'command*').as('command')
+
         cy.get("#password-textfield").type(Cypress.env("SITE_PASSWORD"))
         cy.get("button").contains("Login as Boss").click()
+        //cy.wait('@command')
 
         // User should be taken to map page after login
-        cy.title().should('eq','Kaart')
+        cy.title({timeout: 15000}).should('eq','Kaart')
     })
 
 })
