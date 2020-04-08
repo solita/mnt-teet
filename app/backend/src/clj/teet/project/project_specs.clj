e51959611cb2c70761fa9fa8cc93f9197d75ab33
jkorpelin <joni.korpelin@solita.fi>
2020-04-08 16:41:06 +0300

    Moves project specs from clj to cljc

--- app/backend/src/clj/teet/project/project_specs.clj
+++
@@ -1,8 +0,0 @@
-(ns teet.project.project-specs
-  (:require [clojure.spec.alpha :as s]))
-
-(s/def :thk.project/id string?)
-
-(s/def :thk.project/skip-setup (s/keys :req [:thk.project/id]))
-
-(s/def :thk.project/continue-setup (s/keys :req [:thk.project/id]))
