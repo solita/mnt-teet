dfff170980be0fad829adb43c074ee022547038a
Ville Outamaa <ville.outamaa@solita.fi>
2020-04-14 12:03:13 +0300

    TEET-372 Filter by visibility in the query

--- app/backend/src/clj/teet/comment/comment_queries.clj
+++ app/backend/src/clj/teet/comment/comment_queries.clj
@@ -3,14 +3,17 @@
             [datomic.client.api :as d]
             [teet.authorization.authorization-check :as ac]
             [teet.meta.meta-query :as meta-query]
-            [teet.project.project-db :as project-db]
-            [teet.comment.comment-model :as comment-model]))
+            [teet.project.project-db :as project-db]))
 
-(defn- filter-by-visibility [can-see-internal-comments? comments]
-
-  (cond->> comments
-    (not can-see-internal-comments?) (filter #(= (-> % :comment/visibility :db/ident)
-                                                 :comment.visibility/all))))
+(defn comment-query [type visibility]
+  (let [type-attribute (-> type name (str "/comments") keyword)]
+    {:find '[(pull ?comment [*
+                             {:comment/author [*]
+                              :comment/files [:db/id :file/name]}])]
+     :in '[$ ?entity-id]
+     :where (into [['?entity-id type-attribute '?comment]]
+                  (when visibility
+                    [['?comment :comment/visibility visibility]]))}))
 
 (defquery :comment/fetch-comments
   {:doc "Fetch comments for any :db/id and entity type. Returns comments newest first."
@@ -18,27 +21,16 @@
    :args {id :db/id entity-type :for}
    :project-id (project-db/entity-project-id db entity-type id)
    :authorization {:land/read-comments {:db/id id}}}
-  (let [entity-comments (->> (if (= entity-type :task)
-                               (d/q '[:find (pull ?comment [*
-                                                            {:comment/author [*]
-                                                             :comment/files [:db/id :file/name]}])
-                                      :in $ ?entity-id
-                                      :where [?entity-id :task/comments ?comment]]
-                                    db id)
-                               (d/q '[:find (pull ?comment [*
-                                                            {:comment/author [*]
-                                                             :comment/files [:db/id :file/name]}])
-                                      :in $ ?entity-id
-                                      :where [?entity-id :file/comments ?comment]]
-                                    db id))
-                             (map first)
-                             (remove nil?))]
+  (let [visibility (when-not (ac/authorized? user
+                                             :project/view-internal-comments)
+                     :comment.visibility/all)
+        entity-comments (->> (d/q (comment-query entity-type visibility)
+                                  db id)
+                             (map first))]
     (if (empty? entity-comments)
       []
       (->> entity-comments
            (meta-query/without-deleted db)
-           (filter-by-visibility (ac/authorized? user
-                                                 :project/view-internal-comments))
            (sort-by :comment/timestamp)
            reverse
            vec))))
