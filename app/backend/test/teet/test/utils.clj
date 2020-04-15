6d787bb3fc8eae7c648e7a96b5a3c86ff3fd4e11
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-04-15 17:01:18 +0300

    Move mock users to test utils

--- app/backend/test/teet/test/utils.clj
+++ app/backend/test/teet/test/utils.clj
@@ -5,9 +5,59 @@
             [teet.log :as log]
             [teet.user.user-db :as user-db]))
 
+(def mock-users
+  [{:user/id #uuid "4c8ec140-4bd8-403b-866f-d2d5db9bdf74"
+    :user/person-id "1234567890"
+    :user/given-name "Danny D."
+    :user/family-name "Manager"
+    :user/email "danny.d.manager@example.com"
+                                        ; :user/organization "Maanteeamet"
+    }
+
+   {:user/id #uuid "ccbedb7b-ab30-405c-b389-292cdfe85271"
+    :user/person-id "3344556677"
+    :user/given-name "Carla"
+    :user/family-name "Consultant"
+    :user/email "carla.consultant@example.com"
+                                        ; :user/organization "ACME Road Consulting, Ltd."
+    }
+
+   {:user/id #uuid "fa8af5b7-df45-41ba-93d0-603c543c880d"
+    :user/person-id "9483726473"
+    :user/given-name "Benjamin"
+    :user/family-name "Boss"
+    :user/email "benjamin.boss@example.com"
+                                        ; :user/organization "Maanteeamet"
+    }
+
+   {:user/id #uuid "008af5b7-0f45-01ba-03d0-003c111c8f00"
+    :user/person-id "1233726123"
+    :user/given-name "Edna E."
+    :user/family-name "Consultant"
+    :user/email "edna.e.consultant@example.com"
+                                        ; :user/organization "Maanteeamet"
+    }])
+
 ;;
 ;; Test fixtures
 ;;
+
+(def ^:dynamic *connection* "Datomic connection during tests" nil)
+
+(defn connection
+  "Returns the current connection. Can only be called within tests using with-db fixture."
+  []
+  *connection*)
+
+(defn db
+  "Returns the current database. Can only be called within tests using with-db fixture."
+  []
+  (d/db (connection)))
+
+(defn tx
+  "Transact the given tx-data maps. Can only be called within tests using with-db fixture."
+  [& tx-data]
+  (d/transact (connection) {:tx-data (vec tx-data)}))
 
 (defn with-environment [f]
   (log/info "Loading local config.")
@@ -21,10 +71,15 @@
   (let [test-db-name (str "test-db-" (System/currentTimeMillis))]
     (log/info "Creating database " test-db-name)
     (swap! environment/config assoc-in [:datomic :db-name] test-db-name)
-    (d/create-database (d/client (environment/config-value :datomic :client)) {:db-name test-db-name})
-    (f)
-    (log/info "Deleting database " test-db-name)
-    (d/delete-database (d/client (environment/config-value :datomic :client)) {:db-name test-db-name})))
+    (let [client (d/client (environment/config-value :datomic :client))
+          db-name {:db-name test-db-name}]
+      (d/create-database client db-name)
+      (binding [*connection* (d/connect client db-name)]
+        (environment/migrate *connection*)
+        (d/transact *connection* {:tx-data mock-users})
+        (f))
+      (log/info "Deleting database " test-db-name)
+      (d/delete-database client db-name))))
 
 (defn with-test-data [f]
   ;; TODO: Initialize test db with some test data
@@ -49,7 +104,7 @@
   @logged-in-user-id)
 
 (defn local-login
-  [user-id]   
+  [user-id]
   (reset! logged-in-user-id user-id)
   (log/info "Locally logged in as " user-id))
 
