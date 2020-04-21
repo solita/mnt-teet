15ce473bbb141659699ee0fd0be02fdb1090d291
Ville Outamaa <ville.outamaa@solita.fi>
2020-04-21 07:53:05 +0300

    TEET-451 Test that external consultants can't track comment status

--- app/backend/test/teet/comment_commands_test.clj
+++ app/backend/test/teet/comment_commands_test.clj
@@ -5,13 +5,13 @@
             [teet.test.utils :as tu]
             [teet.util.collection :as cu]))
 
-(use-fixtures :once tu/with-environment (tu/with-db))
-(use-fixtures :each tu/with-global-data)
+(use-fixtures :each tu/with-environment (tu/with-db) tu/with-global-data)
 
 
 (deftest commenting-tasks-requires-authorization
   (let [task-id (tu/create-task {:user tu/mock-user-boss :activity (tu/->db-id "p1-lc1-act1")} :task-id)]
     (is (some? task-id)))
+
   (testing "Boss can comment the task they created"
     (tu/create-comment {:user tu/mock-user-boss
                         :entity-type :task
@@ -58,4 +58,28 @@
                                                 :for :task})
                                (cu/find-by-id (tu/get-data :tracked-comment-id)))]
       (is (= (-> created-comment :comment/status :db/ident)
-             :comment.status/unresolved)))))
+             :comment.status/unresolved))))
+
+  (testing "External consultant cannot track the comment status"
+    (tu/local-command tu/mock-user-boss
+                      :thk.project/add-permission
+                      {:project-id (tu/->db-id "p1")
+                       :user {:user/id tu/external-consultant-id}
+                       :role :external-consultant})
+
+    ;; Edna creates comment with tracking on...
+    (tu/create-comment {:user tu/mock-user-carla-consultant
+                        :entity-type :task
+                        :entity-id (tu/get-data :task-id)
+                        :comment {:comment/comment "Consultant comment"
+                                  ;; ... as can be seen here ...
+                                  :track? true}}
+                       :ednas-comment-id)
+
+    ;; ... but the comment status is untracked
+    (let [created-comment (->> (tu/local-query tu/mock-user-boss :comment/fetch-comments
+                                               {:db/id (tu/get-data :task-id)
+                                                :for :task})
+                               (cu/find-by-id (tu/get-data :ednas-comment-id)))]
+      (is (= :comment.status/untracked
+             (-> created-comment :comment/status :db/ident))))))
