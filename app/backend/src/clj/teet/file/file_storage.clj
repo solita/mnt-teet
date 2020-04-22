500e23905b492bdf124206cc9cbec35c816b83ec
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-04-22 11:49:38 +0300

    Filename metadata handling

--- app/backend/src/clj/teet/file/file_storage.clj
+++ app/backend/src/clj/teet/file/file_storage.clj
@@ -2,7 +2,10 @@
   "Access to S3 bucket for storing files. Access must be checked in previous layers."
   (:require [amazonica.aws.s3 :as s3]
             [teet.environment :as environment])
-  (:import (java.util Date)))
+  (:import (java.util Date)
+           (com.amazonaws.services.s3.model GeneratePresignedUrlRequest
+                                            ResponseHeaderOverrides)
+           (com.amazonaws HttpMethod)))
 
 (def ^:const url-expiration-ms 300000)
 
@@ -12,15 +15,21 @@
 (defn- storage-bucket []
   (environment/config-value :document-storage :bucket-name))
 
-(defn- presigned-url [method file-name]
-  (str (s3/generate-presigned-url
-        {:bucket-name (storage-bucket)
-         :key file-name
-         :method method
-         :expiration (expiration-date)})))
+(defn- presigned-url [method content-disposition file-name]
+  (let [request (-> (GeneratePresignedUrlRequest. (storage-bucket)
+                                                  file-name
+                                                  (HttpMethod/valueOf method))
+                    (.withExpiration (expiration-date)))
+        request (if content-disposition
+                  (.withResponseHeaders
+                   request
+                   (.withContentDisposition (ResponseHeaderOverrides.)
+                                            content-disposition))
+                  request)]
+    (str (s3/generate-presigned-url request))))
 
 (def download-url (partial presigned-url "GET"))
-(def upload-url (partial presigned-url "PUT"))
+(def upload-url (partial presigned-url "PUT" nil))
 
 (defn document-s3-ref
   "Returns an integration S3 file descriptor for a document."
