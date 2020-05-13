b2274e0e3de3364c937270a737884c7a047c765c
jkorpelin <joni.korpelin@solita.fi>
2020-05-13 13:23:57 +0300

    Adds land-commands for saving new estate-compensations

--- app/backend/src/clj/teet/land/land_commands.clj
+++ app/backend/src/clj/teet/land/land_commands.clj
@@ -2,7 +2,9 @@
   (:require [teet.db-api.core :as db-api :refer [defcommand]]
             [datomic.client.api :as d]
             [teet.meta.meta-model :as meta-model]
-            [teet.util.collection :as cu]))
+            [teet.util.collection :as cu]
+            [teet.util.spec :as su]
+            teet.land.land-specs))
 
 
 (defcommand :land/create-land-acquisition
@@ -58,15 +60,75 @@
               :land-acquisition/pos-number pos-number}
              (meta-model/modification-meta user)))]})
 
+(defn new-compensations-tx
+  [compensations]
+  (into []
+        (map-indexed
+          (fn [i compensation]
+            (-> (select-keys compensation (su/keys-of :estate-procedure/compensation))
+                (merge {:db/id (str "new-third-party-comp-" i)})
+                (cu/update-in-if-exists [:estate-compensation/amount] bigdec))))
+        compensations))
 
-(defcommand :land/create-estate-compensation
+(defn new-land-exchange-txs
+  [land-exchanges]
+  (into []
+        (map-indexed
+          (fn [i exchange]
+            (-> (select-keys exchange (su/keys-of :estate-procedure/land-exchange))
+                (merge {:db/id (str "new-exchange-" i)})
+                (cu/update-in-if-exists [:land-exchange/area] bigdec)
+                (cu/update-in-if-exists [:land-exchange/price-per-sqm] bigdec))))
+        land-exchanges))
+
+(def common-procedure-updates
+  [[:estate-procedure/pos #(Integer/parseInt %)]
+   [:estate-procedure/compensations new-compensations-tx]
+   [:estate-procedure/third-party-compensations new-compensations-tx]
+   [:estate-procedure/land-exchanges new-land-exchange-txs]])
+
+(def common-procedure-keys
+  [:estate-procedure/pos :estate-procedure/type :estate-procedure/estate-id])
+
+(def procedure-type-options
+  {:estate-procedure.type/urgent {:keys
+                                  [:estate-procedure/urgent-bonus
+                                   :estate-procedure/motivation-bonus :estate-procedure/third-party-compensations]}
+   :estate-procedure.type/acquisition-negotiation {:keys [:estate-procedure/pos :estate-procedure/compensations
+                                                          :estate-procedure/motivation-bonus
+                                                          :estate-procedure/third-party-compensations]}
+   :estate-procedure.type/expropriation {:keys [:estate-procedure/pos :estate-procedure/compensations
+                                                :estate-procedure/motivation-bonus
+                                                :estate-procedure/third-party-compensations]}
+   :estate-procedure.type/property-rights {:keys [:estate-procedure/pos :estate-procedure/compensations
+                                                  :estate-procedure/motivation-bonus :estate-procedure/third-party-compensations]}
+   :estate-procedure.type/property-trading {:keys [:estate-procedure/pos :estate-procedure/land-exchanges
+                                                   :estate-procedure/third-party-compensations]}})
+
+(defn estate-procedure-tx
+  [procedure-form-data]
+  (let [payload (su/select-by-spec :land/create-estate-procedure procedure-form-data)
+        type (:estate-procedure/type payload)
+        {:keys [keys updates]} (type procedure-type-options)]
+    (merge
+      (reduce
+        (fn [payload [path update-fn]]
+          (cu/update-in-if-exists payload [path] update-fn))
+        (select-keys payload (concat common-procedure-keys keys))
+        (concat common-procedure-updates updates))
+      {:db/id "new-estate"
+       :estate-procedure/project [:thk.project/id (:thk.project/id procedure-form-data)]})))
+
+
+(defcommand :land/create-estate-procedure
   {:doc "Create a new compensation for estate"
    :context {conn :conn
              user :user
              db :db}
-   :payload {:estate-compensation/keys [pos procedure compensation]
-             :thk.project/keys [id] :as payload}
+   :payload {:thk.project/keys [id] :as payload}
    :project-id [:thk.project/id id]
-   :authorization {}}
-  (println payload))
+   :authorization {}
+   :pre [(nil? (:db/id payload))]
+   :transact
+   [(estate-procedure-tx payload)]})
 
