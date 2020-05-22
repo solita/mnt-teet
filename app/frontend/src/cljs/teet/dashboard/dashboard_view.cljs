a3f11cd81b2c4efd35ea24022b5abcc987e5cf77
jkorpelin <joni.korpelin@solita.fi>
2020-05-22 13:26:40 +0300

    Adds some WIP spacing to dashboard view

--- app/frontend/src/cljs/teet/dashboard/dashboard_view.cljs
+++ app/frontend/src/cljs/teet/dashboard/dashboard_view.cljs
@@ -36,7 +36,7 @@
 
 (defn- section [label content]
   [:div {:class (<class section-style)}
-   [typography/SectionHeading label]
+   [typography/Heading2 {:style {:margin-bottom "1rem"}} label]
    content])
 
 (defn- task-row [{:task/keys [group type status estimated-end-date]
@@ -53,12 +53,13 @@
         (fmt/date estimated-end-date)]
        [:div {:class (<class common-styles/flex-table-column-style 20)}
         [IconButton {:element "a"
+                     :color :primary
                      :href (url/activity-task (merge params {:task (str id)}))}
-         [icons/navigation-arrow-forward]]]]))])
+         [icons/action-arrow-right-alt]]]]))])
 
 (defn- task-rows-by-group [[group tasks]]
   [:<>
-   [:b (tr-enum group)]
+   [:b (tr-enum group) ":"]
    (mapc task-row tasks)])
 
 
@@ -81,6 +82,7 @@
                   :action (r/as-element
                            [CardActions
                             [IconButton {:element "a"
+                                         :color :primary
                                          :href (url/project (:thk.project/id project))}
                              [icons/action-arrow-right-alt]]
                             [IconButton {:on-click #(toggle-project (:db/id project))}
@@ -91,13 +93,17 @@
       [CardContent
        [section
         (tr [:dashboard :notifications])
-        (doall
-         (for [{:notification/keys [type]
-                :meta/keys [created-at]
-                id :db/id} notifications]
-           ^{:key (str id)}
-           [buttons/link-button {:on-click #(e! (notification-controller/->NavigateTo id))}
-            (tr-enum type) " " (fmt/date-time created-at)]))]
+        (if (empty? notifications)
+          [:span {:class (<class common-styles/gray-text)}
+           "You have no notifications related to this project"]
+          (doall
+            (for [{:notification/keys [type]
+                   :meta/keys [created-at]
+                   id :db/id} notifications]
+              ^{:key (str id)}
+              [:div
+               [buttons/link-button {:on-click #(e! (notification-controller/->NavigateTo id))}
+                (tr-enum type) " " (fmt/date-time created-at)]])))]
 
 
        [section
@@ -106,11 +112,15 @@
          (for [{:thk.lifecycle/keys [type activities estimated-start-date estimated-end-date] :as lifecycle}
                (:thk.project/lifecycles project)]
            ^{:key (str (:db/id lifecycle))}
-           [:<>
-            [:b  (tr-enum type)]
-            [typography/BoldGreyText (str (fmt/date estimated-start-date)
-                                          "\u2013"
-                                          (fmt/date estimated-end-date))]
+           [:div {:style {:margin-bottom "2rem"}}
+            [:div {:style {:margin-bottom "1rem"
+                           :display :flex
+                           :justify-content :space-between
+                           :border-bottom "2px solid black"}}
+             [typography/Heading3 (tr-enum type)]
+             [typography/BoldGreyText (str (fmt/date estimated-start-date)
+                                           "\u2013"
+                                           (fmt/date estimated-end-date))]]
             (doall
              (for [{:activity/keys [name estimated-start-date estimated-end-date
                                     status tasks]
@@ -123,7 +133,8 @@
                            :activity (str id)}}
                  [:<>
                   [itemlist/ItemList {:title (tr-enum name)
-                                         :subtitle (str (fmt/date estimated-start-date)
+                                      :variant :tertiary
+                                      :subtitle (str (fmt/date estimated-start-date)
                                                         "\u2013"
                                                         (fmt/date estimated-end-date))}
                       [itemlist/Item {:label (tr [:fields :activity/status])}
@@ -132,7 +143,7 @@
                        (mapc task-rows-by-group
                              (sort-by (comp task-model/task-group-order :db/ident first)
                                       (group-by :task/group tasks)))]]
-                  [Divider]])]))]))]]]]))
+                  [Divider {:style {:margin "2rem 0"}}]])]))]))]]]]))
 
 (defn dashboard-page [e!
                       {user :user :as _app}
