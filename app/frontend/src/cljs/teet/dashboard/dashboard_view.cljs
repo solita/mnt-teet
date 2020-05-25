1d7383a72cf594a43c1a62ac924290a50f43239e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-05-25 14:59:12 +0300

    Use hierarchical-container in dashboard

--- app/frontend/src/cljs/teet/dashboard/dashboard_view.cljs
+++ app/frontend/src/cljs/teet/dashboard/dashboard_view.cljs
@@ -20,7 +20,9 @@
             [teet.util.collection :as cu]
             [teet.projects.projects-style :as projects-style]
             [teet.common.common-styles :as common-styles]
-            [alandipert.storage-atom :refer [local-storage]]))
+            [teet.ui.common :as common]
+            [alandipert.storage-atom :refer [local-storage]]
+            [teet.theme.theme-colors :as theme-colors]))
 
 (defonce open-projects-atom (local-storage (r/atom #{}) "dashboard-open-projects"))
 
@@ -62,88 +64,88 @@
    [:b (tr-enum group) ":"]
    (mapc task-row tasks)])
 
+(defn- activity-info
+  [project {:activity/keys [name estimated-start-date estimated-end-date
+                    status tasks]
+    id :db/id :as _activity}]
+  [:<>
+   (url/provide-navigation-info
+    {:params {:project (:thk.project/id project)
+              :activity (str id)}}
+    [:<>
+     [itemlist/ItemList {:title (tr-enum name)
+                         :variant :tertiary
+                         :subtitle (str (fmt/date estimated-start-date)
+                                        "\u2013"
+                                        (fmt/date estimated-end-date))}
+      [itemlist/Item {:label (tr [:fields :activity/status])}
+       (tr-enum status)]
+      [:<>
+       (mapc task-rows-by-group
+             (sort-by (comp task-model/task-group-order :db/ident first)
+                      (group-by :task/group tasks)))]]
+     [Divider {:style {:margin "2rem 0"}}]])])
+
+(defn- lifecycle-info
+  [project {:thk.lifecycle/keys [type activities estimated-start-date estimated-end-date] :as _lifecycle}]
+  [:div {:style {:margin-bottom "2rem"}}
+   [:div {:style {:margin-bottom "1rem"
+                  :display :flex
+                  :justify-content :space-between
+                  :border-bottom "2px solid black"}}
+    [typography/Heading3 (tr-enum type)]
+    [typography/BoldGreyText (str (fmt/date estimated-start-date)
+                                  "\u2013"
+                                  (fmt/date estimated-end-date))]]
+   (mapc (r/partial activity-info project) activities)])
 
 (defn project-card [{:keys [e! open-projects toggle-project]}
                     {:keys [project notifications]}]
   (let [open? (boolean (open-projects (:db/id project)))]
-    [Card {:class (<class project-card-style)}
-     [CardHeader {:title
-                  (r/as-element
-                   [:div {:class (<class common-styles/flex-align-center)}
-                    [:div {:class (<class projects-style/project-status-circle-style
-                                          (:thk.project/status project))}]
-                    (project-model/get-column project :thk.project/project-name)])
-                  :subheader
-                  (r/as-element
-                   [:div {:class (<class project-card-subheader-style)}
-                    (str (fmt/date (:thk.project/estimated-start-date project))
-                         "\u2013"
-                         (fmt/date (:thk.project/estimated-end-date project)))])
-                  :action (r/as-element
-                           [CardActions
-                            [IconButton {:element "a"
-                                         :color :primary
-                                         :href (url/project (:thk.project/id project))}
-                             [icons/action-arrow-right-alt]]
-                            [IconButton {:on-click #(toggle-project (:db/id project))}
-                             (if open?
-                               [icons/navigation-unfold-less]
-                               [icons/navigation-unfold-more])]])}]
-     [Collapse {:in open?}
-      [CardContent
-       [section
-        (tr [:dashboard :notifications])
-        (if (empty? notifications)
-          [:span {:class (<class common-styles/gray-text)}
-           "You have no notifications related to this project"]
-          (doall
-            (for [{:notification/keys [type]
-                   :meta/keys [created-at]
-                   id :db/id} notifications]
-              ^{:key (str id)}
-              [:div
-               [buttons/link-button {:on-click #(e! (notification-controller/->NavigateTo id))}
-                (tr-enum type) " " (fmt/date-time created-at)]])))]
-
-
-       [section
-        (tr [:dashboard :activities-and-tasks])
-        (doall
-         (for [{:thk.lifecycle/keys [type activities estimated-start-date estimated-end-date] :as lifecycle}
-               (:thk.project/lifecycles project)]
-           ^{:key (str (:db/id lifecycle))}
-           [:div {:style {:margin-bottom "2rem"}}
-            [:div {:style {:margin-bottom "1rem"
-                           :display :flex
-                           :justify-content :space-between
-                           :border-bottom "2px solid black"}}
-             [typography/Heading3 (tr-enum type)]
-             [typography/BoldGreyText (str (fmt/date estimated-start-date)
-                                           "\u2013"
-                                           (fmt/date estimated-end-date))]]
+    [common/hierarchical-container
+     {:heading-color theme-colors/gray-lighter
+      :heading-text-color theme-colors/gray-dark
+      :heading-content
+      [:div {:on-click #(toggle-project (:db/id project))}
+       [:div {:class (<class common-styles/flex-align-center)}
+        [:div {:class (<class projects-style/project-status-circle-style
+                              (:thk.project/status project))}]
+        [:div {:style {:flex-grow 10 :font-weight :bold}
+               :class (<class common-styles/inline-block)}
+         (project-model/get-column project :thk.project/project-name)]
+        [buttons/button-secondary {:on-click (fn [e] (.stopPropagation e))
+                                   :style {:margin "0.5rem"}
+                                   :element "a"
+                                   :href (url/project (:thk.project/id project))}
+         (tr [:dashboard :open])]]
+       [:div {:class (<class project-card-subheader-style)}
+        (str (fmt/date (:thk.project/estimated-start-date project))
+             "\u2013"
+             (fmt/date (:thk.project/estimated-end-date project)))]
+]
+      :children
+      (list
+       ^{:key "project-info"}
+       [Collapse {:in open?}
+        [:<>
+         [section
+          (tr [:dashboard :notifications])
+          (if (empty? notifications)
+            [:span {:class (<class common-styles/gray-text)}
+             "You have no notifications related to this project"]
             (doall
-             (for [{:activity/keys [name estimated-start-date estimated-end-date
-                                    status tasks]
-                    id :db/id :as activity}
-                   activities]
+             (for [{:notification/keys [type]
+                    :meta/keys [created-at]
+                    id :db/id} notifications]
                ^{:key (str id)}
-               [:<>
-                (url/provide-navigation-info
-                 {:params {:project (:thk.project/id project)
-                           :activity (str id)}}
-                 [:<>
-                  [itemlist/ItemList {:title (tr-enum name)
-                                      :variant :tertiary
-                                      :subtitle (str (fmt/date estimated-start-date)
-                                                        "\u2013"
-                                                        (fmt/date estimated-end-date))}
-                      [itemlist/Item {:label (tr [:fields :activity/status])}
-                       (tr-enum status)]
-                      [:<>
-                       (mapc task-rows-by-group
-                             (sort-by (comp task-model/task-group-order :db/ident first)
-                                      (group-by :task/group tasks)))]]
-                  [Divider {:style {:margin "2rem 0"}}]])]))]))]]]]))
+               [:div
+                [buttons/link-button {:on-click #(e! (notification-controller/->NavigateTo id))}
+                 (tr-enum type) " " (fmt/date-time created-at)]])))]
+
+
+         [section
+          (tr [:dashboard :activities-and-tasks])
+          (mapc (r/partial lifecycle-info project) (:thk.project/lifecycles project))]]])}]))
 
 (defn dashboard-page [e!
                       {user :user :as _app}
