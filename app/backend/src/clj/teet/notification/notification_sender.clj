b748c7e92a7711a8ee09832f8ae4a61e540750e8
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-06-08 09:10:16 +0300

    Notification email with link to target

--- app/backend/src/clj/teet/notification/notification_sender.clj
+++ app/backend/src/clj/teet/notification/notification_sender.clj
@@ -5,11 +5,14 @@
             [datomic.client.api :as d]
             [teet.notification.notification-db :as notification-db]
             [teet.notification.notification-queries :as notification-queries]
-            [teet.log :as log]))
+            [teet.log :as log]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [teet.localization :refer [with-language tr-enum]]))
 
 (def ^:private email (delay (aws/client {:api :email})))
 
-(defn send-email [from to subject body]
+(defn- send-email* [from to subject body]
   (aws/invoke
    @email
    {:op :SendEmail
@@ -20,22 +23,43 @@
                                            [to])}
               :Source from }}))
 
-(defn prepare-email [db notification]
+(defn send-email [from to subject body]
+  (try
+    (send-email* from to subject body)
+    (catch Exception e
+      (log/warn e "Exception while sending email to" to))))
+
+(defmacro route-data []
+  (into {}
+        (map (fn [[route {path :path}]]
+               [route path]))
+        (read-string (slurp (io/file "../frontend/resources/routes.edn")))))
+
+(def routes (route-data))
+
+(defn- navigation-info->url [base-url {:keys [page params]}]
+  (let [path (routes page)]
+    (str base-url "#" (str/replace path #":[^/]+"
+                                   (fn [m]
+                                     (params (keyword (subs m 1))))))))
+
+(defn prepare-email [db base-url notification]
   ;; FIXME: we need email template/message based on notification type
   (let [nav-info (notification-queries/notification-navigation-info
                   db
                   (-> notification :notification/receiver :db/id)
                   (:db/id notification))]
-    {:hey-you-received (:notification/type notification)
-     :with-nav-info nav-info
-     :fixme "implement the actual message"}))
+    (with-language :et
+      {:subject (str "TEET: " (tr-enum (:notification/type notification)))
+       :body (str "Please see it here: " (navigation-info->url base-url nav-info))})))
 
 (defn send-notifications-ion [_event]
   (let [conn (environment/datomic-connection)
         db (d/db conn)
         notifications (notification-db/notifications-to-send db)
         now (java.util.Date.)
-        from (environment/ssm-param :email :from)]
+        from (environment/ssm-param :email :from)
+        base-url (environment/config-value :base-url)]
 
     (if (empty? notifications)
       (log/info "No pending notifications to send.")
@@ -49,7 +73,8 @@
         ;; Send emails
         (doseq [notification notifications
                 :let [to (get-in notification [:notification/receiver :user/email])
-                      email (prepare-email db notification)]
+                      {:keys [subject body]} (prepare-email db base-url notification)]
                 :when to]
-          (log/info "sending to: " to ", email: " email))))
+          (log/info "sending to: " to ", subject: " subject ", body: " body)
+          (send-email from to subject body))))
     :ok))
