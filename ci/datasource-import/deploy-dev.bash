131f1df2e1396742408a4a26da10aa36dbdd41d3
Erno Kuusela <erno!erno-laptop>
2020-06-15 12:27:53 +0300

    add scheduling to datasource import codebuild job

--- ci/datasource-import/deploy-dev.bash
+++ ci/datasource-import/deploy-dev.bash
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-aws --region eu-central-1 cloudformation deploy  --stack-name teet-datasource-import --template $PWD/import.yml
+aws --region eu-central-1 cloudformation deploy  --stack-name teet-datasource-import \
+    --capabilities CAPABILITY_IAM --template $PWD/import.yml
 aws --region eu-central-1 cloudformation describe-stack-events --stack-name teet-datasource-import
