af4a2e3ae373ed85c828357924c7676081184324
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-06-18 11:42:40 +0300

    Move permission giving test to activity

--- app/backend/test/teet/activity_commands_test.clj
+++ app/backend/test/teet/activity_commands_test.clj
@@ -194,3 +194,38 @@
 
     (is (:meta/deleted? (du/entity (tu/db)
                                    (tu/get-data :new-activity-id))))))
+
+
+(deftest assigning-pm-gives-permission
+  (let [current-manager #(get-in (tu/entity (act1-id))
+                                 [:activity/manager :user/id] :no-manager)]
+    (testing "Initially p1 doesn't have a manager"
+      (is (= :no-manager (current-manager))))
+
+    (testing "Assigning p1 lc1 act1 manager"
+      (tu/local-command tu/mock-user-boss
+                        :activity/update
+                        {:activity {:db/id (act1-id)
+                                    :activity/manager {:user/id (second tu/mock-user-edna-consultant)}}})
+
+      (testing "sets the new manager"
+        (is (= (second tu/mock-user-edna-consultant)
+               (current-manager))))
+
+
+      (testing "gives the new pm permission"
+        (let [permissions (d/q '[:find (pull ?p [:permission/valid-from])
+                                 :where
+                                 [?user :user/permissions ?p]
+                                 [?p :permission/role :manager]
+                                 [?p :permission/projects ?project]
+                                 [?p :permission/valid-from ?valid-from]
+                                 [(.before ?valid-from ?now)]
+                                 [(missing? $ ?p :permission/valid-until)]
+                                 :in
+                                 $ ?user ?project ?now]
+                               (tu/db)
+                               tu/mock-user-edna-consultant
+                               [:thk.project/id "11111"]
+                               (java.util.Date.))]
+          (is (seq permissions) "permission is present"))))))
--- app/backend/test/teet/project/project_commands_test.clj
+++ app/backend/test/teet/project/project_commands_test.clj
@@ -7,36 +7,4 @@
   tu/with-environment
   (tu/with-db))
 
-(deftest assigning-pm-gives-permission
-  (let [current-manager #(get-in (tu/entity (tu/->db-id "p1"))
-                                 [:thk.project/manager :user/id] :no-manager)]
-    (testing "Initially p1 doesn't have a manager"
-      (is (= :no-manager (current-manager))))
-
-    (testing "Assigning p1 project manager"
-      (tu/local-command tu/mock-user-boss
-                        :thk.project/update
-                        {:thk.project/id "11111"
-                         :thk.project/manager {:user/id (second tu/mock-user-edna-consultant)}})
-
-      (testing "sets the new manager"
-        (is (= (second tu/mock-user-edna-consultant)
-               (current-manager))))
-
-
-      (testing "gives the new pm permission"
-        (let [permissions (d/q '[:find (pull ?p [:permission/valid-from])
-                                 :where
-                                 [?user :user/permissions ?p]
-                                 [?p :permission/role :manager]
-                                 [?p :permission/projects ?project]
-                                 [?p :permission/valid-from ?valid-from]
-                                 [(.before ?valid-from ?now)]
-                                 [(missing? $ ?p :permission/valid-until)]
-                                 :in
-                                 $ ?user ?project ?now]
-                               (tu/db)
-                               tu/mock-user-edna-consultant
-                               [:thk.project/id "11111"]
-                               (java.util.Date.))]
-          (is (seq permissions) "permission is present"))))))
+;; PENDING: No tests at the moment (pm test was moved to activity command tests)
