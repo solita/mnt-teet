c9ef2d2cb30dad17971d7217d8d401107cdf44ee
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-06-30 09:55:41 +0300

    Implement all needed S3 operations

--- app/backend/src/clj/teet/file/file_storage.clj
+++ app/backend/src/clj/teet/file/file_storage.clj
@@ -1,7 +1,8 @@
 (ns teet.file.file-storage
   "Access to S3 bucket for storing files. Access must be checked in previous layers."
   (:require [amazonica.aws.s3 :as s3]
-            [teet.environment :as environment])
+            [teet.environment :as environment]
+            [teet.integration.integration-s3 :as integration-s3])
   (:import (java.util Date)
            (com.amazonaws.services.s3.model GeneratePresignedUrlRequest
                                             ResponseHeaderOverrides)
@@ -15,21 +16,29 @@
 (defn- storage-bucket []
   (environment/config-value :document-storage :bucket-name))
 
-(defn- presigned-url [method content-disposition file-name]
-  (let [request (-> (GeneratePresignedUrlRequest. (storage-bucket)
-                                                  file-name
-                                                  (HttpMethod/valueOf method))
-                    (.withExpiration (expiration-date)))
-        request (if content-disposition
-                  (.withResponseHeaders
-                   request
-                   (.withContentDisposition (ResponseHeaderOverrides.)
-                                            content-disposition))
-                  request)]
-    (str (s3/generate-presigned-url request))))
+(comment
+  (defn- presigned-url [method content-disposition file-name]
+    (let [request (-> (GeneratePresignedUrlRequest. (storage-bucket)
+                                                    file-name
+                                                    (HttpMethod/valueOf method))
+                      (.withExpiration (expiration-date)))
+          request (if content-disposition
+                    (.withResponseHeaders
+                     request
+                     (.withContentDisposition (ResponseHeaderOverrides.)
+                                              content-disposition))
+                    request)]
+      (str (s3/generate-presigned-url request))))
 
-(def download-url (partial presigned-url "GET"))
-(def upload-url (partial presigned-url "PUT" nil))
+  (def download-url (partial presigned-url "GET"))
+  (def upload-url (partial presigned-url "PUT" nil)))
+
+(defn download-url [content-disposition file-name]
+  (integration-s3/presigned-url content-disposition
+                                "GET" (storage-bucket) file-name))
+
+(defn upload-url [file-name]
+  (integration-s3/presigned-url "PUT" (storage-bucket) file-name))
 
 (defn document-s3-ref
   "Returns an integration S3 file descriptor for a document."
