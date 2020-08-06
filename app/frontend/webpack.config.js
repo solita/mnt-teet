b0c036c2fda1914a6b0259446c539d5d7638dc86
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-08-06 09:39:19 +0300

    Don't minify master branch with terser

--- app/frontend/webpack.config.js
+++ app/frontend/webpack.config.js
@@ -1,7 +1,18 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+// Only minimize when doing production build as it takes a long time
+// master branch is deployed to dev constantly so we want it to be quick
+// even with the expense of having larger bundle (eg. 3.0mb => 4.5mb)
+branch = process.env["CODEBUILD_SOURCE_VERSION"] || "master";
+minimize = branch !== "master"
+
 module.exports = {
-  entry: './out/index.js',
-  output: {
-    path: __dirname + "/out",
-    filename: 'main.js'
-  }
+    entry: './out/index.js',
+    output: {
+        path: __dirname + "/out",
+        filename: 'main.js'
+    },
+    optimization: {
+        minimize
+    }
 }
