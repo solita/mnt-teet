d5f062b88960b6f429580479f9695f706676a169
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-08-24 10:25:21 +0300

    Add new alias for ion push

--- app/backend/build.sh
+++ app/backend/build.sh
@@ -4,4 +4,4 @@ set -eu
 
 #echo "{:git-commit \"$CODEBUILD_RESOLVED_SOURCE_VERSION\"}" > resources/build-info.edn
 
-clojure -Adev -m datomic.ion.dev '{:op :push :region "eu-central-1"}'
+clojure -Aion -m datomic.ion.dev '{:op :push :region "eu-central-1"}'
