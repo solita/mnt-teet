b5c12282abe11d4ca797be626007c6defe29c9b0
Erno Kuusela <erno!erno-laptop>
2020-08-25 12:32:53 +0300

    add log catting

--- app/frontend/run-tests.sh
+++ app/frontend/run-tests.sh
@@ -3,4 +3,11 @@
 npm install
 
 clojure -A:testbuild
-clojure -A:testrun
+if clojure -A:testrun; then
+  echo test successful
+else
+  echo test failed, trying to cat /tmp/*.edn in case they contain logs:
+  cat /tmp/*.edn
+  exit 1
+fi
+
