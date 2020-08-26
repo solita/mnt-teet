b5e35c17073427cc4e5560a1aa3bac55ca60d9f7
jkorpelin <joni.korpelin@solita.fi>
2020-08-26 11:30:20 +0300

    Changes project-meetings-query to use poject-by-id with options

--- app/backend/src/clj/teet/meeting/meeting_queries.clj
+++ app/backend/src/clj/teet/meeting/meeting_queries.clj
@@ -2,22 +2,31 @@
   (:require [teet.project.project-db :as project-db]
             [teet.db-api.core :refer [defquery]]
             [teet.meta.meta-query :as meta-query]
-            [datomic.client.api :as d]
-            [teet.meeting.meeting-db :as meeting-db]))
+            [teet.project.project-model :as project-model]))
+
+
+(defn fetch-project-meetings
+  [db eid]
+  (project-db/project-by-id db eid {:thk.lifecycle/activities
+                                    [{:activity/meetings
+                                      [:db/id
+                                       :meeting/title
+                                       :meeting/location
+                                       {:meeting/organizer [:user/person-id
+                                                            :user/given-name
+                                                            :user/family-name]}
+                                       :meeting/end :meeting/start]}]}))
 
 (defquery :meeting/project-with-meetings
-  {:doc "Fetch project info (for project navigator) and project's meetings"
-   :context {:keys [db user]}
+  {:doc "Fetch project data with project meetings"
+   :context {db :db
+             user :user}
    :args {:thk.project/keys [id]}
    :project-id [:thk.project/id id]
    :authorization {:project/read-info {:eid [:thk.project/id id]
                                        :link :thk.project/owner
                                        :access :read}}}
-  (let [project (meta-query/without-deleted
-                 db (project-db/project-by-id db [:thk.project/id id]))]
-    (assoc project
-           :meetings (meeting-db/meetings db
-                                          '[[?p :thk.project/lifecycles ?lc]
-                                            [?lc :thk.lifecycle/activities ?act]
-                                            [?act :activity/meetings ?meeting]]
-                                          {'?p [:thk.project/id id]}))))
+  (update (meta-query/without-deleted
+            db
+            (fetch-project-meetings db [:thk.project/id id]))
+          :thk.project/lifecycles project-model/sort-lifecycles))
