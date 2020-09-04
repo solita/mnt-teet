d3f4e30c7e538324ad95aa7f33af6a0d505dbf35
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-09-04 08:27:14 +0300

    Leave the modification timestamp and user for delete/undelete

--- app/backend/src/clj/teet/meta/meta_commands.clj
+++ app/backend/src/clj/teet/meta/meta_commands.clj
@@ -1,7 +1,6 @@
 (ns teet.meta.meta-commands
   "Generic commands for meta management"
   (:require [teet.db-api.core :refer [defcommand]]
-            [datomic.client.api :as d]
             [teet.meta.meta-query :as meta-query]))
 
 (defcommand :meta/undo-delete
@@ -11,9 +10,4 @@
    :project-id nil
    :authorization {}
    :pre [(meta-query/can-undo-delete? db id user)]
-   :transact (let [{:meta/keys [deleted? modifier modified-at]}
-                   (d/pull db [:meta/deleted? :meta/modifier :meta/modified-at]
-                           id)]
-               [[:db/retract id :meta/deleted? deleted?]
-                [:db/retract id :meta/modifier (:db/id modifier)]
-                [:db/retract id :meta/modified-at modified-at]])})
+   :transact [[:db/retract id :meta/deleted? true]]})
