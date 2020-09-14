be7866473ad9ec6577c4f4d43ba662212678f8a3
Erno Kuusela <erno!erno-laptop>
2020-09-14 11:10:07 +0300

    use specific commithash in backend build.sh and buildspec to support deploying by commithash instead of by branch name

--- app/backend/build.sh
+++ app/backend/build.sh
@@ -3,5 +3,10 @@
 set -eu
 
 #echo "{:git-commit \"$CODEBUILD_RESOLVED_SOURCE_VERSION\"}" > resources/build-info.edn
-
-clojure -Aion -m datomic.ion.dev '{:op :push :region "eu-central-1"}'
+COMMITHASH="$1"
+if [ -z "$COMMITHASH" ]; then
+    echo commithash arg missing
+    exit 1
+else
+    clojure -Aion -m datomic.ion.dev '{:op :push :region "eu-central-1" :rev "'$COMMITHASH'"}'
+fi
