4ffd68ab5a0f6ca16d5c3b31129d5612e6a9c38f
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-09-16 09:49:07 +0300

    TEET-816 Use POST in feature fetch
    GET will fail if there's so many features that the id list exceeds URL
    length limits, switching to POST with ids as JSON payload.

--- app/backend/src/clj/teet/gis/features.clj
+++ app/backend/src/clj/teet/gis/features.clj
@@ -2,7 +2,6 @@
   "Utilities to fetch from PostgREST datasource/feature tables."
   (:require [org.httpkit.client :as client]
             [cheshire.core :as cheshire]
-            [clojure.string :as str]
             [teet.auth.jwt-token :as jwt-token]))
 
 
@@ -13,8 +12,8 @@
 
 (defn geojson-features-by-id [{:keys [api-url api-secret]} ids]
   (-> (str api-url "/rpc/geojson_features_by_id")
-      (client/get
-       {:query-params {"ids" (str "{" (str/join "," ids) "}")}
+      (client/post
+       {:body (cheshire/encode {"ids" (vec ids)})
         :headers {"Accept" "text/plain"
                   "Authorization"
                   (str "Bearer "
