8a900e5c743d67835084dc76975b2cc00137c420
Erno Kuusela <erno!erno-laptop>
2020-10-20 10:57:05 +0300

    fix syntax in test

--- app/backend/test/teet/util/date_test.clj
+++ app/backend/test/teet/util/date_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [teet.util.date :as sut]))
 
-(deftest "date past functions work"
-  (= nil (sut/test-past)))
+(deftest test-past  
+  (is (= nil (sut/test-past))))
 
