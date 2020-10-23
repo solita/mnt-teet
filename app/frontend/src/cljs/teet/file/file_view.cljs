a0a4091b5d0b61ddf2e0073b8e59149e3b65a06c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-10-23 13:13:00 +0300

    Show initial upload vs edit times

--- app/frontend/src/cljs/teet/file/file_view.cljs
+++ app/frontend/src/cljs/teet/file/file_view.cljs
@@ -453,9 +453,14 @@
         [typography/GreyText
          [:strong (str (tr [:fields :file/original-name]) ": ")]
          [:span (:file/original-name file)]]]
-       [:div.file-details-upload-info
-        (tr [:file :upload-info] {:author (user-model/user-name (:meta/creator file))
-                                  :date (format/date (:meta/created-at file))})]
+
+       (when-let [first-version (last other-versions)]
+         [:div.file-details-upload-info
+          (tr [:file :upload-info] {:author (user-model/user-name (:meta/creator first-version))
+                                    :date (format/date (:meta/created-at first-version))})])
+       [:div.file-details-edit-info
+        (tr [:file :edit-info] {:author (user-model/user-name (:meta/creator file))
+                                :date (format/date (:meta/created-at file))})]
 
 
 
