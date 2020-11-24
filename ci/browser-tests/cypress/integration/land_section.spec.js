fb51b2fce339ce72fcc77642a2c81232bb268548
Erno Kuusela <erno!erno-laptop>
2020-11-24 20:06:21 +0200

    improve test

--- ci/browser-tests/cypress/integration/land_section.spec.js
+++ ci/browser-tests/cypress/integration/land_section.spec.js
@@ -13,10 +13,9 @@ context('Land section', () => {
     cy.get('thead > tr > th:nth-child(1) > label > div > input').type("aruvalla");
     cy.get("p").contains("Aruvalla").click();
     cy.get("main span.MuiIconButton-label > span.material-icons").click();
-    cy.get("body > div:nth-child(3) > div > ul > li:nth-child(6)").click();
-    cy.get("a", {timeout: 20000}).contains("Vaatan omanike andmeid").click();
-    cy.get("h2").contains("Omanikud ja omandiosad");
-    // empty dialog as of this writing, can continue test when dialog impl is there
+    cy.get("li.MuiButtonBase-root:nth-child(6) > div:nth-child(1)").click();
+    cy.contains("Vaatan omanike andmeid", {timeout: 20000}).click();
+    cy.contains("Omanikud ja omandiosad");
   });
 
 })
