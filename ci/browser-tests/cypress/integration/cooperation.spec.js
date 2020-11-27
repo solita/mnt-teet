a7fe2e22cf6bc35047465059c9ed87d64ac3269d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-11-27 18:24:15 +0200

    Cooperation workflow test and utility commands

--- ci/browser-tests/cypress/integration/cooperation.spec.js
+++ ci/browser-tests/cypress/integration/cooperation.spec.js
@@ -1,9 +1,10 @@
-context('Cooperation', () => {
-    beforeEach(() => {
+context('Cooperation', function() {
+    before(() => {
         cy.dummyLogin("Carla")
+        cy.randomName("thirdparty", "testcompany")
     })
 
-    it("Cooperation menu visible in project", () => {
+    it("Cooperation workflow", function() {
         cy.get(".left-menu-projects-list").click()
         cy.get("td").contains("cooperation test").click()
 
@@ -13,6 +14,26 @@ context('Cooperation', () => {
         // open menu and select cooperation
         cy.get("button.project-menu").click()
         cy.get("li.project-menu-item-cooperation").click()
+
+        cy.get(".cooperation-overview-page")
+
+        // Create new third party
+
+        cy.get("button.new-third-party").click()
+
+        cy.formInput(
+            ":cooperation.3rd-party/name", this.thirdparty,
+            ":cooperation.3rd-party/id-code", "123456",
+            ":cooperation.3rd-party/email", "test@example.com",
+            ":cooperation.3rd-party/phone", "555-1234-567890")
+
+        cy.formSubmit()
+
+        // page has h1 with name of 3rd party and new application button
+
+        cy.get(".cooperation-third-party-page h1").contains(this.thirdparty)
+        cy.get("button.new-application")
+
     })
 
 })
