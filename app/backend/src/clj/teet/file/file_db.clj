d96e1e6a57ba35999577e27a4fb9e8902a0b5e7b
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2020-12-08 14:39:44 +0200

    Add file sorting by description (file name)

--- app/backend/src/clj/teet/file/file_db.clj
+++ app/backend/src/clj/teet/file/file_db.clj
@@ -189,18 +189,20 @@
         next-version (fn [file]
                        (some #(when (= (:db/id file)
                                        (get-in % [:file/previous-version :db/id])) %)
-                             next-versions))]
-    (vec
-     (for [f first-versions
-           :let [versions (filter (complement :meta/deleted?)
-                                  (reverse
-                                   (take-while some? (iterate next-version f))))
-                 [latest-version & previous-versions] versions]
-           :when latest-version]
-       (assoc latest-version
-              :file/full-name (filename-metadata/metadata->filename
-                               (file-metadata db (:db/id latest-version)))
-              :versions previous-versions)))))
+                             next-versions))
+        meta-files (vec
+                       (for [f first-versions
+                             :let [versions (filter (complement :meta/deleted?)
+                                                    (reverse
+                                                      (take-while some? (iterate next-version f))))
+                                   [latest-version & previous-versions] versions]
+                             :when latest-version]
+                         (assoc latest-version
+                           :file/full-name (filename-metadata/metadata->filename
+                                             (file-metadata db (:db/id latest-version)))
+                           :versions previous-versions)))]
+      (sort-by :file/name meta-files)
+      ))
 
 (defn land-files-by-project-and-sequence-number [db user project-id sequence-number]
   (file-listing
