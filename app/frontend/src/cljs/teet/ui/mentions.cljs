452a9d506657009d3f843f14bae5266804547dc0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-12-11 15:51:55 +0200

    Fix caret jumping on first edit of existing mentions input

--- app/frontend/src/cljs/teet/ui/mentions.cljs
+++ app/frontend/src/cljs/teet/ui/mentions.cljs
@@ -32,41 +32,49 @@
           [:span {:class (<class common-styles/label-text-style)}
            label (when required
                    [common/required-astrix])]
-          [MentionsInput {:value value
-                          :id id
-                          :on-focus (fn [_]
-                                      (reset! caret (.-selectionStart @input-ref)))
-                          :on-change (fn [e new-value new-plain-text-value new-mentions]
-                                       (let [old-mention-ids (into #{} (map #(aget % "id")) @old-mentions)
-                                             new-mention-ids (into #{} (map #(aget % "id")) new-mentions)
-                                             added-mention (first (set/difference new-mention-ids old-mention-ids))
-                                             removed-mention (first (set/difference old-mention-ids new-mention-ids))]
-                                         (reset! caret
-                                                 (cond
-                                                   ;; Mention added: move caret after it
-                                                   added-mention
-                                                   (some #(when (= (aget % "id") added-mention)
-                                                            (+ (aget % "plainTextIndex")
-                                                               (count (aget % "display")))) new-mentions)
+          [MentionsInput
+           {:value value
+            :id id
+            :on-focus (fn [_]
+                        (reset! caret (.-selectionStart @input-ref)))
+            :on-change (fn [e _new-value new-plain-text-value new-mentions]
+                         (let [new-mention-ids (into #{} (map #(aget % "id")) new-mentions)
+                               current-old-mentions @old-mentions
+                               old-mention-ids (if (nil? current-old-mentions)
+                                                 ;; First change, old mentions not initialized yet
+                                                 ;; Use the new mentions
+                                                 new-mention-ids
 
-                                                   ;; Mention removed: move caret at it's start index
-                                                   removed-mention
-                                                   (some #(when (= (aget % "id") removed-mention)
-                                                            (aget % "plainTextIndex")) @old-mentions)
+                                                 ;; Use old mentions from previous edit
+                                                 (into #{} (map #(aget % "id")) current-old-mentions))
+                               added-mention (first (set/difference new-mention-ids old-mention-ids))
+                               removed-mention (first (set/difference old-mention-ids new-mention-ids))]
+                           (reset! caret
+                                   (cond
+                                     ;; Mention added: move caret after it
+                                     added-mention
+                                     (some #(when (= (aget % "id") added-mention)
+                                              (+ (aget % "plainTextIndex")
+                                                 (count (aget % "display")))) new-mentions)
 
-                                                   ;; No mentions changed, keep caret at current position
-                                                   :else
-                                                   (.-selectionStart @input-ref)))
-                                         (reset! old-mentions new-mentions))
-                                       (on-change e)
-                                       (when (not= (count new-plain-text-value)
-                                                   @caret)
-                                         ;; Flush forces re-render did-update call
-                                         ;; Otherwise there would be situations in which the change is fired twice
-                                         ;; without a call to did-update
-                                         (r/flush)))
-                          :input-ref #(reset! input-ref %)
-                          :class-name "comment-textarea"}
+                                     ;; Mention removed: move caret at it's start index
+                                     removed-mention
+                                     (some #(when (= (aget % "id") removed-mention)
+                                              (aget % "plainTextIndex")) @old-mentions)
+
+                                     ;; No mentions changed, keep caret at current position
+                                     :else
+                                     (.-selectionStart @input-ref)))
+                           (reset! old-mentions new-mentions))
+                         (on-change e)
+                         (when (not= (count new-plain-text-value)
+                                     @caret)
+                           ;; Flush forces re-render did-update call
+                           ;; Otherwise there would be situations in which the change is fired twice
+                           ;; without a call to did-update
+                           (r/flush)))
+            :input-ref #(reset! input-ref %)
+            :class-name "comment-textarea"}
            [Mention {:trigger "@"
                      :display-transform (fn [_ display]
                                           (str "@" display))
