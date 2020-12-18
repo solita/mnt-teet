87d88fc7f94dd544cbecd3b2fa41a38461e18965
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2020-12-18 10:35:28 +0200

    Add helper method to call backend command from cypress tests

--- ci/browser-tests/cypress/integration/meeting_links.spec.js
+++ ci/browser-tests/cypress/integration/meeting_links.spec.js
@@ -2,8 +2,23 @@ describe('Meeting Links Test', function () {
     before(() => {
         cy.dummyLogin("Danny")
         cy.selectLanguage("ENG")
-        cy.projectByName("Alatskivi")
+        cy.projectByName("integration test project")
+        cy.backendCommand(":thk.project/update-cadastral-units", "{:project-id \"18463\" :cadastral-units " +
+            "#{\"2:63801:001:0241\"" +
+             "\"2:92901:001:0138\"" +
+            " \"2:93001:001:0019\"" +
+            " \"2:63801:001:0176\"" +
+            "\"2:63801:001:0172\"" +
+            "\"2:93001:001:0029\"" +
+            "\"2:63801:001:0239\"" +
+            "\"2:63801:001:0243\"" +
+            "\"2:63801:001:0238\"" +
+            "\"2:92901:001:0137\"" +
+            "\"2:93001:001:0071\"" +
+            "\"2:93001:001:0009\"" +
+            "\"2:63801:001:0237\"}}")
     })
+
 
     it('sort searched cadastral units ', function () {
         cy.get("h1").contains("Alatskivi")
