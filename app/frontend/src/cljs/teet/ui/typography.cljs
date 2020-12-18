82c4a1e78594e24e22bc80b0d64bcc1f71303b09
Ville Outamaa <ville.outamaa@solita.fi>
2020-12-18 09:39:02 +0200

    TEET-1000 Add raw styles for new typography

--- app/frontend/src/cljs/teet/ui/typography.cljs
+++ app/frontend/src/cljs/teet/ui/typography.cljs
@@ -4,6 +4,142 @@
             [teet.ui.util :as util]
             [teet.theme.theme-colors :as theme-colors]))
 
+;; New typography styles
+(defn h1-desktop []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "300"
+   :font-size "32px"
+   :line-height "48px"})
+
+(defn h2-desktop []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "28px"
+   :line-height "42px"})
+
+(defn h3-desktop []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "24px"
+   :line-height "36px"})
+
+(defn h4-desktop []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "20px"
+   :line-height "30px"})
+
+(defn h5-desktop []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "18px"
+   :line-height "27px"})
+
+(defn body-1-bold []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "bold"
+   :font-size "16px"
+   :line-height "24px"})
+
+(defn body-1-regular []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "16px"
+   :line-height "24px"})
+
+(defn link-1 []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "16px"
+   :line-height "24px"})
+
+(defn link-2 []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "14px"
+   :line-height "21px"})
+
+(defn body-2-bold []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "bold"
+   :font-size "14px"
+   :line-height "21px"})
+
+(defn body-3-bold []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "bold"
+   :font-size "12px"
+   :line-height "18px"})
+
+(defn body-3 []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "12px"
+   :line-height "18px"})
+
+(defn subtitle-1 []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "bold"
+   :font-size "16px"
+   :line-height "24px"})
+
+(defn subtitle-2 []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "14px"
+   :line-height "21px"})
+
+(defn h1-mobile []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "300"
+   :font-size "24px"
+   :line-height "36px"})
+
+(defn h2-mobile []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "22px"
+   :line-height "30px"})
+
+(defn h3-mobile []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "20px"
+   :line-height "30px"})
+
+(defn h4-mobile []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "19px"
+   :line-height "28px"})
+
+(defn h5-mobile []
+  {:font-family "Roboto"
+   :font-style "normal"
+   :font-weight "normal"
+   :font-size "18px"
+   :line-height "27px"})
+
+
+;; Old typography styles
 (defn- small-gray-text-style
   []
   {:display :block
