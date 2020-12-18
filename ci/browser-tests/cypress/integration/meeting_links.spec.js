851a549c2069b9e3062b5e2f30aab3b420872acb
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2020-12-18 19:16:59 +0200

    Search Cadastral Units and Search Estates tests are ready. Minor refactoring

--- ci/browser-tests/cypress/integration/meeting_links.spec.js
+++ ci/browser-tests/cypress/integration/meeting_links.spec.js
@@ -1,5 +1,5 @@
-describe('Meeting Links Test', function () {
-    before(() => {
+describe('Meeting Links', function () {
+    beforeEach(() => {
         cy.dummyLogin("Danny")
         cy.selectLanguage("ENG")
         cy.projectByName("integration test project")
@@ -21,62 +21,95 @@ describe('Meeting Links Test', function () {
             "\"2:63801:001:0237\"}}")
     })
 
-    after(() => {
+    afterEach(() => {
         cy.get(".button-with-menu button").click({"multiple":true, "force":true})
         cy.get("#delete-button").click()
         cy.get("#confirm-delete").click()
     })
 
+    function checkCadastralUnitsSorted() {
+        const search = `input[placeholder='Link...']`
+        cy.get(search).type("Tee")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(1)").contains("58 Aluste-Kergu tee")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(2)").contains("58 Aluste-Kergu tee")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(3)").contains("58 Aluste-Kergu tee")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(4)").contains("Juurdepääsutee lõik 471")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(5)").contains("Rapla-Pärnu raudtee 471")
+    }
 
-    it('sort searched cadastral units ', function () {
-        cy.get("h1").contains("integration test project")
+    function checkEstatesSorted() {
+        const search = `input[placeholder='Link...']`
+        cy.get(search).type("36")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(1)").contains("4343606")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(2)").contains("8293650")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(3)").contains("11263650")
+        cy.get("div.select-user-list div.select-user-entry:nth-child(4)").contains("16036950")
+    }
 
+    function createMeeting() {
         cy.get("button.project-menu").click()
         cy.get("li.project-menu-item-project-meetings").click()
-
         cy.wait(1000)
 
-        // create test meeting
         cy.contains('Preliminary design').click()
-
         cy.wait(1000)
 
         cy.contains('Create meeting').click()
-
         cy.get(`input[class*=':date-input']`).type(new Date().toLocaleDateString("et-EE"))
-
-        cy.wait(1000)
-
-        cy.get("[class*=start-time]").type( "10:00")
-        cy.get("[class*=end-time]").type("11:00" )
-
+        cy.get("[class*=start-time]").type("10:00")
+        cy.get("[class*=end-time]").type("11:00")
         cy.formInput(
             ":meeting/title", this.testmeeting,
             ":meeting/location", "Test environment RAM")
         cy.formSubmit()
+    }
 
+    function createAgenda() {
         let sel = "a:contains(" + this.testmeeting + ")"
         cy.get(sel).click()
 
         cy.get("#add-agenda").click()
-
         cy.formInput(":meeting.agenda/topic", this.testtopic)
-        cy.get("button[type=submit]").click({"multiple":true, "force": true})
+        cy.get("button[type=submit]").click({"multiple": true, "force": true})
+    }
 
+    function selectCadastralUnits() {
         cy.get(".agenda-heading h3").contains(this.testtopic)
         cy.get(".agenda-heading h3").click()
-
         const select = "#links-type-select"
         const option = `[data-item*=':cadastral-unit']`
-
         cy.get(option).then(($opt) => {
             cy.get(select).select($opt.attr("value"))
         })
+    }
 
+    function selectEstates() {
+        cy.get(".agenda-heading h3").contains(this.testtopic)
+        cy.get(".agenda-heading h3").click()
+        const select = "#links-type-select"
+        const option = `[data-item*=':estate']`
+        cy.get(option).then(($opt) => {
+            cy.get(select).select($opt.attr("value"))
+        })
+    }
+
+    it('sort searched cadastral units', function () {
+        cy.get("h1").contains("integration test project")
+        createMeeting.call(this);
+        createAgenda.call(this);
+        selectCadastralUnits.call(this);
         cy.wait(1000)
+        checkCadastralUnitsSorted();
+        cy.wait(3000)
+    })
 
-        const search = `input[placeholder='Link...']`
-        cy.get(search).type("Tee")
+    it('sort searched real estates', function () {
+        cy.get("h1").contains("integration test project")
+        createMeeting.call(this);
+        createAgenda.call(this);
+        selectEstates.call(this);
+        cy.wait(1000)
+        checkEstatesSorted();
         cy.wait(3000)
     })
 })
