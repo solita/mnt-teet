38cfc7ce9a9856a52f6d6b911f4cef5139805626
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-12-21 09:47:57 +0200

    Support adding multiple conditional rights with a map

--- app/frontend/src/cljs/teet/ui/authorization_context.cljs
+++ app/frontend/src/cljs/teet/ui/authorization_context.cljs
@@ -4,10 +4,31 @@
 (defn consume [component-fn]
   (context/consume :authorization component-fn))
 
-(defn with [added-rights child-component]
-  (context/update-context :authorization
-                          #(into (or % #{}) added-rights)
-                          child-component))
+(defn with
+  "Return child-component in a context that has added rights.
+  If added-rights is a set, those rights are directly added
+  to the context.
+
+  If added-rights is a map from keyword to truthy values,
+  the keys are conjoined/disjoined from the context based
+  on their value."
+  [added-rights child-component]
+  (context/update-context
+   :authorization
+   (fn [current-rights-set]
+     (let [rights (or current-rights-set #{})]
+       (if (map? added-rights)
+         ;; Disj/conj based on value
+         (reduce-kv (fn [r k v]
+                      (println "with auth: " k " => " v)
+                      (if v
+                        (conj r k)
+                        (disj r k)))
+                    rights added-rights)
+
+         ;; Otherwise add new rights as is
+         (into rights added-rights))))
+   child-component))
 
 (defn when-authorized [right child-component]
   (context/consume :authorization
