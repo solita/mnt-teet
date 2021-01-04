f0b494b526c35359d8b69d41b0ec310112333b54
Erno Kuusela <erno!erno-laptop>
2021-01-04 13:21:37 +0200

    generate random integration ids in migrate-2 to fix TEET-1173
    The previous migration code didn't work on databases that had gone through backup/restore as integration-id/number->uuid isn't safe to use for generating integration ids then.

--- app/backend/src/clj/teet/migration/thk_integration_id.clj
+++ app/backend/src/clj/teet/migration/thk_integration_id.clj
@@ -1,6 +1,7 @@
 (ns teet.migration.thk-integration-id
   (:require [datomic.client.api :as d]
             [clojure.set :as set]
+            [taoensso.timbre :as log]
             [teet.integration.integration-id :as integration-id]))
 
 (defn- entities-with-attr [db attr]
@@ -31,21 +32,23 @@
               '[(missing? $ ?e :integration/id)]
               ] db)))
 
+
 (defn migrate-2
-  "Migrate existing :db/id values to UUID :integration/id, for those lc/activity/task entities that are stillmissing integration id"
+  "Migrate existing :db/id values to UUID :integration/id, for those lc/activity/task entities that are still missing integration id"
   [conn]
   (let [db (d/db conn)
         lifecycles (entities-including-attr-and-missing-iid db :thk.lifecycle/id)
         activities (entities-including-attr-and-missing-iid db :activity/name)
         tasks (entities-including-attr-and-missing-iid db :task/send-to-thk?)
-        entities (set/union lifecycles activities tasks)]
-    
+        entities (set/union lifecycles activities tasks)]    
     (d/transact
      conn
      {:tx-data (vec
                 (for [e entities]
-                  {:db/id e
-                   :integration/id (integration-id/number->uuid e)}))})))
+                  (let [new-iid (integration-id/unused-random-small-uuid db)]
+                    (log/info "assigning entity" e "integration id" new-iid)
+                    {:db/id e
+                     :integration/id new-iid})))})))
 
 (defn check-uuids [db]
   (every?
