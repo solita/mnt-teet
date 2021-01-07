e64c97d03e3d74fff8f34c1e89feb571518caf07
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-01-07 17:07:13 +0200

    Refactoring after code review: using function composition instead of two maps

--- app/backend/src/clj/teet/activity/activity_db.clj
+++ app/backend/src/clj/teet/activity/activity_db.clj
@@ -185,14 +185,16 @@
 (defn activity-task-has-files?
   "Check a task under a activity has undeleted files."
   [db activity-id]
-  (println (str "activity-id is checked for tasks files " activity-id))
-  [db activity-id]
-  (seq (filter boolean
-            (map (partial teet.file.file-db/task-has-files? db)
-              (mapv :db/id (map first (d/q '[:find (pull ?t [:db/id])
-                                             :in $ ?a
-                                             :where
-                                             [?a :activity/tasks ?t]
-                                             [(missing? $ ?t :meta/deleted?)]]
-                                        db activity-id)))))))
+  (seq
+    (filter boolean
+      (map (partial teet.file.file-db/task-has-files? db)
+        (mapv
+          (comp :db/id first)
+          ;; Find all Tasks that belong to Activity
+          (d/q '[:find (pull ?t [:db/id])
+                 :in $ ?a
+                 :where
+                 [?a :activity/tasks ?t]
+                 [(missing? $ ?t :meta/deleted?)]]
+            db activity-id))))))
 
