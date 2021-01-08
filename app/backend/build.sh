7c42b53d9c8eadd146b584a2fa34ea279d0e3311
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-01-08 15:19:17 +0200

    Fix expansion

--- app/backend/build.sh
+++ app/backend/build.sh
@@ -3,4 +3,5 @@
 set -eu
 
 echo "{:git-commit \"$CODEBUILD_RESOLVED_SOURCE_VERSION\"}" > resources/build-info.edn
-clojure -Aion -m datomic.ion.dev '{:op :push :region "eu-central-1" :uname "$CODEBUILD_RESOLVED_SOURCE_VERSION"}'
+clojure -Aion -m datomic.ion.dev \
+        "{:op :push :region \"eu-central-1\" :uname \"$CODEBUILD_RESOLVED_SOURCE_VERSION\"}"
