da1dde24a01b4e68dc8142a038414033ff0ab2de
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-01-14 18:17:10 +0200

    Refactor ION config to have shorter Lambda functions names

---
+++ app/backend/src/clj/teet/notification/ion.clj
@@ -0,0 +1,10 @@
+(ns teet.notification.ion
+  "Notifications lambdas")
+
+(defn notify [_Event]
+  "Lambda function for notification ION"
+  ;;TODO: add required notifications
+  (println (str "notify called with " _Event)))
+
+
+
