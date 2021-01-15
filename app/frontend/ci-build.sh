8343ec365e1fc0e251786a0de81d2bac513ae3bf
jkorpelin <joni.korpelin@solita.fi>
2021-01-15 11:42:16 +0200

    teet-1150 proper path to localdev config

--- app/frontend/ci-build.sh
+++ app/frontend/ci-build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+echo runnit frontend test script
 npm install
 
 clojure -A:dev
 
-npx cypress open --config-file cypress-localdev.json
+npx cypress run --config-file /ci/browser-tests/cypress-localdev.json
 
 echo test failed, trying to cat /tmp/*.edn in case they contain logs:
 cat /tmp/*.edn
