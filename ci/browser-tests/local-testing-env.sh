5aa8eca9632904171d4dc4a683e5c126d8e028d3
jkorpelin <joni.korpelin@solita.fi>
2021-01-15 14:43:35 +0200

    s3-bucket-stuff

--- ci/browser-tests/local-testing-env.sh
+++ ci/browser-tests/local-testing-env.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cd ../../app/backend/
+export AWS_ACCESS_KEY_ID=$1
+export AWS_SECRET_ACCESS_KEY=$2
+export AWS_REGION=eu-central-1
+
+cd ../../app/frontend
+
+./ci-build.sh
+
+cd ../backend/
 
 clojure -A:run-browser-test &
 
