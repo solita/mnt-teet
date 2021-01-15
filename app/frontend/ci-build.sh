46b450205003abc91f2b809577e5c7cc89ab7951
jkorpelin <joni.korpelin@solita.fi>
2021-01-15 12:49:18 +0200

    teet-1150 cypress in separate step

--- app/frontend/ci-build.sh
+++ app/frontend/ci-build.sh
@@ -5,8 +5,6 @@ npm install
 
 clojure -A:prod
 
-npx cypress run --config-file /ci/browser-tests/cypress-localdev.json
-
 echo test failed, trying to cat /tmp/*.edn in case they contain logs:
 cat /tmp/*.edn
 exit 1
