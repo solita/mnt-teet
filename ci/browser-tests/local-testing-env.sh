464e4746d74b49e8e5d109052d8abedd1457f285
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-01-20 08:05:29 +0200

    Add space after value

--- ci/browser-tests/local-testing-env.sh
+++ ci/browser-tests/local-testing-env.sh
@@ -13,7 +13,7 @@ cd ../../ci/browser-tests
 npm install
 
 timeout 30s bash -c 'while ! nc -z localhost 4000; do sleep 1; done; echo Backend started ok'
-if [ $? -eq 124]; then
+if [ $? -eq 124 ]; then
     echo "Backend failed to start in 30 seconds"
     exit 1;
 fi
