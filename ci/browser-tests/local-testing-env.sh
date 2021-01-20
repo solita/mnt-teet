cec5bb9b3859c509519b7663bd9f19e534ab33d8
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-01-20 07:59:14 +0200

    Build frontend build separately

--- ci/browser-tests/local-testing-env.sh
+++ ci/browser-tests/local-testing-env.sh
@@ -4,11 +4,7 @@ export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_REGION=eu-central-1
 
-cd ../../app/frontend
-
-./ci-build.sh
-
-cd ../backend/
+cd ../../app/backend/
 
 clojure -A:run-browser-test &
 
