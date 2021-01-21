f2ea2e49bb8d0ed228b4b7a3f62d517923f70602
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-01-21 15:23:03 +0200

    Return something useful from db query and add asset db

--- app/backend/src/clj/teet/system/system_queries.clj
+++ app/backend/src/clj/teet/system/system_queries.clj
@@ -1,6 +1,8 @@
 (ns teet.system.system-queries
   (:require [teet.db-api.core :as db-api :refer [defquery]]
-            [teet.util.build-info :as build-info]))
+            [teet.util.build-info :as build-info]
+            [teet.environment :as environment]
+            [datomic.client.api :as d]))
 
 (defn db-state-response [_db-result]
   (with-meta
@@ -8,16 +10,14 @@
      :body (build-info/git-commit)}
     {:format :raw}))
 
-;; The actual query here is irrelevant. This provides an endpoint for
-;; checking whether the db is alive.
+;; Return db stats datom counts for teet and asset (if enabled) databases
 (defquery :teet.system/db
   {:doc "Check database status"
    :context {db :db}
    :spec empty?
    :args _
    :unauthenticated? true}
-  {:query '[:find ?e
-            :where
-            [?e :thk.project/project-name "non-existent"]]
-   :args [db]
-   :result-fn db-state-response})
+  (merge
+   {:teet (:datoms (d/db-stats db))}
+   (when (environment/feature-enabled? :asset-db)
+     {:asset (:datoms (d/db-stats (environment/asset-db)))})))
