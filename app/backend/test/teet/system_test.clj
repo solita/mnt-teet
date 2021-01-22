88bec6024aef95a38d41b40f0c639403f38c572b
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-01-22 10:21:41 +0200

    Run with :asset-db feature flag

--- app/backend/test/teet/system_test.clj
+++ app/backend/test/teet/system_test.clj
@@ -7,9 +7,11 @@
 (use-fixtures :once tu/with-environment (tu/with-db))
 
 (deftest db-query
-  (let [migrated-atom @#'teet.environment/asset-db-migrated?]
-    (is (not @migrated-atom) "asset db not migrated before")
-    (testing ":teet.system/db can be called by anonymous user"
-      (is (= (:status (tu/local-query nil :teet.system/db {}))
-             200))
-      (is @migrated-atom "Asset db was initialized by system query"))))
+  (tu/run-with-config
+   {:enabled-features #{:asset-db}}
+   (let [migrated-atom @#'teet.environment/asset-db-migrated?]
+     (is (not @migrated-atom) "asset db not migrated before")
+     (testing ":teet.system/db can be called by anonymous user"
+       (is (= (:status (tu/local-query nil :teet.system/db {}))
+              200))
+       (is @migrated-atom "Asset db was initialized by system query")))))
