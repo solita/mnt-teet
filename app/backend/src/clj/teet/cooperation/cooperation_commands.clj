321ffaa1085c7487a820e3dbd625fd188db5898b
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-01-22 11:12:06 +0200

    Add check for empty users in new notification

--- app/backend/src/clj/teet/cooperation/cooperation_commands.clj
+++ app/backend/src/clj/teet/cooperation/cooperation_commands.clj
@@ -75,14 +75,19 @@
     (= response-id current-response-id)))
 
 (defn application-response-notification-tx
+  "Add notification about new Application response,
+  return a notification transaction map or empty map if receiver or sender is empty"
   [db user activity-user project application-id]
-  (notification-db/notification-tx
-    db
-    {:from user
-     :to activity-user
-     :target application-id
-     :type :notification.type/cooperation-response-to-application-added
-     :project project}))
+  (if
+    (and
+      (some? user)
+      (some? activity-user))
+    (notification-db/notification-tx
+      db {:from user
+          :to activity-user
+          :target application-id
+          :type :notification.type/cooperation-response-to-application-added
+          :project project}) {}))
 
 (defcommand :cooperation/save-application-response
   {:doc "Create a new response to the application"
