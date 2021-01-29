28228969e451d07d5be39dd78ec8153306ee5756
jkorpelin <joni.korpelin@solita.fi>
2021-01-29 15:39:16 +0200

    TEET-1158 fixes indentation

--- app/backend/src/clj/teet/account/account_queries.clj
+++ app/backend/src/clj/teet/account/account_queries.clj
@@ -3,11 +3,11 @@
             [teet.db-api.core :as db-api :refer [defquery]]))
 
 (defquery :account/account-page
-          {:doc "query information about the current logged in user"
-           :context {db :db
-                     user :user}
-           :unauthenticated? true
-           :args {}}
-          (d/pull db '[:user/phone-number :user/email
-                       :user/given-name :user/family-name]
-                  (:db/id user)))
+  {:doc "query information about the current logged in user"
+   :context {db :db
+             user :user}
+   :unauthenticated? true
+   :args {}}
+  (d/pull db '[:user/phone-number :user/email
+               :user/given-name :user/family-name]
+          (:db/id user)))
