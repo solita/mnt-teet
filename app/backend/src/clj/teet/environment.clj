287273e9e64b8b98d63c35c127aa8a2011030408
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-02-03 08:31:01 +0200

    Add email subject prefix parameter

--- app/backend/src/clj/teet/environment.clj
+++ app/backend/src/clj/teet/environment.clj
@@ -126,7 +126,8 @@
            :instance-id (->ssm [:xroad-instance-id] nil)
            :kr-subsystem-id (->ssm [:xroad-kr-subsystem-id] nil)}
    :eelis {:wms-url (->ssm [:eelis :wms-url] nil)}
-   :email {:from (->ssm [:email :from] nil)}})
+   :email {:from (->ssm [:email :from] nil)
+           :subject-prefix (->ssm [:email :subject-prefix] nil)}})
 
 (defn- load-ssm-config! [base-config]
   (let [old-config @config
