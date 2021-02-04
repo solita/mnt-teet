2757f18121760e1eeb7c7d0065178a89a278965e
jkorpelin <joni.korpelin@solita.fi>
2021-02-04 09:20:57 +0200

    TEET-1221 removes not needed code to handle non teet user emails

--- app/backend/src/clj/teet/meeting/meeting_tx.clj
+++ app/backend/src/clj/teet/meeting/meeting_tx.clj
@@ -24,21 +24,8 @@
                           participant :participation/participant
                           :as participation}]
   (let [teet-user? (not (string? (:db/id participant)))]
-    (if (or
-         ;; If user is TEET user, check that they are not already participating
-         (and teet-user?
-              (meeting-db/user-is-participating? db participant meeting))
-
-         ;; Check that email is not in use by some participant
-         (let [emails (into #{}
-                            (comp
-                             (map :user/email)
-                             (remove nil?)
-                             (map str/lower-case))
-                            (meeting-db/participants db meeting))
-               new-email (:user/email participant)]
-           ;; If some participant already has the email being added
-           (and new-email (emails (str/lower-case new-email)))))
+    (if (and teet-user?
+             (meeting-db/user-is-participating? db participant meeting))
       (ion/cancel {:cognitect.anomalies/category :cognitect.anomalies/conflict
                    :cognitect.anomalies/message "User is already participating"
                    :teet/error :user-is-already-participant})
