a8da027f0f9393cc70ca14525103352fcc6d8c40
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-02-09 16:56:01 +0200

    Fix test after code review

--- app/backend/test/teet/integration/integration_email_test.clj
+++ app/backend/test/teet/integration/integration_email_test.clj
@@ -4,9 +4,9 @@
             [teet.test.utils :as tu]
             [clojure.string :as str]))
 
-(def outbox (atom [:server {} :msg {}]))
+(def outbox (atom []))
 
-(defn- update-outbox [server msg] (swap! outbox conj server msg))
+(defn- update-outbox [server msg] (swap! outbox conj {:server server :msg msg}))
 
 (defn with-outbox []
   (fn [tests]
@@ -31,20 +31,20 @@
    {:email {:subject-prefix "[TESTING]"}}
    (integration-email/send-email! test-message)
    (is (= "[TESTING] Hello there"
-          (:subject (second @outbox))))))
+         (:subject (:msg (first @outbox)))))))
 
 (deftest email-subject-no-prefix
   (tu/run-with-config
    {:email {:subject-prefix nil}}
    (integration-email/send-email! test-message)
    (is (= "Hello there"
-          (:subject (second @outbox))))))
+          (:subject (:msg (first @outbox)))))))
 
 (deftest data-protection-footer
   (tu/run-with-config
    {:email {:contact-address "FOO@EXAMPLE.COM"}}
    (integration-email/send-email! test-message)
-   (let [txt (get-in @outbox [1 :parts 0 :body])]
+   (let [txt (get-in @outbox [0 :msg :parts 0 :body])]
      (is (str/includes? txt "e-posti aadressiga FOO@EXAMPLE.COM")
          "body contains estonian footer")
      (is (str/includes? txt "e-mail address FOO@EXAMPLE.COM")
@@ -54,7 +54,7 @@
   (tu/run-with-config
     {:email test-smtp-config}
     (integration-email/send-email! test-message)
-    (let [smtp-conf (first @outbox)]
+    (let [smtp-conf (:server (first @outbox))]
       (is (str/includes? (:host smtp-conf) "localhost")
         "SMTP configuration contains host name")
       (is (str/includes? (:user smtp-conf) "user")
