b7a2260af26aec49cd9d84c655f589c5fdf51790
Ville Outamaa <ville.outamaa@solita.fi>
2021-02-09 20:12:13 +0200

    TEET-1261 Delete application with tx function

--- app/backend/src/clj/teet/cooperation/cooperation_tx.clj
+++ app/backend/src/clj/teet/cooperation/cooperation_tx.clj
@@ -1,6 +1,7 @@
 (ns teet.cooperation.cooperation-tx
   (:require [datomic.client.api :as d]
-            [datomic.ion :as ion]))
+            [datomic.ion :as ion]
+            [teet.meta.meta-model :as meta-model]))
 
 (defn create-3rd-party [db {:cooperation.3rd-party/keys [project name] :as tp}]
   (if (seq (d/q '[:find ?tp
@@ -14,3 +15,8 @@
       :cognitect.anomalies/message "This 3rd party already exists in project"
       :teet/error :third-party-already-exists})
     [tp]))
+
+(defn delete-application
+  [db user application-id]
+  ;; TODO User can delete a cooperation application if the involved party response has not been added to the application
+  [(meta-model/deletion-tx user application-id)])
