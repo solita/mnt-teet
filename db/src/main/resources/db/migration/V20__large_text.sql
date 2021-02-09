796a12df99953a7c7cc1cacfa152c51e71b1755a
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-02-09 15:40:40 +0200

    Change migration, use sha256 content addressible storage

--- db/src/main/resources/db/migration/V20__large_text.sql
+++ db/src/main/resources/db/migration/V20__large_text.sql
@@ -1,3 +1,20 @@
-CREATE TABLE teet.large_text (id UUID PRIMARY KEY, text TEXT);
+CREATE TABLE teet.large_text (hash bytea PRIMARY KEY, text TEXT);
+
+-- Function to upsert new text, returns hash
+CREATE FUNCTION teet.store_large_text(text TEXT) RETURNS TEXT
+AS $$
+INSERT INTO teet.large_text (hash,text)
+VALUES (sha256(convert_to($1::TEXT,'UTF-8')), $1)
+ON CONFLICT (hash) DO UPDATE SET hash=large_text.hash
+RETURNING encode(sha256(convert_to($1::TEXT,'UTF-8')),'hex');
+$$ LANGUAGE SQL;
+
+-- Function to fetch text by hash
+CREATE FUNCTION teet.fetch_large_text(hash TEXT) RETURNS TEXT
+AS $$
+SELECT lt.text FROM teet.large_text lt WHERE lt.hash=decode($1, 'hex')
+$$ LANGUAGE SQL IMMUTABLE;
 
 GRANT ALL ON TABLE teet.large_text TO teet_backend;
+GRANT EXECUTE ON FUNCTION teet.store_large_text(TEXT) TO teet_backend;
+GRANT EXECUTE ON FUNCTION teet.fetch_large_text(TEXT) TO teet_backend;
