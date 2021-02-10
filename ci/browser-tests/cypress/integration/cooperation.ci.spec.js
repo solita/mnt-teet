28175feb5305c6685216e22c097f56f9e044660a
jkorpelin <joni.korpelin@solita.fi>
2021-02-10 09:37:25 +0200

    TEET-1277 cypress tests for new cooperation search

--- ci/browser-tests/cypress/integration/cooperation.ci.spec.js
+++ ci/browser-tests/cypress/integration/cooperation.ci.spec.js
@@ -7,20 +7,34 @@ context('Cooperation', function() {
             .then((response) => {
                 cy.wrap(response.body["project-id"]).as("projectID")
             })
+    });
 
+    beforeEach(() => {
         cy.dummyLogin("Danny")
         cy.randomName("thirdparty", "testcompany")
 
         const now = new Date()
         cy.wrap(now.toLocaleDateString("et-EE")).as("today")
         cy.wrap(new Date(now.getTime() + 1000 * 60 * 60 * 24 * 14).toLocaleDateString("et-EE")).as("twoWeeks")
-
     })
 
-    it("Cooperation workflow", function() { // use function instead of fat arrow because we use "this"
+    function createCooperation(name) {
+        cy.get("button.new-third-party").click()
 
+        cy.formInput(
+            ":cooperation.3rd-party/name", name,
+            ":cooperation.3rd-party/id-code", "123456",
+            ":cooperation.3rd-party/email", "test@example.com",
+            ":cooperation.3rd-party/phone", "555-1234-567890")
+
+        cy.formSubmit()
+
+        cy.get(".MuiSnackbar-root") // snackbar message is shown
+    }
+
+    function navigateToProjectCooperations(projectId) {
         // visit project page
-        cy.visit("#/projects/"+this.projectID)
+        cy.visit("#/projects/"+projectId)
 
         // open menu and select cooperation
         cy.get("button.project-menu").click({force: true})
@@ -29,6 +43,11 @@ context('Cooperation', function() {
         cy.get(".cooperation-overview-page")
 
         // Create new third party
+    }
+
+    it("Cooperation workflow", function() { // use function instead of fat arrow because we use "this"
+
+        navigateToProjectCooperations(this.projectID);
 
         cy.get("button.new-third-party").click()
 
@@ -136,4 +155,22 @@ context('Cooperation', function() {
 
     })
 
+
+    it("Cooperation search filtering should work", function () {
+        const newCompanyName = "xxxx"
+        navigateToProjectCooperations(this.projectID);
+        createCooperation(newCompanyName);
+
+        cy.formInput(
+            ":third-party-name", "x");
+
+        cy.get(`.cooperation-overview-page [data-third-party='${newCompanyName}'] a`).should("be.visible");
+
+        // Add non matching string to the name search and the company should not me found
+        cy.formInput(
+            ":third-party-name", "abcd");
+
+        cy.get(`.cooperation-overview-page [data-third-party='${newCompanyName}'] a`).should("not.exist");
+
+    })
 })
