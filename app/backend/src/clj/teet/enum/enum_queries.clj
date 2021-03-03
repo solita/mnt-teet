f2c5dd55134e00dae4ceecb9f617eb7c033c8afd
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-03-03 10:50:27 +0200

    Support fetching enum values from asset db as well

--- app/backend/src/clj/teet/enum/enum_queries.clj
+++ app/backend/src/clj/teet/enum/enum_queries.clj
@@ -1,20 +1,24 @@
 (ns teet.enum.enum-queries
   "Fetch enumeration values"
   (:require [teet.db-api.core :as db-api :refer [defquery]]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [teet.environment :as environment]))
 
 
 (s/def ::attribute keyword?)
 (defquery :enum/values
   {:doc "Fetch values for given enumeration"
    :context {db :db}
-   :args {:keys [attribute]}
+   :args {:keys [attribute database]}
    :spec (s/keys :req-un [::attribute])
-   
+
    :project-id nil
    :authorization {}}
+
   {:query '{:find [(pull ?e [*])]
             :in [$ ?attr]
             :where [[?e :enum/attribute ?attr]]}
-   :args [db attribute]
+   :args [(if (and (= :asset database)
+                   (environment/feature-enabled? :asset-db))
+            (environment/asset-db) db) attribute]
    :result-fn (partial mapv first)})
