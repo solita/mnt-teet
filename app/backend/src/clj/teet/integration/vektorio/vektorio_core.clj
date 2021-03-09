7e5cdc700e0284631c487c0b1a6e261ae2cb5e7f
jkorpelin <joni.korpelin@solita.fi>
2021-03-09 16:42:48 +0200

    TEET-1255 removes "teet" from vektorio project name

--- app/backend/src/clj/teet/integration/vektorio/vektorio_core.clj
+++ app/backend/src/clj/teet/integration/vektorio/vektorio_core.clj
@@ -24,7 +24,7 @@
         project (d/pull db [:db/id :thk.project/name :thk.project/project-name :thk.project/id] project-eid)
         project-name (or (:thk.project/project-name project)
                          (:thk.project/name project))
-        project-name-for-vektor (str "TEET " project-name " (THK" (:thk.project/id project) "#)")
+        project-name-for-vektor (str project-name " (THK" (:thk.project/id project) ")")
         resp (vektorio-client/create-project! vektor-config {:name project-name-for-vektor})
         vektorio-project-id (str (:id resp))
         vektorio-user-id (get-or-create-user! vektor-config)]
