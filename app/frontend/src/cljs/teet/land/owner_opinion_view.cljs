29d75aa877dfb39c4da399591cfe0481b9fd8a4b
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-03-17 14:11:56 +0200

    Editing form is added, at the moment support small body only.

--- app/frontend/src/cljs/teet/land/owner_opinion_view.cljs
+++ app/frontend/src/cljs/teet/land/owner_opinion_view.cljs
@@ -30,17 +30,20 @@
    :border-bottom (str "1px solid " theme-colors/border-dark)
    :margin-bottom "1rem"})
 
+(defn get-activities [project]
+  (let [skip-activities #{:activity.name/warranty :activity.name/land-acquisition}]
+    (into []
+      (comp
+        (mapcat :thk.lifecycle/activities)
+        (remove (comp skip-activities :activity/name)))
+      (:thk.project/lifecycles
+        (du/idents->keywords project)))))
+
 (defn owner-opinion-form
   [e! project target]
   (r/with-let [form-state (r/atom {})
                form-change (form/update-atom-event form-state merge)
-               skip-activities #{:activity.name/warranty :activity.name/land-acquisition}
-               activities (into []
-                                (comp
-                                  (mapcat :thk.lifecycle/activities)
-                                  (remove (comp skip-activities :activity/name)))
-                                (:thk.project/lifecycles
-                                  (du/idents->keywords project)))]
+               activities (get-activities project)]
     [:div {:style {:padding "0.5rem"
                    :overflow "hidden"}}
      [:div {:class (<class add-opinion-heading-style)}
@@ -115,6 +118,8 @@
    :border-color theme-colors/border-dark
    :padding "1rem"})
 
+(def open? (r/atom false))
+
 (defn opinion-view-container
   [{:keys [content text-color open? heading heading-button on-toggle-open]
     :or {open? false}} bg-color]
@@ -152,51 +157,127 @@
   ;; TODO: implement
   )
 
-(defn opinion-form []
-  ;; TODO: implement
-  )
+(defn opinion-form [e! form project target close-event]
+  (r/with-let [form-state
+               (r/atom (select-keys
+                         @form [:db/id
+                                :land-owner-opinion/body
+                                :land-owner-opinion/type
+                                :land-owner-opinion/date
+                                :land-owner-opinion/respondent-connection-to-land
+                                :land-owner-opinion/authority-position
+                                :land-owner-opinion/respondent-name
+                                :land-owner-opinion/link-to-response]))
+               form-change (form/update-atom-event form-state merge)
+               activities (get-activities project)]
+    (println @form-state)
+    [:<>
+     [form/form2 {:e! e!
+                  :on-change-event form-change
+                  :save-event #(common-controller/->SaveForm
+                                 :land-owner-opinion/save-opinion
+                                 {:form-data (common-controller/prepare-form-data (form/to-value @form-state))
+                                  :project-id (:db/id project)
+                                  :land-unit-id target}
+                                 (fn [_response]
+                                   (fn [e!]
+                                     (e! (opinion-controller/->OpinionFormClose)))))
+                  :value @form-state
+                  :cancel-event close-event
+                  :spec :land-owner-opinion/form}
+      [Grid {:container true
+             :spacing 1}
+       [Grid {:item true
+              :md 4
+              :xs 12}
+        [form/field :land-owner-opinion/activity
+         [select/form-select {:show-empty-selection? true
+                              :items (mapv #(select-keys % [:db/id :activity/name]) activities)
+                              :format-item (comp tr-enum :activity/name)}]]]
+       [Grid {:item true
+              :md 4
+              :xs 12}
+        [form/field :land-owner-opinion/respondent-name
+         [TextField {}]]]
+       [Grid {:item true
+              :md 4
+              :xs 12}
+        [form/field :land-owner-opinion/date
+         [date-picker/date-input {}]]]
+       [Grid {:item true
+              :md 4
+              :xs 12}
+        [form/field :land-owner-opinion/type
+         [select/select-enum {:e! e! :attribute :land-owner-opinion/type}]]]
+       [Grid {:item true
+              :md 4
+              :xs 12}
+        [form/field :land-owner-opinion/respondent-connection-to-land
+         [TextField {}]]]
+       [Grid {:item true
+              :md 4
+              :xs 12}
+        [form/field :land-owner-opinion/link-to-response
+         [TextField {}]]]
+       [Grid {:item true
+              :md 6
+              :xs 12}
+        [form/field :land-owner-opinion/body
+         [rich-text-editor/rich-text-field {}]]]
+       [Grid {:item true
+              :md 6
+              :xs 12}
+        [form/field :land-owner-opinion/authority-position
+         [rich-text-editor/rich-text-field {}]]]
+       [Grid {:item true
+              :xs 12}
+        [form/footer2]]]]]))
 
 (defn opinion-content [e! {id :db/id
-                           body :land-owner-opinion/body}
+                           body :land-owner-opinion/body
+                           authority-position :land-owner-opinion/authority-position}
                        edit-right? editing?]
   [:div {:id (str "opinion-" id)}
-   (if (and body (not editing?))
+   (if (not editing?)
      [:div
-      [rich-text-editor/display-markdown body]]
+      [:div [rich-text-editor/display-markdown body]]
+      [:div [rich-text-editor/display-markdown authority-position]]]
      (println "opinion-content is hidden"))])
 
 (defn owner-opinion-details
   [e! {:keys [edit-rights?]}
    {id :db/id
-    :land-owner-opinion/keys [respondent-connection-to-land]
-    :as opinion}]
+    :land-owner-opinion/keys [respondent-connection-to-land body authority-position]
+    :as opinion}
+   project
+   target]
   (r/with-let [seen-at (date/start-of-today)
                [pfrom pto] (common/portal)
-               edit-open-atom (r/atom false)]
+               edit-open-atom (r/atom false)
+               form-data (r/atom opinion)]
     [opinion-view-container
      {:heading [owner-opinion-heading seen-at opinion]
       :on-toggle-open #(e! (println "Opinion view toggled"))
       :open? true
       :heading-button [form/form-container-button
-                          {:form-component [opinion-form e! opinion]
-                           :container pfrom
-                           :open-atom edit-open-atom
-                           :id (str "edit-opinion-" id)
-                           :form-value (select-keys opinion [:land-owner-opinion/body
-                                                             :land-owner-opinion/topic :db/id
-                                                             :land-owner-opinion/respondent-connection-to-land])
-                           :button-component [buttons/button-secondary {:size :small}
-                                              (tr [:buttons :edit])]}]
+                       {:form-component [opinion-form e! form-data project target]
+                        :container pfrom
+                        :open-atom edit-open-atom
+                        :id (str "edit-opinion-" id)
+                        :form-value (select-keys @form-data [:db/id
+                                                             :land-owner-opinion/body
+                                                             :land-owner-opinion/type
+                                                             :land-owner-opinion/date
+                                                             :land-owner-opinion/respondent-connection-to-land
+                                                             :land-owner-opinion/authority-position
+                                                             :land-owner-opinion/respondent-name
+                                                             :land-owner-opinion/link-to-response])
+                        :button-component [buttons/button-secondary {:size :small}
+                                           (tr [:buttons :edit])]}]
       :content
       [:<>
        [pto]
-       [opinion-content e! opinion edit-rights? @edit-open-atom]]
-      :children []
-      :after-children-component nil}]))
-
-(def open? (r/atom false))
-
-(defn toggle-open [] (swap! open? not))
+       [opinion-content e! @form-data edit-rights? @edit-open-atom]]}]))
 
 (defn owner-opinion-row
   [e! project target opinion rights]
@@ -209,7 +290,7 @@
       [typography/SmallText {:style {:padding-left "0.25rem"
                                      :display :inline}}
        (:land-owner-opinion/respondent-connection-to-land opinion)]
-      [owner-opinion-details e! authorization opinion]]]))
+      [owner-opinion-details e! authorization opinion project target]]]))
 
 (defn owner-opinions-list
   [e! project unit target opinions]
