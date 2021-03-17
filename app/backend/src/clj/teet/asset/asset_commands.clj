0196d3c6d3a2e55b7e8f7acde91f249f9af7c3c8
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-03-17 13:31:00 +0200

    Move atl code around

--- app/backend/src/clj/teet/asset/asset_commands.clj
+++ app/backend/src/clj/teet/asset/asset_commands.clj
@@ -8,28 +8,7 @@
             [teet.asset.asset-type-library :as asset-type-library]))
 
 
-(defn- prepare-asset-form-data
-  "Prepare data from asset form to be saved in the database"
-  [adb form-data]
-  (def *adb adb)
-  (let [rotl (asset-type-library/rotl-map
-              (asset-db/asset-type-library adb))]
 
-    (walk/prewalk
-     (fn [x]
-       (if-let [attr (and (map-entry? x)
-                          (get rotl (first x)))]
-         (case (get-in attr [:db/valueType :db/ident])
-           :db.type/bigdec
-           (update x 1 bigdec)
-
-           :db.type/long
-           (update x 1 #(Long/parseLong %))
-
-           ;; No parsing
-           x)
-         x))
-     form-data)))
 
 (defcommand :asset/save-cost-item
   {:doc "Create/update an asset cost item"
@@ -40,8 +19,9 @@
    :project-id [:thk.project/id project-id]
    :authorization {:cost-items/edit-cost-items {}}}
   (let [asset (merge {:asset/project project-id}
-                     (prepare-asset-form-data adb asset))]
-    (def *asset asset)
+                     (asset-type-library/form->db
+                      (asset-type-library/rotl-map (asset-db/asset-type-library adb))
+                      asset))]
     (:tempids
      (d/transact aconn
                  {:tx-data [asset]}))))
