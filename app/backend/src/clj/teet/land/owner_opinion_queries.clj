bfb1f5a25a17638cff77e41b67a96d9899d89bab
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-03-17 17:01:02 +0200

    Add large text pulling in opinions read query

--- app/backend/src/clj/teet/land/owner_opinion_queries.clj
+++ app/backend/src/clj/teet/land/owner_opinion_queries.clj
@@ -1,6 +1,14 @@
 (ns teet.land.owner-opinion-queries
   (:require [teet.db-api.core :refer [defquery]]
-            [teet.land.owner-opinion-db :as owner-opinion-db]))
+            [teet.land.owner-opinion-db :as owner-opinion-db]
+            [teet.db-api.db-api-large-text :as db-api-large-text]
+            [teet.land.owner-opinion-commands :as owner-opinion-commands]))
+
+(defn- pull-large-texts [opinions]
+  (mapv (partial
+          db-api-large-text/with-large-text
+          owner-opinion-commands/land-owner-opinion-rich-text-fields)
+    opinions))
 
 (defquery :land-owner-opinion/fetch-opinions
   {:doc "Fetch all land owner opinions for a project and unit"
@@ -9,7 +17,7 @@
           land-unit-id :land-unit-id}
    :project-id [:thk.project/id project-id]
    :authorization {:land/view-land-owner-opinions {}}}
-  (owner-opinion-db/owner-opinions db project-id land-unit-id))
+  (pull-large-texts (owner-opinion-db/owner-opinions db project-id land-unit-id)))
 
 
 (defquery :land-owner-opinion/opinions-count
