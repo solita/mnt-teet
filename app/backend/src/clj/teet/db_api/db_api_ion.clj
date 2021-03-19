64d5e12de6d33b09891e82781f830bbaad98e35d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-03-19 09:10:07 +0200

    Add docstring so we remember why we use this instead of delay

--- app/backend/src/clj/teet/db_api/db_api_ion.clj
+++ app/backend/src/clj/teet/db_api/db_api_ion.clj
@@ -34,7 +34,13 @@
       cookies/wrap-cookies
       wrap-exception-alert))
 
-(defn cached [value-fn]
+(defn cached
+  "Return function to get deferred value. Succesfully created
+  value is cached and returned with subsequent calls.
+
+  If value-fn throws exception when creating value, it isn't cached.
+  Use this instead of delay if value-fn can have intermittent failures."
+  [value-fn]
   (let [val (atom nil)]
     (fn []
       (if-let [v @val]
