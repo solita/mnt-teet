93c75ceba26b959e4c2250bf4d6f1b257ca5b37f
aleksievelizhanin <Aleksei.velizanin@solita.fi>
2021-03-26 09:02:58 +0200

    Teet 1357 land owner opinion details and teet 1358 edit and delete land owner opinions (#352)
    * Add Open/Close button for Owner Opinion
    
    * Open / Edit button added, partially functioning
    
    * Editing form is added, at the moment support small body only.
    
    * Add large text pulling in opinions read query
    
    * Close opinion rows by default
    
    * Close update opinion form on Save click
    
    * Fix the pre-selection of Opinion activity by matching the form data and the UI component expectation
    
    * Refactoring
    
    * Fix the header of the opinion row
    
    * Fix rich editor changes saving. Save click flickering removed.
    
    * Add Activity name and Opinion Type for the row of opinion details
    
    * Add response body and authority position to the preview accordeon
    
    * Fix read only ui using proper layout Grid-s
    
    * Add refresh! callback to re-run the query after update
    
    * Fix the bug in saving changes in rich text editor fields: response and authority position
    
    * Translations added to some labels
    
    * Fix merge conflicts
    
    * Remove unnecessary changes
    
    * Vunlnerablity lock added
    
    * Fix add new opinion button
    
    * Fix sending actual opinion from backend to display markdown
    
    * Remove unnecessary triangle icon from opinion details container style
    
    * Add one more lock
    
    * Minor reformatting changes
    
    * Use header row component for meeting opinion header
    
    * Cosmetic L&F changes in opinion details read only view
    
    * Add creator/modifier to Opinion details view
    
    * Delete working
    
    * TEET-1358 Fix page flickering after opinion delete
    
    * Duplicated nvd lock removed
    
    * Minor change after code review
    
    * Remove duplicate
    
    * Add delete opinion confirmation localizations
    
    * Set correct delete confirmation messages
    
    * Add snak confirmation messages to opinion delete and update
    
    * Hide header when editing
    
    * Minor changes
    
    * Add header 'Change opinion' when editing opinion
    
    * Add more margin undo Add new opinion button and change primary color as designed
    
    * Hide Close button if Edit is in progress

--- app/backend/src/clj/teet/land/owner_opinion_queries.clj
+++ app/backend/src/clj/teet/land/owner_opinion_queries.clj
@@ -1,8 +1,16 @@
 (ns teet.land.owner-opinion-queries
   (:require [teet.db-api.core :refer [defquery]]
             [teet.land.owner-opinion-db :as owner-opinion-db]
+            [teet.db-api.db-api-large-text :as db-api-large-text]
+            [teet.land.owner-opinion-commands :as owner-opinion-commands]
             [teet.land.owner-opinion-export :as owner-opinion-export]
             [teet.project.project-db :as project-db]))
+
+(defn- pull-large-texts [opinions]
+  (mapv (partial
+          db-api-large-text/with-large-text
+          owner-opinion-commands/land-owner-opinion-rich-text-fields)
+    opinions))
 
 (defquery :land-owner-opinion/fetch-opinions
   {:doc "Fetch all land owner opinions for a project and unit"
@@ -11,7 +19,7 @@
           land-unit-id :land-unit-id}
    :project-id [:thk.project/id project-id]
    :authorization {:land/view-land-owner-opinions {}}}
-  (owner-opinion-db/owner-opinions db project-id land-unit-id))
+  (pull-large-texts (owner-opinion-db/owner-opinions db project-id land-unit-id)))
 
 
 (defquery :land-owner-opinion/opinions-count
