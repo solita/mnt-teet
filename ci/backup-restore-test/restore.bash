51e1f7825f2e1f8be3e0fdc80e90265457be753e
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-16 17:17:07 +0300

    Call restore

--- ci/backup-restore-test/restore.bash
+++ ci/backup-restore-test/restore.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Check started"
+echo "Restore started"
 
 export min_backup_size=1000000
 export BACKUP_SIZE=$(aws s3 ls s3://teet-dev2-documents | grep "backup" | grep $(date +%Y-%m-%d) | awk '{print $3}')
@@ -8,15 +8,16 @@ echo $BACKUP_SIZE
 
 if (( $BACKUP_SIZE < $min_backup_size)); then
   echo "Backup is too small"
-  exit(1);
-else
-  echo "Backup size is OK"
+  exit 1;
 fi
 
 export BACKUP_FILE_NAME=$(aws s3 ls s3://teet-dev2-documents | grep "backup" | grep $(date +%Y-%m-%d) | awk '{print $4}')
 
 echo $BACKUP_FILE_NAME
 
+aws lambda invoke --function-name teet-datomic-Compute-restore --payload "{:input '{\"bucket\":\"teet-dev-documents\",\"file-key\":$BACKUP_FILE_NAME}'}" out
+
+echo "Restore completed"
 
 
-echo "Check completed"
+
