84aebc2c8a4cbe2c2cd75a41073361bf40dd7477
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-16 15:34:39 +0300

    Debugging

--- ci/backup-restore-test/backup.bash
+++ ci/backup-restore-test/backup.bash
@@ -3,7 +3,13 @@
 echo "Backup started"
 
 aws lambda invoke --function-name teet-datomic-Compute-backup --payload '{"key": "value"}' out
+
+echo out
+
 sed -i'' -e 's/"//g' out
+
+echo out
+
 sleep 15
 aws logs get-log-events --log-group-name /aws/lambda/teet-datomic-Compute-backup --log-stream-name $(cat out) --limit 5
 
