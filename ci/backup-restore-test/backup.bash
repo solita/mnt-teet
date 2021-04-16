8cae20ca0bba0df5a07031301eb02581c5f79f45
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-16 16:13:30 +0300

    Add outfile back to lambda invoke

--- ci/backup-restore-test/backup.bash
+++ ci/backup-restore-test/backup.bash
@@ -2,7 +2,10 @@
 
 echo "Backup started"
 
-aws lambda invoke --function-name teet-datomic-Compute-backup --payload '{"key": "value"}'
+aws lambda invoke --function-name teet-datomic-Compute-backup --payload '{"key": "value"}' out
+
+sed -i'' -e 's/"//g' out
+echo $(cat out)
 
 sleep 15
 
