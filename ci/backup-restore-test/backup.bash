d2edab246343a399b3fecf0ab35e953c10e8b0a6
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-16 13:47:49 +0300

    Debugging

--- ci/backup-restore-test/backup.bash
+++ ci/backup-restore-test/backup.bash
@@ -4,10 +4,7 @@ echo "Backup started"
 
 # create backup using existing default config
 clojure -A:dev - <<EOF
-(teet.environment/load-local-config!)
-(require '[teet.backup.backup-ion :as bu])
 (println "Backup is here")
-
 (System/exit 0)
 EOF
 
