71184a92ac06cbe7ca114b11320fbae11ebb7991
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-16 13:30:28 +0300

    Call backup lambda function

--- ci/backup-restore-test/backup.bash
+++ ci/backup-restore-test/backup.bash
@@ -2,6 +2,13 @@
 
 echo "Backup started"
 
-# TODO: call the backup lambda
+# create backup using existing default config
+clojure -A:dev - <<EOF
+(teet.environment/load-local-config!)
+(require '[teet.backup.backup-ion :as bu])
+(bu/backup* {:input "{\"bucket\":\"teet-dev-documents\",\"file-key\":\"teet-dev-backup-Fri Apr 16 13:00:00 EEST 2021.edn.zip\"}"})
+
+(System/exit 0)
+EOF
 
 echo "Backup completed"
