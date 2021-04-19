b75d657613aff518811e1376e2c8dddd3b196961
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-19 13:50:02 +0300

    Add placeholder for check of new restored DB

--- ci/backup-restore-test/backup.bash
+++ ci/backup-restore-test/backup.bash
@@ -4,9 +4,6 @@ echo "Backup started"
 
 aws lambda invoke --function-name teet-datomic-Compute-backup --payload '{"key": "value"}' out
 
-sed -i'' -e 's/"//g' out
 echo $(cat out)
-
-sleep 15
 
 echo "Backup completed"
---
+++ ci/backup-restore-test/check.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Check started"
+
+# TODO: implement some queries in restored DB
+
+echo "Check completed"
