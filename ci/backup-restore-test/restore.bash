da1dc1cc101ec8fcf83452e458b0a4668f7c382c
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-20 18:14:03 +0300

    Fix log file name

--- ci/backup-restore-test/restore.bash
+++ ci/backup-restore-test/restore.bash
@@ -17,13 +17,17 @@ fi
 export S3_BUCKET="teet-dev2-documents"
 export BACKUP_FILE_NAME=$(aws s3 ls s3://teet-dev2-documents | grep "backup" | grep $(date +%Y-%m-%d) | awk '{print $4}')
 
-echo $BACKUP_FILE_NAME
+echo "Backup file name " $BACKUP_FILE_NAME
 
-#aws lambda invoke --function-name teet-datomic-Compute-restore --payload "{
-#  \"bucket\":\"teet-dev2-documents\",
-#  \"file-key\":\"$BACKUP_FILE_NAME\",
-#  \"create-database\":\"$RESTORE_DB_NAME\",
-#  \"create-asset-database\":\"$RESTORE_ASSET_DB_NAME\"}" out
+aws lambda invoke --function-name teet-datomic-Compute-restore --payload "{
+  \"bucket\":\"teet-dev2-documents\",
+  \"file-key\":\"$BACKUP_FILE_NAME\",
+  \"create-database\":\"$RESTORE_DB_NAME\",
+  \"create-asset-database\":\"$RESTORE_ASSET_DB_NAME\"}" out
+
+export BACKUP_LOG_NAME=$BACKUP_FILE_NAME".log"
+
+echo "Restore log file name " $BACKUP_LOG_NAME
 
 export SECONDS=$(date +%s)
 export END_TIME=$((${SECONDS}+300))
@@ -33,7 +37,7 @@ echo $SECONDS
 echo $END_TIME
 # polling 5 min for .log file
 while [ "$SECONDS" -lt "$END_TIME" ]; do
-  aws s3api head-object --bucket $S3_BUCKET --key "$BACKUP_FILE_NAME" || not_exist=true
+  aws s3api head-object --bucket $S3_BUCKET --key "$BACKUP_LOG_NAME" || not_exist=true
   echo $API_RESPONSE
   if [ $not_exist ]; then
     echo "Polling for successfully completed restore log"
