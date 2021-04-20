634c387995a832173869fdd429d907639b064f30
Erno Kuusela <erno!erno-laptop>
2021-04-20 09:08:30 +0300

    install dev-tools in standalone script

--- ci/scripts/install-standalone-vm.bash
+++ ci/scripts/install-standalone-vm.bash
@@ -10,6 +10,8 @@ set -euo pipefail
 
 TEET_BRANCH=master
 TEET_GIT_URL=https://github.com/solita/mnt-teet.git
+DATOMIC_SOURCE_DB_NAME=$(aws ssm get-parameter --name /teet/datomic/db-name --query "Parameter.Value" --output text)
+TEET_ENV_NAME=$(aws ssm get-parameter --name /teet/env --query "Parameter.Value" --output text)
 
 function gensecret {
     name="$1"
@@ -28,6 +30,22 @@ function start-teet-app {
     tmux send-keys -t "${uuid}.0" "cd app/backend && clj -A:dev" ENTER "(restart)" ENTER
     tmux send-keys -t "${uuid}.1" "cd app/frontend && ./dev.sh" ENTER
     # tmux a -t "$uuid"
+}
+
+function import-datomic-to-dev-local {
+    clojure -e "
+     (datomic.dev-local/import-cloud
+    	    {:source {:system \"teet-datomic\"
+              :db-name \"$DATOMIC_SOURCE_DB_NAME\" ; use SSM /teet/datomic/db-name 
+              :server-type :ion
+              :region \"eu-central-1\"
+              :endpoint \"http://entry.teet-datomic.eu-central-1.datomic.net:8182/\"
+              :query-geoup \"teet-datomic\"
+              :proxy-port 8182} ; not needed when running in same vpc as compute group
+     :dest {:system \"teet-test-env\"
+            :server-type :dev-local
+            :db-name \"teet\"}
+     :filters {:since 0}})"
 }
 
 
@@ -64,22 +82,33 @@ function install_deps_and_app {
     docker exec teetdb sed -i -e '1i host all all 172.16.0.0/14 trust' /var/lib/postgresql/data/pg_hba.conf
     docker restart teetdb    
 
+    test -f dev-tools.zip || aws s3 cp s3://${TEET_ENV}-build/cognitect-dev-tools.zip .
+    unzip cognitect-dev-tools.zip
+    cd cognitect-dev-tools-*
+    ./install
+    cd ..      
+    
     cd mnt-teet
     cd db
     ./devdb_create_template.sh
     ./devdb_clean.sh
 
     cd ../..
-    
+
     start-teet-app # backend, frontend & postgrest
     
     sleep 10
+        
+    ## unneeded when restoring pg backup
+    # cd app/datasource-import
+    # clojure -A:import example-config.edn     
     
-    cd app/datasource-import
-    clojure -A:import example-config.edn 
-
     # todo: config.edn for teet app & datomic-local setup
+
+    # todo: use datomic.dev-local/import-cloud for datomic data
     
+    pg_dump 
+    # todo: use postgres backups for pg data (-> remove unneeded datasource-import run)
 }
 
 
