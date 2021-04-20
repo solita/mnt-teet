ffa166d8e99f99a9a409d8d83f3910418a92bd60
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-20 10:55:58 +0300

    Add more parameters to restore to create new DB every time

--- ci/backup-restore-test/restore.bash
+++ ci/backup-restore-test/restore.bash
@@ -4,7 +4,11 @@ echo "Restore started"
 
 export min_backup_size=1000000
 export BACKUP_SIZE=$(aws s3 ls s3://teet-dev2-documents | grep "backup" | grep $(date +%Y-%m-%d) | awk '{print $3}')
+export RESTORE_DB_NAME="teet"$(date +%Y%m%d)"debug1"
+export RESTORE_ASSET_DB_NAME="teetasset"$(date +%Y%m%d)"debug1"
 echo $BACKUP_SIZE
+echo $RESTORE_DB_NAME
+echo $RESTORE_ASSET_DB_NAME
 
 if [ "$BACKUP_SIZE" -lt "$min_backup_size" ]; then
   echo "Backup is too small"
@@ -15,7 +19,11 @@ export BACKUP_FILE_NAME=$(aws s3 ls s3://teet-dev2-documents | grep "backup" | g
 
 echo $BACKUP_FILE_NAME
 
-aws lambda invoke --function-name teet-datomic-Compute-restore --payload "{\"input\":{\"bucket\":\"teet-dev2-documents\",\"file-key\":\"$BACKUP_FILE_NAME\"}}" out
+aws lambda invoke --function-name teet-datomic-Compute-restore --payload "{\"input\":{
+  \"bucket\":\"teet-dev2-documents\",
+  \"file-key\":\"$BACKUP_FILE_NAME\",
+  \"create-database\":\"$RESTORE_DB_NAME\",
+  \"create-asset-database\":\"$RESTORE_ASSET_DB_NAME\"}}" out
 
 echo $(cat out)
 
