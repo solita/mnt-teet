92ea5a8d56b8d90f91931ce195ad8cff0f7b1818
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-04-21 13:40:46 +0300

    Use asset-db from ctx instead of pulling from env

--- app/backend/test/teet/system_test.clj
+++ app/backend/test/teet/system_test.clj
@@ -4,14 +4,13 @@
             teet.system.system-queries
             teet.environment))
 
-(use-fixtures :once tu/with-environment (tu/with-db))
+(use-fixtures :once
+  tu/with-environment
+  (tu/with-db))
 
 (deftest db-query
   (tu/run-with-config
    {:enabled-features #{:asset-db}}
-   (let [migrated-atom @#'teet.environment/asset-db-migrated?]
-     (reset! migrated-atom false)
-     (testing ":teet.system/db can be called by anonymous user"
-       (is (= (:status (tu/local-query nil :teet.system/db {}))
-              200))
-       (is @migrated-atom "Asset db was initialized by system query")))))
+   (testing ":teet.system/db can be called by anonymous user"
+     (let [resp (tu/local-query nil :teet.system/db {})]
+       (is (= (:status resp) 200))))))
