989220b97ae2f02b309a3ed279ec7742730fd929
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-21 11:38:53 +0300

    Move ion changes out of this pr

--- app/backend/src/clj/teet/backup/backup_ion.clj
+++ app/backend/src/clj/teet/backup/backup_ion.clj
@@ -100,24 +100,6 @@
                                    (:clear-database? config))
     ctx))
 
-(defn log-restore-result
-  "Write log to bucket into the same file name from which the restore was started"
-  [{{bucket :bucket file-key :file-key} :s3}]
-  (try
-    (let [file (java.io.File/createTempFile file-key ".log")]
-      (log/info "Generating restore log file: " (.getAbsolutePath file))
-      (with-open [w (io/writer file :append true)]
-                 (.write w (str "Backup " file-key " was restored successfully.")))
-      (with-open [in (io/input-stream file)]
-                 (s3/write-file-to-s3
-                   {:to {:bucket bucket
-                         :file-key (str file-key ".log")}
-                    :contents in}))
-      (io/delete-file file))
-    (log/info "Backup restore log created.")
-    (catch Exception e
-      (log/error e "Backup restore logging failed"))))
-
 (defn- read-restore-config [{event :event :as ctx}]
   (let [{:keys [file-key bucket] :as config*} (-> event :input (cheshire/decode keyword))
         config (dissoc config* :bucket :file-key)]
@@ -382,7 +364,6 @@
              prepare-asset-db-for-restore
              restore-tx-file
              migrate
-             log-restore-result
              delete-backup-file)
       (catch Exception e
         (log/error e "ERROR IN RESTORE" (ex-data e)))))
