d9fb377d3e891234ce417f96ffdda6acd85e7817
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-04-22 12:34:19 +0300

    Take first result

--- app/backend/src/clj/teet/asset/asset_db.clj
+++ app/backend/src/clj/teet/asset/asset_db.clj
@@ -341,20 +341,21 @@
 (defn latest-change-in-project
   "Fetch latest timestamp and author UUID that has changed anything in the given THK project."
   [db thk-project-id]
-  (d/q '[:find ?max-txi ?author
-         :where
-         ;; Find the max tx time for any change in the given project
-         [(q '[:find (max ?txi)
-               :where
-               (or [?e :asset/project ?project]
-                   [?e :cost-group/project ?project]
-                   [?e :boq-version/project ?project])
-               [?e _ _ ?tx]
-               [?tx :db/txInstant ?txi]
-               :in $ ?project] $ ?project) [[?max-txi]]]
+  (first
+   (d/q '[:find ?max-txi ?author
+          :where
+          ;; Find the max tx time for any change in the given project
+          [(q '[:find (max ?txi)
+                :where
+                (or [?e :asset/project ?project]
+                    [?e :cost-group/project ?project]
+                    [?e :boq-version/project ?project])
+                [?e _ _ ?tx]
+                [?tx :db/txInstant ?txi]
+                :in $ ?project] $ ?project) [[?max-txi]]]
 
-         ;; Get the tx and its author
-         [?tx :db/txInstant ?max-txi]
-         [?tx :tx/author ?author]
-         :in $ ?project]
-       db thk-project-id))
+          ;; Get the tx and its author
+          [?tx :db/txInstant ?max-txi]
+          [?tx :tx/author ?author]
+          :in $ ?project]
+        db thk-project-id)))
