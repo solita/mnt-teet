5e8388ec1b40b9a54284da9b790583cf014d8683
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-04-23 14:17:46 +0300

    Util to consume multiple contexts

--- app/frontend/src/cljs/teet/ui/context.cljs
+++ app/frontend/src/cljs/teet/ui/context.cljs
@@ -32,6 +32,20 @@
                          (conj component-fn ctx)
                          [component-fn ctx])))))
 
+(defn consume-many
+  "Consume multiple contexts at once. The context values will be added
+  as the last parameters to `component-vec` in the same order as they
+  appear in `context-names`."
+  [context-names component-vec]
+  (if (empty? context-names)
+    component-vec
+    (r/create-element (aget (get-or-create-context (first context-names)) "Consumer")
+                      nil
+                      (fn [ctx]
+                        (r/as-element
+                         [consume-many (drop 1 context-names)
+                          (conj component-vec ctx)])))))
+
 (defn- context-update [context-name component update-fn ctx]
   (provide context-name
            (update-fn ctx)
