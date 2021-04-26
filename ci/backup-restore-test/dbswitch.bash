53c98bdae8b787c95f81e2839942405b8b687e8a
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-26 15:32:13 +0300

    Minor changes

--- ci/backup-restore-test/dbswitch.bash
+++ ci/backup-restore-test/dbswitch.bash
@@ -21,24 +21,22 @@ echo "Restored DB "$RESTORE_DB_NAME
 BASEURL=$(aws ssm get-parameters --names "/teet/base-url" --query "Parameters[0].Value" | tr -d '"' | sed 's|/$||g')
 # "query/?q=["^ ","~:query","~:teet.system/db"]"
 ENDPOINT="query/?q=%5B%22%5E%20%22%2C%22~%3Aquery%22%2C%22~%3Ateet.system%2Fdb%22%5D"
-echo "$BASEURL/$ENDPOINT"
 
-echo curl returns $(curl -s -o /dev/null -w "%{http_code}" "$BASEURL/$ENDPOINT")
+echo "curl returns $(curl -s -o /dev/null -w "%{http_code}" "$BASEURL/$ENDPOINT")"
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" "$BASEURL/$ENDPOINT")
 
-if [ "$STATUS" == "200" ]; then
-    echo "Query succeeded"
-    echo "$(aws ssm get-parameters --names "/teet/datomic/db-name" --query "Parameters[0].Value" | tr -d '"') is in use"
-    break;
+if [ "$STATUS" = "200" ]; then
+  echo "Query succeeded"
+  echo "$(aws ssm get-parameters --names "/teet/datomic/db-name" --query "Parameters[0].Value" | tr -d '"') is in use"
 else
-    echo "Query failed with status: $STATUS"
-    # aws ssm put-parameter \
-    #              --name "/teet/datomic/db-name" \
-    #              --type "String" \
-    #              --value "$CURRENT_DB" \
-    #              --overwrite
-    sleep 60
-    echo "$(aws ssm get-parameters --names "/teet/datomic/db-name" --query "Parameters[0].Value" | tr -d '"') is set back"
+  echo "Query failed with status: $STATUS"
+  # aws ssm put-parameter \
+  #              --name "/teet/datomic/db-name" \
+  #              --type "String" \
+  #              --value "$CURRENT_DB" \
+  #              --overwrite
+  # sleep 60
+  echo "$(aws ssm get-parameters --names "/teet/datomic/db-name" --query "Parameters[0].Value" | tr -d '"') is set back"
 fi
 
 echo "Check completed"
