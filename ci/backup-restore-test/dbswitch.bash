0a5e70fdf94d658a2797e47e46cddfb87d36ed4f
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-26 13:09:21 +0300

    Debug checking db by HTTP query

--- ci/backup-restore-test/dbswitch.bash
+++ ci/backup-restore-test/dbswitch.bash
@@ -4,6 +4,10 @@ echo "Check started"
 
 set -eu
 
+CURRENT_DB=$(aws ssm get-parameters --names "/teet/datomic/db-name" --query "Parameters[0].Value" | tr -d '"')
+
+echo "Checking DB "$CURRENT_DB
+
 BASEURL=$(aws ssm get-parameters --names "/teet/base-url" --query "Parameters[0].Value" | tr -d '"' | sed 's|/$||g')
 ENDPOINT="query/?q=%5B%22%5E%20%22%2C%22~%3Aquery%22%2C%22~%3Ateet.system%2Fdb%22%5D"
 
