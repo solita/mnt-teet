743e04dfb62ccdac020cc4bdedf0c21005be3955
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-04-26 11:51:19 +0300

    Trying calling http endpoint instead of Datomic client api to check the restore is valid

--- ci/backup-restore-test/dbswitch.bash
+++ ci/backup-restore-test/dbswitch.bash
@@ -2,6 +2,24 @@
 
 echo "Check started"
 
-# TODO: implement some queries in restored DB
+set -eu
+
+BASEURL=$(aws ssm get-parameters --names "/teet/base-url" --query "Parameters[0].Value" | tr -d '"' | sed 's|/$||g')
+ENDPOINT="query/?q=%5B%22%5E%20%22%2C%22~%3Aquery%22%2C%22~%3Ateet.system%2Fdb%22%5D"
+
+while true
+do
+    echo curl on "$BASEURL/$ENDPOINT" returns $(curl  "$BASEURL/$ENDPOINT")
+    echo curl returns $(curl -s -L -w ' %{http_code}\n' "$BASEURL/$ENDPOINT")
+    read -r CURRENT_COMMIT STATUS < <(curl -s -L -w ' %{http_code}\n' "$BASEURL/$ENDPOINT")
+
+    if [ "$STATUS" == "200" ] && [ "$CODEBUILD_RESOLVED_SOURCE_VERSION" == "$CURRENT_COMMIT" ]; then
+        echo "Matching versions, query succeeded"
+        break;
+    else
+        echo "Waiting, deployed: $CURRENT_COMMIT, expected: $CODEBUILD_RESOLVED_SOURCE_VERSION"
+    fi
+    sleep 5
+done
 
 echo "Check completed"
