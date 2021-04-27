a4e2321a57e80c9f789ba1f303597ad7737d44a1
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-04-27 07:31:35 +0300

    Make heading a separately usable component

--- app/frontend/src/cljs/teet/ui/container.cljs
+++ app/frontend/src/cljs/teet/ui/container.cljs
@@ -5,25 +5,30 @@
             [teet.ui.material-ui :refer [Collapse IconButton]]
             [teet.ui.typography :as typography]))
 
-(defn collapsible-container
+(defn collapsible-container-heading
+  "The heading of collapsible container that shows open/closed state
+  and controls the state."
   [{:keys [on-toggle open? side-component size]
     :or {on-toggle identity
-         size :medium}}
-   heading contents]
-  [:div {:class (<class container-theme/container)}
-   [:div  {:class (<class container-theme/container-control)}
-    [IconButton {:size :small
-                 :color :primary
-                 :on-click on-toggle
-                 :class (<class container-theme/collapse-button)}
-     (if open?
-       [icons/hardware-keyboard-arrow-down {:color :primary}]
-       [icons/hardware-keyboard-arrow-right {:color :primary}])]
+         size :medium}} heading]
+  [:div  {:class (<class container-theme/container-control)}
+   [IconButton {:size :small
+                :color :primary
+                :on-click on-toggle
+                :class (<class container-theme/collapse-button)}
+    (if open?
+      [icons/hardware-keyboard-arrow-down {:color :primary}]
+      [icons/hardware-keyboard-arrow-right {:color :primary}])]
 
-    (if (= size :small)
-      [typography/Text2Bold heading]
-      [:h3 {:style {:margin 0 :flex 1}} heading])
-    (when side-component
-      side-component)]
+   (if (= size :small)
+     [typography/Text2Bold heading]
+     [:h3 {:style {:margin 0 :flex 1}} heading])
+   (when side-component
+     side-component)])
+
+(defn collapsible-container
+  [{:keys [open?] :as opts} heading contents]
+  [:div {:class (<class container-theme/container)}
+   [collapsible-container-heading opts heading]
    [Collapse {:in (boolean open?)}
     contents]])
