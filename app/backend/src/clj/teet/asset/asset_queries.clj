bc26fcc34b4d989bdfb11c0adacc4f7372300264
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-05-04 12:12:06 +0300

    Show fgroup/fclass subtotals

--- app/backend/src/clj/teet/asset/asset_queries.clj
+++ app/backend/src/clj/teet/asset/asset_queries.clj
@@ -32,6 +32,23 @@
 
 (def ^:private relevant-roads-buffer-meters 50)
 
+(defn- fclass-and-fgroup-totals
+  "Calculate subtotals for each fgroup and fclass."
+  [atl cost-groups]
+  (reduce
+   (fn [totals {:keys [type total-cost]}]
+     (let [[{fgroup :db/ident}
+            {fclass :db/ident} & _]
+           (asset-type-library/type-hierarchy atl type)
+           add-by (fn [v by]
+                    (+ (or v 0M)
+                       (or by 0M)))]
+       (-> totals
+           (update fgroup add-by total-cost)
+           (update fclass add-by total-cost))))
+   {}
+   cost-groups))
+
 (defquery :asset/project-cost-items
   {:doc "Query project cost items"
    :context {:keys [db user] adb :asset-db}
@@ -43,45 +60,47 @@
    :project-id [:thk.project/id project-id]
    ;; fixme: cost items authz
    :authorization {:project/read-info {}}}
-  (transit/with-write-options
-    euro/transit-type-handlers
-    (merge
-     {:asset-type-library (asset-db/asset-type-library adb)
-      :cost-items (asset-db/project-cost-items adb project-id)
-      :version (asset-db/project-boq-version adb project-id)
-      :latest-change (when-let [[timestamp author] (asset-db/latest-change-in-project adb project-id)]
-                       {:user (user-db/user-display-info db [:user/id author])
-                        :timestamp timestamp})
-      :project (project-db/project-by-id db [:thk.project/id project-id])}
-     (when cost-totals
-       (let [cost-groups (asset-db/project-cost-groups-totals
-                          adb project-id
-                          (when road
-                            (asset-db/project-assets-and-components-matching-road adb project-id road)))]
-         {:cost-totals
-          {:cost-groups cost-groups
-           :total-cost (reduce + (keep :total-cost cost-groups))}}))
-     (when cost-item
-       {:cost-item (fetch-cost-item
-                    adb
-                    ;; Always pull the full asset even when focusing on a
-                    ;; specific subcomponent.
-                    ;; PENDING: what if there are thousands?
-                    (if (asset-model/component-oid? cost-item)
-                      (asset-model/component-asset-oid cost-item)
-                      cost-item))})
-     ;; Fetch relevant roads for cost items of project, meaning the
-     ;; project road along with roads intersecting project geometry
-     ;; with a buffer of 50 meters.
-     (when relevant-roads
-       {:relevant-roads
-        (when-let [integration-id (project-db/thk-id->integration-id-uuid db project-id)]
-          (let [ctx (environment/config-map {:api-url [:api-url]
-                                             :api-secret [:auth :jwt-secret]
-                                             :wfs-url [:road-registry :wfs-url]})]
-            (road-query/fetch-relevant-roads-for-project-cost-items ctx
-                                                                    integration-id
-                                                                    relevant-roads-buffer-meters)))}))))
+  (let [atl (asset-db/asset-type-library adb)]
+    (transit/with-write-options
+      euro/transit-type-handlers
+      (merge
+       {:asset-type-library atl
+        :cost-items (asset-db/project-cost-items adb project-id)
+        :version (asset-db/project-boq-version adb project-id)
+        :latest-change (when-let [[timestamp author] (asset-db/latest-change-in-project adb project-id)]
+                         {:user (user-db/user-display-info db [:user/id author])
+                          :timestamp timestamp})
+        :project (project-db/project-by-id db [:thk.project/id project-id])}
+       (when cost-totals
+         (let [cost-groups (asset-db/project-cost-groups-totals
+                            adb project-id
+                            (when road
+                              (asset-db/project-assets-and-components-matching-road adb project-id road)))]
+           {:cost-totals
+            {:cost-groups cost-groups
+             :fclass-and-fgroup-totals (fclass-and-fgroup-totals atl cost-groups)
+             :total-cost (reduce + (keep :total-cost cost-groups))}}))
+       (when cost-item
+         {:cost-item (fetch-cost-item
+                      adb
+                      ;; Always pull the full asset even when focusing on a
+                      ;; specific subcomponent.
+                      ;; PENDING: what if there are thousands?
+                      (if (asset-model/component-oid? cost-item)
+                        (asset-model/component-asset-oid cost-item)
+                        cost-item))})
+       ;; Fetch relevant roads for cost items of project, meaning the
+       ;; project road along with roads intersecting project geometry
+       ;; with a buffer of 50 meters.
+       (when relevant-roads
+         {:relevant-roads
+          (when-let [integration-id (project-db/thk-id->integration-id-uuid db project-id)]
+            (let [ctx (environment/config-map {:api-url [:api-url]
+                                               :api-secret [:auth :jwt-secret]
+                                               :wfs-url [:road-registry :wfs-url]})]
+              (road-query/fetch-relevant-roads-for-project-cost-items ctx
+                                                                      integration-id
+                                                                      relevant-roads-buffer-meters)))})))))
 
 (s/def :boq-export/version integer?)
 (s/def :boq-export/unit-prices? boolean?)
