6135b11875bd4e201e38a65b0497b0c9a3e5ea47
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-05-05 14:52:58 +0300

    Using fake activity project

--- ci/browser-tests/cypress/integration/activity.spec.js
+++ ci/browser-tests/cypress/integration/activity.spec.js
@@ -1,15 +1,15 @@
-describe("Task view", function() {
+describe("Activity view", function() {
     before(() => {
         cy.dummyLogin("benjamin")
-        cy.selectLanguage("#EN")
-        cy.projectByName("integration test project")
-
-        // Navigate to Detailed design
-        cy.get("li a").contains("Detailed design").click()
+        cy.setup("task", {"project-name": "ACTIVITY TESTING",
+            "activity": "preliminary-design"})
     })
 
     context("Edit Activity", function() {
         it("can be edited", function() {
+            cy.get("@task").then((t) => cy.visit("#/projects/"+t["project-id"]))
+            cy.wait(1000)
+            cy.get("li a").contains("Eelprojekt").click({force: true})
             cy.get("button[data-cy=activity-edit-button]").click({force: true})
             cy.get("div[data-form-attribute=" +
                 "\"[:activity/estimated-start-date :activity/estimated-end-date]\"]").should("exist")
