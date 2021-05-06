99978410902bf52651ac923a24eb3ca38f525646
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-05-06 14:27:59 +0300

    Using setup in before hook

--- ci/browser-tests/cypress/integration/activity.ci.spec.js
+++ ci/browser-tests/cypress/integration/activity.ci.spec.js
@@ -1,21 +1,14 @@
 describe("Activity view", function() {
     beforeEach(() => {
-
-        cy.request({method: "POST",
-            url: "/testsetup/task",
-            body: {"project-name": "ACTIVITY TESTING"}})
-            .then((response) => {
-                cy.wrap(response.body["project-id"]).as("projectID")
-            })
-
+        cy.setup("task", {"project-name": "ACTIVITY TESTING", "activity": "preliminary-design"})
         cy.dummyLogin("Danny")
     })
 
     context("Edit Activity", function() {
         it("can be edited", function() {
             // visit project page
-            cy.visit("#/projects/"+this.projectID)
-            cy.get("li a").contains("Eskiis").click({force: true})
+            cy.get("@task").then((t) => cy.visit("#/projects/"+t["project-id"]))
+            cy.get("li a").contains("Eelprojekt").click({force: true})
             cy.get("button[data-cy=activity-edit-button]").click({force: true})
             cy.get("div[data-form-attribute=" +
                 "\"[:activity/estimated-start-date :activity/estimated-end-date]\"]").should("exist")
