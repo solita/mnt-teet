748a7ac0a1989165a9fc9491d4e702ac60e80f82
aleksievelizhanin <Aleksei.velizanin@solita.fi>
2021-05-06 11:13:23 +0300

    Update activity.spec.js
    Use one of the existing projects to test the Activity edit

--- ci/browser-tests/cypress/integration/activity.spec.js
+++ ci/browser-tests/cypress/integration/activity.spec.js
@@ -1,15 +1,29 @@
 describe("Activity view", function() {
-    before(() => {
-        cy.dummyLogin("benjamin")
-        cy.setup("task", {"project-name": "ACTIVITY TESTING",
-            "activity": "preliminary-design"})
+    beforeEach(() => {
+        cy.visit("")
+        cy.get("#password-textfield").type(Cypress.env("SITE_PASSWORD"));
+        cy.get("button").contains("Login as Benjamin Boss").click();
+        cy.contains('Minu projektid');
     })
+
+    function selectProject() {
+        cy.get("span").contains("menu").click({force: true});
+
+        cy.get("span").contains("Projektide loetelu").click();
+        cy.contains("Kõik projektid").click();
+
+        cy.get("#quick-search").type("aruvalla");
+        cy.get("p").contains("Aruvalla").click();
+    }
+
+    function selectActivity() {
+        cy.get("li a").contains("Põhiprojekt").click({force: true})
+    }
 
     context("Edit Activity", function() {
         it("can be edited", function() {
-            cy.get("@task").then((t) => cy.visit("#/projects/"+t["project-id"]))
-            cy.wait(1000)
-            cy.get("li a").contains("Eelprojekt").click({force: true})
+            selectProject();
+            selectActivity();
             cy.get("button[data-cy=activity-edit-button]").click({force: true})
             cy.get("div[data-form-attribute=" +
                 "\"[:activity/estimated-start-date :activity/estimated-end-date]\"]").should("exist")
