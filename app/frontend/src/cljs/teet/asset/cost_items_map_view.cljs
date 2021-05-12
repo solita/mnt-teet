1cb28bf3159033eb30ac53b79bb652a21c737ccb
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-05-12 12:54:14 +0300

    Show existing start/end location

--- app/frontend/src/cljs/teet/asset/cost_items_map_view.cljs
+++ app/frontend/src/cljs/teet/asset/cost_items_map_view.cljs
@@ -6,7 +6,8 @@
             [teet.map.map-view :as map-view]
             [teet.map.map-layers :as map-layers]
             [teet.map.map-features :as map-features]
-            [teet.map.openlayers.drag :as drag]))
+            [teet.map.openlayers.drag :as drag]
+            [teet.asset.cost-items-controller :as cost-items-controller]))
 
 (defn project-map* [{:keys [e!] :as opts} {:keys [app project]}]
   (r/with-let [overlays (r/atom [])
@@ -36,46 +37,53 @@
    child])
 
 
-(defn location-map [{:keys [e! value on-change]}]
-  (r/with-let [current-value (atom value)
-               dragging? (atom false)]
-    ;;(project-map-view/create-project-map e! app project)
-    (let [geojson (last value)]
-      (reset! current-value value)
-      [project-map
-       {:e! e!
-        :on-click (fn [{c :coordinate}]
-                    (let [[start end :as v] @current-value]
-                      (cond
-                        ;; If no start point, set it
-                        (nil? start) (on-change (assoc v 0 c))
+(defn location-map [{:keys [e! value]}]
+  (e! (cost-items-controller/->InitMap))
+  (let [current-value (atom value)
+        dragging? (atom false)]
+    (fn [{:keys [e! value on-change]}]
+      ;;(project-map-view/create-project-map e! app project)
+      (let [geojson (last value)]
+        (reset! current-value value)
+        [project-map
+         {:e! e!
+          :on-click
+          (fn [{c :coordinate}]
+            (let [[start end :as v] @current-value]
+              (cond
+                ;; If no start point, set it
+                (nil? start) (on-change (assoc v 0 c))
 
-                        ;; If no end point, set it
-                        (nil? end) (on-change (assoc v 1 c))
+                ;; If no end point, set it
+                (nil? end) (on-change (assoc v 1 c))
 
-                        ;; Otherwise do nothing
-                        :else nil)))
-        :event-handlers (drag/drag-feature
-                         {:accept (comp :map/feature :geometry)
-                          :on-drag drag/on-drag-set-coordinates
-                          :on-drop
-                          (fn [target to]
-                            (when-let [p (some-> target :geometry :map/feature
-                                                 .getProperties (aget "start/end"))]
-                              (on-change
-                               (assoc @current-value
-                                      (case p
-                                        "start" 0
-                                        "end" 1)
-                                      to))))
-                          :dragging? dragging?})
-        :layers {:selected-road-geometry
-                 (when-let [g geojson]
-                   (map-layers/geojson-data-layer
-                    "selected-road-geometry"
-                    geojson
-                    map-features/asset-road-line-style
-                    {:fit-on-load? true
-                     :fit-condition
-                     (fn [_]
-                       (not @dragging?))}))}}])))
+                ;; Otherwise do nothing
+                :else nil)))
+
+          :event-handlers
+          (drag/drag-feature
+           {:accept (comp :map/feature :geometry)
+            :on-drag drag/on-drag-set-coordinates
+            :on-drop
+            (fn [target to]
+              (when-let [p (some-> target :geometry :map/feature
+                                   .getProperties (aget "start/end"))]
+                (on-change
+                 (assoc @current-value
+                        (case p
+                          "start" 0
+                          "end" 1)
+                        to))))
+            :dragging? dragging?})
+
+          :layers
+          {:selected-road-geometry
+           (when-let [g geojson]
+             (map-layers/geojson-data-layer
+              "selected-road-geometry"
+              geojson
+              map-features/asset-road-line-style
+              {:fit-on-load? true
+               :fit-condition
+               (fn [_]
+                 (not @dragging?))}))}}]))))
