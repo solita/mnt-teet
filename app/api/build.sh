e3ede3d7d5c11211ef26a67e59b8931a18e03edd
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-05-14 14:08:18 +0300

    Move docker login to pre-build phase

--- app/api/build.sh
+++ app/api/build.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 set -eu
-
-DOCKER_USER=$(aws ssm get-parameters --names "/teet/docker/user" --query "Parameters[0].Value" | tr -d '"')
-DOCKER_ACCESS_TOKEN=$(aws ssm get-parameters --names "/teet/docker/token" --query "Parameters[0].Value" | tr -d '"')
-
-echo $DOCKER_USER
-echo $DOCKER_ACCESS_TOKEN
-
-echo $DOCKER_ACCESS_TOKEN > ~/token.txt
-
-cat ~/token.txt | docker login --username $DOCKER_USER --password-stdin
-
 docker build . -t mnt-teet/teet-api:latest --build-arg POSTGREST_VERSION=v6.0.1
