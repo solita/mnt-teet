5dd83b95b194eb528d183cb0d0d4b5b753a3d49c
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-05-14 13:33:31 +0300

    Move docker login to bash

--- app/api/build.sh
+++ app/api/build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 set -eu
+
+DOCKER_USER=$(aws ssm get-parameters --names "/teet/docker/user" --query "Parameters[0].Value" | tr -d '"')
+DOCKER_ACCESS_TOKEN=$(aws ssm get-parameters --names "/teet/docker/token" --query "Parameters[0].Value" | tr -d '"')
+
+echo $DOCKER_USER
+echo $DOCKER_ACCESS_TOKEN
+
+$(echo $DOCKER_ACCESS_TOKEN | docker login --username $DOCKER_USER --password-stdin)
+
 docker build . -t mnt-teet/teet-api:latest --build-arg POSTGREST_VERSION=v6.0.1
