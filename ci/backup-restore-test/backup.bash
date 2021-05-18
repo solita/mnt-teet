ef876ff9a23cd35ac2f2d57bb91495cf07ce538c
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-05-18 13:58:23 +0300

    Remove unnecessary logtrace info. Use SSM parameter to get S3 backup bucket name

--- ci/backup-restore-test/backup.bash
+++ ci/backup-restore-test/backup.bash
@@ -2,7 +2,7 @@
 set -eu
 echo "Backup started"
 
-S3_BUCKET="teet-dev2-documents"
+S3_BUCKET=$(aws ssm get-parameters --names "/teet/s3/backup-bucket" --query "Parameters[0].Value" | tr -d '"')
 BACKUP_FILE_NAME="teet-dev2-backup-"$(date +%Y-%m-%d)".edn.zip"
 
 aws lambda invoke --function-name teet-datomic-Compute-backup --payload '{"key": "value"}' out
@@ -13,8 +13,6 @@ SECONDS=$(date +%s)
 END_TIME=$((${SECONDS}+600))
 interval=10
 
-echo $SECONDS
-echo $END_TIME
 # polling 10 min for .zip file
 while [ "$SECONDS" -lt "$END_TIME" ]; do
   aws s3api wait object-exists --bucket $S3_BUCKET --key "$BACKUP_FILE_NAME" || not_exist=true
