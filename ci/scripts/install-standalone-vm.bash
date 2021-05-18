c49266aa1cceb84d6418e313df07565b108fe549
Erno Kuusela <erno!erno-laptop>
2021-05-18 17:53:18 +0300

    standalone vm script: postgrest and backend config

--- ci/scripts/install-standalone-vm.bash
+++ ci/scripts/install-standalone-vm.bash
@@ -20,14 +20,76 @@ function gensecret {
 }
 
 function start-teet-app {
-    cd mnt-teet/app/api
-    nohup ./dev.sh > api.out &
-    cd ../../..
+    local DB_URI="postgres://authenticator@teetdb:5432/teet"
+
+    mkdir -p mnt-teet-private
+    JWT_SECRET=$(head -c 42 /dev/urandom | base64)
+
+    docker run --network docker_teet --name teetapi -p 127.0.0.1:3000:3000 \
+       -e PGRST_DB_URI="$DB_URI" \
+       -e PGRST_DB_ANON_ROLE="teet_anon" \
+       -e PGRST_DB_SCHEMA="teet" \
+       -e PGRST_JWT_SECRET="$JWT_SECRET" \
+       postgrest/postgrest    
+
+    cat > mnt-teet-private/config.edn <<EOF
+{:datomic
+ {:db-name "teet"
+  :asset-db-name "asset"
+  :client {:server-type :dev-local
+           :system "teet"}}
+
+ :auth {:jwt-secret "$JWT_SECRET"
+        :basic-auth-password "$(ssm-get /teet/auth/basic-auth-password"}
+ :env :dev
+ :api-url "http://localhost:3000"
+ :enabled-features #{:road-information-view
+                     :meetings
+                     :component-view
+                     :data-frisk
+                     :my-role-display
+                     :dummy-login
+                     :cooperation
+                     :admin-inspector
+                     ; :vektorio
+                     :asset-db
+                     :cost-items
+                     :land-owner-opinions
+                     :assetmanager}
+
+ :file {:allowed-suffixes #{"doc" "docx" "xlsx" "xls" "ppt" "pptx" "rtf" "odf" "pdf" "dwf" "dwg" "dgn" "dxf" "shp" "dbf" "kml" "kmz" "ifc" "xml" "bcf" "rvt" "skp" "3dm" "ags" "gpx" "png" "jpg" "jpeg" "tif" "tiff" "ecw" "shx" "lin" "wav" "mp3" "ogg" "aac" "mov" "mp4" "m4v" "avi" "las"}
+        :image-suffixes #{"png" "jpg" "jpeg" "tif" "tiff" "ecw"}}
+n
+ :xroad {:query-url "$(ssm-get /teet/xroad-query-url)"
+         :instance-id "$(ssm-get /teet/xroad-instance-id)"}
+
+ :document-storage {:bucket-name "teet-dev2-documents"
+                    :export-bucket-name "teet-dev-zip-export"}
+
+ :road-registry {:wfs-url "https://teeregister.mnt.ee/290424/wfs"
+                 :wms-url "https://teeregister.mnt.ee/teenus/wms"
+                 :api {:endpoint "https://teeregister-api.mnt.ee"
+                       :username "$(ssm-get /teet/road-registry/api/username)"
+                       :password "$(ssm-get /teet/road-registry/api/password)"}}
+ ;; Estonian Nature Information System (EELIS)
+ :eelis {:wms-url "https://gsavalik.envir.ee/geoserver/eelis/ows"}
+ :heritage {:wms-url "https://xgis.maaamet.ee/xgis2/service/clnl/muinsus"}
+ :base-url "https://$MDNS:4000"
+ :notify {:application-expire-days "45"}
+ :email {:subject-prefix "LOCAL"
+         :contact-address "local@dev.fi"
+         :from "local@dev.fi"
+         :server "localhost:25"}
+ :asset {:default-owner-code "DEV"}
+ :contract {:state-procurement-url "$(ssm-get /teet/contract/state-procurement-url)"
+            :thk-procurement-url "$(ssm-get /teet/contract/thk-procurement-url)"}
+ }
+EOF
     uuid="$(uuidgen)"
     tmux new -d -s "$uuid"
     tmux splitw -h -t "${uuid}:0.0"
-    tmux send-keys -t "${uuid}.0" "cd app/backend && clj -A:dev" ENTER "(restart)" ENTER
-    tmux send-keys -t "${uuid}.1" "cd app/frontend && ./dev.sh" ENTER
+    tmux send-keys -t "${uuid}.0" "cd mnt-teet/app/backend && clj -A:dev" ENTER "(restart)" ENTER
+    tmux send-keys -t "${uuid}.1" "cd mnt-teet/app/frontend && ./dev.sh" ENTER
     # tmux a -t "$uuid"
 }
 
@@ -66,6 +128,8 @@ function allow-datomic-bastion-ssh {
 
 function import-datomic-to-dev-local {
     # run in backend directory, needs the deps
+    local DATOMIC_SOURCE_DB_NAME="$1"
+    local DEST_DB_NAME="$2"
     mkdir -p ~/.datomic
     cd mnt-teet/app/backend
     clojure -A:dev -e "
@@ -81,7 +145,7 @@ function import-datomic-to-dev-local {
      :dest {:system \"teet-test-env\"
             :storage-dir \"$HOME/.datomic/dev-local-data\"
             :server-type :dev-local
-            :db-name \"teet\"}
+             :db-name \"$2\"}
      :filters {:since 0}})"
     cd -
 }
@@ -96,12 +160,30 @@ function setup-postgres {
     createuser -h localhost -U postgres teetmaster
     createdb -h localhost -U postgres $SOURCE_DB_NAME -O teetmaster
     psql -c "CREATE ROLE teeregister" -h localhost -U postgres
+    psql -c "CREATE ROLE rdsadmin" -h localhost -U postgres
     PGPASSWORD=$SOURCE_DB_PASS /usr/bin/pg_dump -Fc -h $SOURCE_DB_HOST -U $SOURCE_DB_USER $SOURCE_DB_NAME | pg_restore -d "$SOURCE_DB_NAME" -h localhost -U postgres
 }
 
 
 function ssm-get {
     aws ssm get-parameter --name "$1" --query "Parameter.Value" --output text
+}
+
+function ssm-get-csv {
+    local v="$(aws ssm get-parameter --name "$1" --output text --query Parameter.Value)"
+    IFS=", " read -ra SSMVALS <<< $v
+}
+
+
+function setup-reverse-proxy {
+    MY_EMAIL=$(ssm-get /teet/email/from)
+    MY_DNS=$(ec2metadata --public-hostname)
+    certbot certonly \
+	    --standalone \
+	    --non-interactive \
+	    --agree-tos \
+	    --email $MY_EMAIL \
+	    --domains $MY_DNS
 }
 
 
@@ -112,7 +194,7 @@ function install-deps-and-app {
     cd /var/tmp/teetinstall
 
     apt-get update
-    apt-get -y install docker.io git openjdk-11-jdk coreutils python3-pip python3-venv rlwrap postgresql-client-{12,common} maven unzip wget jq awscli net-tools
+    apt-get -y install docker.io git openjdk-11-jdk coreutils python3-pip python3-venv rlwrap postgresql-client-{12,common} maven unzip wget jq awscli net-tools certbot
 
     curl -O https://download.clojure.org/install/linux-install-1.10.3.822.sh
     chmod +x linux-install-1.10.3.822.sh
@@ -138,10 +220,8 @@ function install-deps-and-app {
     docker exec teetdb sed -i -e '1i host all all 172.16.0.0/14 trust' /var/lib/postgresql/data/pg_hba.conf
     docker restart teetdb    
 
-    DATOMIC_SOURCE_DB_NAME=$(ssm-get /teet/datomic/db-name)
     TEET_ENV=$(ssm-get /teet/env)
-
-    
+   
     test -f cognitect-dev-tools.zip || aws s3 cp s3://${TEET_ENV}-build/cognitect-dev-tools.zip .
     unzip cognitect-dev-tools.zip
     cd cognitect-dev-tools-*
@@ -160,7 +240,8 @@ function install-deps-and-app {
     allow-datomic-bastion-ssh
     start-datomic-portforward    
     
-    import-datomic-to-dev-local
+    import-datomic-to-dev-local $(ssm-get /teet/datomic/db-name) teet
+    import-datomic-to-dev-local $(ssm-get /teet/datomic/asset-db-name) asset
     
     setup-postgres
     
