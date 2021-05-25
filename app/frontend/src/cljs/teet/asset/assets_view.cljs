60107d5013062500feaaa9c2e0ba2f05a34bbb24
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-05-25 17:00:37 +0300

    Separately load GeoJSON for assets with bbox

--- app/frontend/src/cljs/teet/asset/assets_view.cljs
+++ app/frontend/src/cljs/teet/asset/assets_view.cljs
@@ -41,41 +41,29 @@
 
     (str value)))
 
-(defn- assets-results [_ _ _]
-  (let [fitted (atom false)
-        map-key (r/atom 1)
+(defn- assets-results [_ _ _ _]
+  (let [map-key (r/atom 1)
         next-map-key! #(swap! map-key inc)]
-    (r/create-class
-     {:component-did-update
-      (fn [_
-           [_ _ _ {old-geojson :geojson}]
-           [_ _ _ {new-geojson :geojson}]]
-        (when (not= old-geojson new-geojson)
-          (reset! fitted false)))
-      :reagent-render
-      (fn [e! _atl {:keys [assets geojson]}]
-        [vertical-split-pane {:defaultSize 400 :primary "second"
-                              :on-drag-finished next-map-key!}
-         [:div
-          [typography/Heading1 (tr [:asset :manager :link])]
-          [table/listing-table
-           {:default-show-count 100
-            :columns asset-model/assets-listing-columns
-            :get-column asset-model/assets-listing-get-column
-            :format-column format-assets-column
-            :data assets
-            :key :asset/oid}]]
-         ^{:key (str "map" @map-key)}
-         [map-view/map-view e!
-          {:full-height? true
-           :layers (when geojson
-                     {:assets-search-geojson
-                      (map-layers/geojson-data-layer
-                       "assets-search-geojson"
-                       (js/JSON.parse geojson)
-                       map-features/asset-line-and-icon
-                       {:fit-on-load? true
-                        :fitted-atom fitted})})}]])})))
+    (fn [e! _atl assets-query {:keys [assets]}]
+      [vertical-split-pane {:defaultSize 400 :primary "second"
+                            :on-drag-finished next-map-key!}
+       [:div
+        [typography/Heading1 (tr [:asset :manager :link])]
+        [table/listing-table
+         {:default-show-count 100
+          :columns asset-model/assets-listing-columns
+          :get-column asset-model/assets-listing-get-column
+          :format-column format-assets-column
+          :data assets
+          :key :asset/oid}]]
+       ^{:key (str "map" @map-key)}
+       [map-view/map-view e!
+        {:full-height? true
+         :layers
+         {:asset-results
+          (map-layers/query-layer e! :assets/geojson (:args assets-query)
+                                  {:style-fn map-features/asset-line-and-icon
+                                   :max-resolution 100})}}]])))
 
 (defn assets-page [e! app]
   (r/with-let [filters (r/atom {})]
@@ -89,4 +77,4 @@
        (when-let [q (assets-controller/assets-query @filters)]
          [query/query
           (merge q {:e! e!
-                    :simple-view [assets-results e! (:asset-type-library app)]})])]]]))
+                    :simple-view [assets-results e! (:asset-type-library app) q]})])]]]))
