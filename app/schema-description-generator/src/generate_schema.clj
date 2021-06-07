56f6ea657b4690cd32075bae1f5f0f1baad7f6b6
Erno Kuusela <erno!erno-laptop>
2021-06-07 17:25:49 +0300

    entity walker works

--- app/schema-description-generator/src/generate_schema.clj
+++ app/schema-description-generator/src/generate_schema.clj
@@ -5,8 +5,23 @@
 (def datomic-client-config (read-string (slurp "config.edn")))
 (when-let [p (get datomic-client-config :aws-profile)]
   (System/setProperty "aws.profile" p))
-(def client (d/client datomic-client-config))
-(def db (d/connect client (select-keys datomic-client-config [:db-name])))
+(def conn (d/connect (d/client datomic-client-config)
+                     (select-keys datomic-client-config [:db-name])))
+
+(defn walk-entity [db eid]
+  (let [entity-map (d/pull db '[*] eid)]
+    {:name name
+     :fields (keys entity-map)
+     :children 
+     (for [[key val] entity-map
+           :when (and (vector? val) (-> val seq first :db/id))]
+       [key (mapv (partial walk-entity db) (mapcat vals val))])}))
+
+(defn walk-db [db root-attr]
+  (let [root-eid-set
+        (mapv first (d/q [:find '?e :where ['?e root-attr]] db))]
+    (map (partial walk-entity db)
+            root-eid-set)))
 
 (defn -main [& args]  
-  (println "foo"))
+  (walk-db (d/db conn) :thk.project/id))
