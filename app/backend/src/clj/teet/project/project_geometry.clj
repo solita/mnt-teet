9cf04251f6a9c1068d67ee8a71025bc85ad21894
Topi Pohjosaho <topi.pohjosaho@solita.fi>
2021-06-09 13:34:24 +0300

    Update each project geometry in separate POST request
    Sending a list of requests does not seem to work anymore.

--- app/backend/src/clj/teet/project/project_geometry.clj
+++ app/backend/src/clj/teet/project/project_geometry.clj
@@ -18,37 +18,35 @@
   Calls store_entity_info in PostgREST API."
   [{:keys [api-url api-secret wfs-url] :as api} projects]
   {:pre [(valid-api-info? api)]}
-  (let [road-part-cache (atom {})
-        request-body (for [{id :integration/id
-                            :thk.project/keys [project-name name road-nr carriageway
-                                               start-m end-m
-                                               custom-start-m custom-end-m]}
-                           projects
-                           :when (and id
-                                      (integer? (or custom-start-m start-m))
-                                      (integer? (or custom-end-m end-m))
-                                      (integer? road-nr)
-                                      (integer? carriageway))
-                           :let [geometry (road-query/fetch-road-geometry {:wfs-url wfs-url
-                                                                           :cache-atom road-part-cache}
-                                                                          road-nr carriageway
-                                                                          (or custom-start-m start-m)
-                                                                          (or custom-end-m end-m))]]
-                       {:id (str (integration-id/uuid->number id))
-                        :type "project"
-                        :tooltip (or project-name name)
-                        :geometry_wkt (geo/line-string-to-wkt geometry)})]
-    (when (not-empty request-body)
-      (let [response @(http/post
-                       (str api-url "/rpc/store_entity_info")
-                       {:headers {"Content-Type" "application/json"
-                                  "Authorization"
-                                  (str "Bearer "
-                                       (jwt-token/create-backend-token api-secret))}
-                        :body (cheshire/encode request-body)})]
-        (when-not (= 200 (:status response))
-          (throw (ex-info "Update project geometries failed"
-                          {:expected-response-status 200
-                           :actual-response-status (:status response)
-                           :response response})))))
+  (let [road-part-cache (atom {})]
+    (doseq [{id :integration/id
+             :thk.project/keys [project-name name road-nr carriageway
+                                start-m end-m
+                                custom-start-m custom-end-m]}
+            projects
+            :when (and id
+                       (integer? (or custom-start-m start-m))
+                       (integer? (or custom-end-m end-m))
+                       (integer? road-nr)
+                       (integer? carriageway))
+            :let [geometry (road-query/fetch-road-geometry {:wfs-url wfs-url
+                                                            :cache-atom road-part-cache}
+                                                           road-nr carriageway
+                                                           (or custom-start-m start-m)
+                                                           (or custom-end-m end-m))
+                  response @(http/post
+                              (str api-url "/rpc/store_entity_info")
+                              {:headers {"Content-Type" "application/json"
+                                         "Authorization"
+                                         (str "Bearer "
+                                              (jwt-token/create-backend-token api-secret))}
+                               :body (cheshire/encode {:id (str (integration-id/uuid->number id))
+                                                       :type "project"
+                                                       :tooltip (or project-name name)
+                                                       :geometry_wkt (geo/line-string-to-wkt geometry)})})]]
+      (when-not (= 200 (:status response))
+        (throw (ex-info "Update project geometries failed"
+                        {:expected-response-status 200
+                         :actual-response-status (:status response)
+                         :response response}))))
     projects))
