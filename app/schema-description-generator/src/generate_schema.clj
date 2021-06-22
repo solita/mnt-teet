d0a39166521687b2646b0b6d22f6ab26cc1357b4
Erno Kuusela <erno!erno-laptop>
2021-06-22 10:20:44 +0300

    record parents

--- app/schema-description-generator/src/generate_schema.clj
+++ app/schema-description-generator/src/generate_schema.clj
@@ -1,8 +1,11 @@
 (ns generate-schema
-  (:require [clojure.string :as s]
-            [datomic.client.api :as d]
+  (:require [datomic.client.api :as d]
             [clojure.data.csv :as csv]
+            [clojure.string :refer [join split]]
             [clojure.set :refer [difference]]))
+
+;; we use this to keep traversal acyclic
+(def descend-blacklist #{:permission/projects})
 
 (def datomic-client-config (read-string (slurp "config.edn")))
 
@@ -21,7 +24,7 @@
         ref-key-set (into #{}
                           (map first kv-pairs-with-db-id))
         all-keys-set (set (keys e))]
-    (into [] (concat (map #(str "ref: " (name %1))
+    (into [] (concat (map #(str  %1 " (entity ref)")
                           ref-key-set)
                      (difference all-keys-set ref-key-set)))))
 
@@ -39,34 +42,40 @@
 
 (defn walk-entity
   ([db entity-map]
-   (walk-entity db 1 (atom #{}) entity-map))
-  ([db depth seen-atom entity-map]
+   (walk-entity db [] entity-map))
+  ([db parents entity-map]
    (let [eid (:db/id entity-map)]
      (assert (some? eid) entity-map)
      (assert (number? eid) eid)
-     (swap! seen-atom conj eid)
+     ;; (println "parents" parents)
      {:fields (field-names entity-map)
+      :parents (join ";" parents)
       :children
       (doall (for [[key val] entity-map
-                   :when (and (vector? val) (-> val seq first :db/id))]
+                   :when (and (vector? val)
+                              (-> val seq first :db/id)
+                              (not (key descend-blacklist))
+                              #_(< (count parents) 3))]
                (let [eids (keep :db/id (filter map? val))
-                     seen (deref seen-atom)
-                     new-eids (filter (complement seen) eids)]
-                 (println "depth" depth "- child eids" eids "from" val)
-                 (do (assert  (every? number? eids))
-                     [key (mapv (partial walk-entity db (inc depth) seen-atom) (pull-entities db new-eids))]))))})))
+                     child-parents (conj parents key)]
 
-
+                 [key (mapv (partial walk-entity db child-parents) (pull-entities db eids))])))})))
 
 (defn walk-db [db root-attr]
   (let [root-ents (mapv first (d/q [:find '(pull ?e [*]) :where ['?e root-attr]] db))]
-    (println (first root-ents))
+    ;; (println (first root-ents))
     (mapv (partial walk-entity db) root-ents)))
 
 (defn flatten-db [name m]
   (assert (map? m) m)
-  (let [field-rows (for [f (:fields m)]
-                     [:entity name :field f])
+  (let [parents (:parents m)
+        field-rows (for [f (:fields m)]
+                     (do
+                       ; (println "parents" parents-text)
+                       [:entity name
+                                        ; :parents parents-text
+                        :parents parents
+                        :field f]))
         child-rows (doall (for [child (:children m)]
                             (do
                               (assert (vector? child))
@@ -77,28 +86,42 @@
 
 
 (defn unique-sorted [x]
-  (sort (into #{} x)))
+
+  (sort (fn [a b]
+          (compare (str a) (str b))) (into #{} x)))
+
+(def pre-split-columns ["entity" "parents" "attribute"])
+(def post-split-columns ["entity" "attribute"  "parent1" "parent2" "parent3" "parent4" "parent5" "parent6" "parent7" "parent8" "parent9" "parent10"])
 
 (defn make-csv-rows [flat-data]
-  (concat [["entity" "attribute"]]
-          (mapv (juxt (comp name second) (comp name last))
-                flat-data)))
+  (mapv (juxt (comp str second) (comp str #(nth % 3)) (comp str last))
+        flat-data))
 
+(defn split-parents [rows]
+  ;; splice the parents to additional columns, from [:myattr [:parent1 :parent2 ...] :myentity] into [:myentity :myattr :parent1 :parent2 ...]
+
+  (for [r rows]
+    (concat [(get r 0) (get r 2)]
+            (split (get r 1) #";"))))
 
 (defn output-csv [flat-data out-csv-name]
   (->> flat-data
        make-csv-rows
+       split-parents
        unique-sorted
+       (concat [post-split-columns])
        (csv/write-csv *out*)
        (with-out-str)
        (spit out-csv-name)))
 
-(defn make-flat-csv-for-db [db root-entity-key out-csv-name]
-  (let [entity-tree (walk-db db root-entity-key)
-        flattened (mapcat #(partition 4 (flatten (flatten-db :projects %))) entity-tree)]
+(defn make-flat-csv-for-db [db root-entity-key out-csv-name root-parent-name]
+  (let [;; entity-tree (take 1 (walk-db db root-entity-key))
+        entity-tree (walk-db db root-entity-key)
+        flattened (mapcat #(partition (* 2 (count pre-split-columns))
+                                      (flatten (flatten-db root-parent-name %)))
+                          entity-tree)]
     (output-csv flattened out-csv-name)))
 
-
 (defn -main [& args]
-  (make-flat-csv-for-db (d/db main-conn) :thk.project/id "main-db.csv")
-  (make-flat-csv-for-db (d/db asset-conn) :asset/project "asset-db.csv"))
+  (make-flat-csv-for-db (d/db main-conn) :thk.project/id "main-db.csv" :projects)
+  (make-flat-csv-for-db (d/db asset-conn) :asset/project "asset-db.csv" :asset/project))
