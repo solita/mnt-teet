4d3410ca7f355489d6a5144b6d1f4654e85f8530
Erno Kuusela <erno!erno-laptop>
2021-06-28 15:12:58 +0300

    contracts search test now works, and also tests expanded card

--- ci/browser-tests/cypress/integration/contract_mm.spec.js
+++ ci/browser-tests/cypress/integration/contract_mm.spec.js
@@ -1,16 +1,19 @@
 // todo: use test data instead of using aws dev data
 context("Contracts MM", () => {
     before(() => {
-        cy.dummyLogin("benjamin")
-        cy.visit("#/contracts")
+        cy.dummyLogin("benjamin");
+        cy.visit("#/contracts");
+        cy.selectLanguage("#EN");
     })
 
     it("finds contracts with search and navigates there", () => {
         cy.get("button").contains("All contracts").click();
 
-        cy.get("button").contains("Show filters").click();
+        cy.get("input[id='contract-filter-input/:contract-name']").type("ÜKS");
 
-        cy.get("#contract-filter-input/contract-name").type("ÜKS");
+        cy.get("button").contains("Expand all").click();
+
+        cy.get('h6:contains("TA Project manager")').should('be.visible');
 
         cy.get("h4 + a[href^=\"#/contracts/\"").click();
 
