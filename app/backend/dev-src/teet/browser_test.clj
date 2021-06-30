c066fd2c03ced86cffd680e87bf362e8aae42ae9
Erno Kuusela <erno!erno-laptop>
2021-06-30 09:24:28 +0300

    import contracts test data in browser-test startup

--- app/backend/dev-src/teet/browser_test.clj
+++ app/backend/dev-src/teet/browser_test.clj
@@ -1,8 +1,23 @@
 (ns teet.browser-test
   (:require [teet.environment :as environment]
-            [teet.thk.thk-import-test :as tit]
+            [teet.thk.thk-import :as thk-import]
+            [teet.thk.thk-mapping :as thk-mapping]
             [teet.main :as main]
             [clojure.java.io :as io]))
+
+(defn import-contracts-test-data!
+  [conn]
+  (let [csv-data (.getBytes (slurp "test/teet/thk/thk-test-data.csv"))
+        contracts
+        (thk-import/parse-thk-export-csv
+         {:input (java.io.ByteArrayInputStream.
+                  csv-data)
+          :column-mapping thk-mapping/thk->teet-contract
+          :group-by-fn (fn [val]
+                         (select-keys val [:thk.contract/procurement-part-id
+                                           :thk.contract/procurement-id]))})
+        import-contract-result (thk-import/import-thk-contracts! conn "test://test-csv" contracts)]
+    import-contract-result))
 
 (defn -main [& args]
   (let [config-file (first args)]
@@ -10,5 +25,5 @@
     (user/make-mock-users!)
     (user/give-admin-permission [:user/id user/manager-uid])
     ;; fixme: following fails on a test fixture call
-    (tit/import-contracts-csv! (.getBytes (slurp "test/teet/thk/thk-test-data.csv")) (environment/datomic-connection))
+    (import-contracts-test-data! (environment/datomic-connection))
     (main/restart (io/file config-file))))
