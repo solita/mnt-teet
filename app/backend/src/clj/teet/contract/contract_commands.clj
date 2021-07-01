f488e3c19aec73134e971d728583dc5a660286c9
jkorpelin <joni.korpelin@solita.fi>
2021-07-01 10:58:01 +0300

    TEET-1771 removes check for contract authorization because it cannot work until you add people

--- app/backend/src/clj/teet/contract/contract_commands.clj
+++ app/backend/src/clj/teet/contract/contract_commands.clj
@@ -141,13 +141,7 @@
    :pre [(not
            (contract-db/is-company-contract-employee?
              db company-contract-eid
-             (get-in form-value [:company-contract-employee/user :db/id])))
-         ^{:error :contract-authorization-failed}
-         (authorization/authorized-for-action? {:db db
-                                                :user user
-                                                :action :contracts/add-existing-teet-users-to-contract
-                                                :company (get-in (du/entity db company-contract-eid)
-                                                                 [:company-contract/company :db/id])})]
+             (get-in form-value [:company-contract-employee/user :db/id])))]
    :transact [(merge
                 {:db/id "new-company-contract-employee"
                  :company-contract-employee/user (:company-contract-employee/user form-value)
