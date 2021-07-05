0011a0f31da5f9a1d20eb3ad575ad89a6165b780
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-07-05 07:43:45 +0300

    Add generated RR id to components as well, change mapping to simple function

--- app/backend/src/clj/teet/asset/asset_road_registry_import.clj
+++ app/backend/src/clj/teet/asset/asset_road_registry_import.clj
@@ -59,92 +59,76 @@
 
 (declare recursive-convert)
 
-(defprotocol Convert
-  (convert [this ctx wfs-feature here key]
-    "Run this conversion and return updated asset.
-`ctx` has context information like WFS client
-`wfs-feature` is the WFS feature being converted into asset
-`here` is the current map of the asset being built
-`key` is the key in the map this conversion was in"))
+(defn from-wfs [wfs-feature wfs-field convert-fn]
+  (some-> wfs-feature (get wfs-field) convert-fn))
 
-(defn from-wfs [wfs-field convert-fn]
-  (reify Convert
-    (convert [_ _ctx wfs-feature here key]
-      (if-let [val (some-> wfs-feature (get wfs-field) convert-fn)]
-        (assoc here key val)
-        here))))
+(defn from-point-to-perpendicular-line [ctx wfs-feature width-field]
+  (let [road-nr (some-> wfs-feature :ms:tee_number ->long)
+        carriageway (some-> wfs-feature :ms:soidutee_nr ->long)
+        meters (some-> wfs-feature :ms:km ->bigdec road-model/km->m)
+        point (some-> wfs-feature :geometry :coordinates)
+        width (some-> wfs-feature (get width-field) ->double)]
+    (when (and road-nr carriageway meters point width)
+      (point-to-perpendicular-line ctx point width road-nr carriageway meters))))
 
-(defn from-point-to-perpendicular-line [width-field]
-  (reify Convert
-    (convert [_ ctx wfs-feature here _key]
-      (let [road-nr (some-> wfs-feature :ms:tee_number ->long)
-            carriageway (some-> wfs-feature :ms:soidutee_nr ->long)
-            meters (some-> wfs-feature :ms:km ->bigdec road-model/km->m)
-            point (some-> wfs-feature :geometry :coordinates)
-            width (some-> wfs-feature (get width-field) ->double)]
-        (if (and road-nr carriageway meters point width)
-          (merge here
-                 (point-to-perpendicular-line ctx point width road-nr carriageway meters))
-          here)))))
+(defn culvert-mapping [ctx wfs-feature]
+  (let [status (from-wfs wfs-feature
+                             :ms:hinne_trhinne_xv
+                             #(if (= % "0")
+                                :item/abandoned
+                                :item/inuse))
+        id (str (:ms:oid wfs-feature))
+        culvertpipenumber (from-wfs wfs-feature :ms:truup ->long)
+        len (from-wfs wfs-feature :ms:trpik ->bigdec)
+        trotsad (from-wfs wfs-feature :ms:otsad_trotsad_xv ->long)
+        material (case (from-wfs wfs-feature :ms:trmater_xv ->long)
+                   1 :material/concrete
+                   2 :material/plastic
+                   3 :material/structuralsteel
+                   4 :material/sheetmetal
+                   ;;5 :material/stone (which stone to use?)
 
-(defn from-conditional
-  "Conditional item, returns nil if condition isn't truthy.
-  Do not use in map values as the whole map is removed."
-  [condition value]
-  (reify Convert
-    (convert [_ ctx wfs-feature here _]
-      (when (condition wfs-feature)
-        (recursive-convert ctx wfs-feature value)))))
+                   ;; For unmapped values, don't create any material
+                   nil)]
+    (merge
+     (from-point-to-perpendicular-line ctx wfs-feature :ms:trpik)
+     {:road-registry/id id
+      :asset/fclass :fclass/culvert
+      :culvert/culvertpipenumber culvertpipenumber
+      :location/road-nr (from-wfs wfs-feature :ms:tee_number ->long)
+      :location/carriageway (from-wfs wfs-feature :ms:soidutee_nr ->long)
+      :location/start-km (from-wfs wfs-feature :ms:km ->bigdec)
+      :common/status status
+      :asset/components
+      (vec
+       (concat
+        (for [i (range culvertpipenumber)
+              :let [id (str id "-pipe" i)]]
+          (merge
+           {:component/ctype :ctype/culvertpipe
+            :road-registry/id id
+            :culvertpipe/culvertpipediameter (from-wfs wfs-feature :ms:trava #(some-> % ->bigdec (* 1000M)))
+            :culvertpipe/culvertpipelenght len
+            :common/quantity len}
+           (when material
+             {:component/materials [{:material/type material
+                                     :road-registry/id (str id "-m0")}]})))
 
-(def ^:private culvert-mapping
-  {:asset/fclass :fclass/culvert
-   :culvert/culvertpipenumber (from-wfs :ms:truup ->long)
-   ::location (from-point-to-perpendicular-line :ms:trpik)
-   :location/road-nr (from-wfs :ms:tee_number ->long)
-   :location/carriageway (from-wfs :ms:soidutee_nr ->long)
-   :location/start-km (from-wfs :ms:km ->bigdec)
+        (when (= 1 trotsad)
+          (list
+           ;; If otsad_trotsad_xv = 1, create 2 culverthead components
+           {:component/ctype :ctype/culverthead
+            :road-registry/id (str id "-head" 1)}
+           {:component/ctype :ctype/culverthead
+            :road-registry/id (str id "-head" 2)}))
 
-   :common/status (from-wfs :ms:hinne_trhinne_xv
-                            #(if (= % "0")
-                               :item/abandoned
-                               :item/inuse))
-
-   :asset/components
-   [{:component/ctype :ctype/culvertpipe
-     ;; RR units in meters
-     :culvertpipe/culvertpipediameter (from-wfs :ms:trava #(some-> % ->bigdec (* 1000M)))
-     :culvertpipe/culvertpipelenght (from-wfs :ms:trpik ->bigdec)}
-
-    ;; If otsad_trotsad_xv = 1, create 2 culverthead components
-    (from-conditional #(= (:ms:otsad_trotsad_xv %) "1")
-                      {:component/ctype :ctype/culverthead})
-    (from-conditional #(= (:ms:otsad_trotsad_xv %) "1")
-                      {:component/ctype :ctype/culverthead})
-
-    ;; If otsad_trotsad_xv > 1, create 2 culvertprotection components
-    (from-conditional #(some-> % :ms:otsad_trotsad_xv ->long (> 1))
-                      {:component/ctype :ctype/culvertprotection})
-    (from-conditional #(some-> % :ms:otsad_trotsad_xv ->long (> 1))
-                      {:component/ctype :ctype/culvertprotection})]})
-
-(defn- recursive-convert [ctx wfs-feature here]
-  (cond
-    (satisfies? Convert here)
-    (convert here ctx wfs-feature here nil)
-
-    (map? here)
-    (reduce-kv (fn [m k v]
-                 (if (satisfies? Convert v)
-                   (convert v ctx wfs-feature m k)
-                   (assoc m k (recursive-convert ctx wfs-feature v))))
-               {} here)
-
-    (vector? here)
-    (into []
-          (keep (partial recursive-convert ctx wfs-feature))
-          here)
-
-    :else here))
+        (when (> trotsad 1)
+          (list
+           ;; If otsad_trotsad_xv > 1, create 2 culvertprotection components
+           {:component/ctype :ctype/culvertprotection
+            :road-registry/id (str id "-prot1")}
+           {:component/ctype :ctype/culvertprotection
+            :road-registry/id (str id "-prot2")}))))})))
 
 (defn import-road-registry-features! [conn ctx fclass wfs-type type-mapping [upper-left lower-right]]
   (let [objects (road-registry-objects ctx wfs-type (gml-area upper-left lower-right))]
@@ -153,12 +137,12 @@
       (d/transact
        conn
        {:tx-data
-        [(list 'teet.asset.asset-tx/import-assets
+        [{:db/id "datomic.tx"
+          :road-registry/import-url (:wfs-url ctx)}
+         (list 'teet.asset.asset-tx/import-assets
                (environment/config-value :asset :default-owner-code)
                fclass
-               (mapv #(merge
-                       {:asset/road-registry-oid (->long (:ms:oid %))}
-                       (recursive-convert ctx % type-mapping))
+               (mapv (partial type-mapping ctx)
                      objects))]}))))
 
 (def ^:private epsg-3301-bounds
