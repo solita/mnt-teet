ff8ef07f5536ede3aa5480cae2ab7ad2fb7b73ae
Erno Kuusela <erno!erno-laptop>
2021-07-05 14:25:35 +0300

    contract browser test improvements
     - do things in single test block because otherwise we need to relogin and repeat a lot
     - add testing for related project link
     - form save now actually works (error was due to need to relogin at each test)

--- ci/browser-tests/cypress/integration/contract_mm.ci.spec.js
+++ ci/browser-tests/cypress/integration/contract_mm.ci.spec.js
@@ -1,11 +1,11 @@
 context("Contracts MM", () => {
-    before(() => {
+    beforeEach(() => {
         cy.dummyLogin("benjamin");
         cy.visit("#/contracts");
         cy.selectLanguage("#EN");
     });
 
-    it("finds contracts with search and navigates there", () => {
+    it("finds contracts with search and navigates there + edits details + project link", () => {
         cy.get("[data-cy='search-shortcut-all-contracts']").click();
         // cy.get("[data-cy='toggle-filters-visibility']").click(); // uncomment if default is hidden, has been in flux
         cy.get("input[id='contract-filter-input/:contract-name']").type("TEPPO");
@@ -20,10 +20,7 @@ context("Contracts MM", () => {
         cy.get("h4 + a[href^=\"#/contracts/\"").click();
 
         cy.url().should('contain', 'contracts/');
-    });
 
-    it("edits contract", () => {
-        cy.selectLanguage("#EN");
         cy.get('h1 + button').contains("Edit").click();
         cy.formInput(":thk.contract/number", "123",
                      ":thk.contract/external-link", "https://example.com",
@@ -32,10 +29,9 @@ context("Contracts MM", () => {
                      ":thk.contract/deadline", "18.06.2021",
                      ":thk.contract/extended-deadline", "19.06.2021",
                      ":thk.contract/cost", "599");
-        cy.formSubmit()
-        // fixme: this passes even though video shows it's failing on a session
-        // timeout, seemingly connected to use of formInput
-        cy.get(".MuiSnackbar-root")
-
+        cy.formSubmit();
+        cy.get("[data-cy='contract-related-link']").click()
+        cy.url().should('contain', 'projects/');
     });
+
 })
