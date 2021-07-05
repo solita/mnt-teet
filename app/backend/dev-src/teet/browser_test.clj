e695694c948c45c174378a44b57ff4d43bf45c1a
Erno Kuusela <erno!erno-laptop>
2021-07-05 14:24:10 +0300

    use Benjamin, not Danny, for admin user in browser-test backend db init

--- app/backend/dev-src/teet/browser_test.clj
+++ app/backend/dev-src/teet/browser_test.clj
@@ -58,7 +58,7 @@
   (let [config-file (first args)]
     (environment/load-local-config! (io/file config-file))
     (user/make-mock-users!)
-    (user/give-admin-permission [:user/id user/manager-uid])
+    (user/give-admin-permission [:user/id user/boss-uid])
 
     (import-test-data!)
 
