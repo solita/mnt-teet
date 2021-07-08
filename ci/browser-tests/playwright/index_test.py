18060fb30f33084eca92eaf3fcd6a10401cd8731
Erno Kuusela <erno!erno-laptop>
2021-07-08 15:24:53 +0300

    GHA Playwright test: improve index test & gha config

--- ci/browser-tests/playwright/index_test.py
+++ ci/browser-tests/playwright/index_test.py
@@ -16,56 +16,88 @@ def run(playwright):
     # with page.expect_navigation(url="http://localhost:4000/#/admin/indexes"):
     with page.expect_navigation():
         page.click("text=Login as Benjamin Boss (Maanteeamet)arrow_forward")
-    # Click button:has-text("addLisan uue indeksi")
+    # Click button:has-text("person")
+    page.click("button:has-text(\"person\")")
+    # Click #EN
+    page.click("#EN")
+    # Click button:has-text("addAdd new index")
     # with page.expect_navigation(url="http://localhost:4000/#/admin/indexes?add-index-form=1"):
     with page.expect_navigation():
-        page.click("button:has-text(\"addLisan uue indeksi\")")
-    # Click text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input
-    page.click("text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input")
-    # Fill text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input
-    page.fill("text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input", "best index")
+        page.click("button:has-text(\"addAdd new index\")")
+    # Click text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input
+    page.click("text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input")
+    # Fill text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input
+    page.fill("text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input", "my lovely index")
     # Select 2
     page.select_option("select", "2")
-    # Click text=Kehtiv alates *calendar_today >> input
-    page.click("text=Kehtiv alates *calendar_today >> input")
-    # Fill text=Kehtiv alates *calendar_today >> input
-    page.fill("text=Kehtiv alates *calendar_today >> input", "01.01.2000")
-    # Click button:has-text("Salvestan")
-    page.click("button:has-text(\"Salvestan\")")
-    # Click text=My Most Excellent Index
-    page.click("text=My Most Excellent Index")
-    # assert page.url == "http://localhost:4000/#/admin/index/87960930223108"
-    # Click button:has-text("Muudan indeksit")
-    page.click("button:has-text(\"Muudan indeksit\")")
-    # Click button:has-text("Tühistan")
-    page.click("button:has-text(\"Tühistan\")")
-    # Click button:has-text("addLisan uue indeksi")
-    # with page.expect_navigation(url="http://localhost:4000/#/admin/indexes?add-index-form=1"):
-    with page.expect_navigation():
-        page.click("button:has-text(\"addLisan uue indeksi\")")
-    # Click text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input
-    page.click("text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input")
-    # Fill text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input
-    page.fill("text=Lisan uue indeksiIndeksi nimetus *Indeksi tüüp *- ValinTarbijahinna indexBituume >> input", "Frank")
+    # Click text=Valid from *calendar_today >> input
+    page.click("text=Valid from *calendar_today >> input")
+    # Fill text=Valid from *calendar_today >> input
+    page.fill("text=Valid from *calendar_today >> input", "12.12.2012")
+    # Click button:has-text("Save")
+    page.click("button:has-text(\"Save\")")
+    # Click button:has-text("addAdd new index")
+    page.click("button:has-text(\"addAdd new index\")")
+    # Click text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input
+    page.click("text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input")
+    # Fill text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input
+    page.fill("text=Add new indexIndex name *Index type *- Select valueConsumer Price IndexBitumen I >> input", "my fantastic index")
     # Select 1
     page.select_option("select", "1")
     # Click button:has-text("calendar_today")
     page.click("button:has-text(\"calendar_today\")")
-    # Click button:has-text("Täna")
-    page.click("button:has-text(\"Täna\")")
-    # Click text=TühistanSalvestan >> div
-    page.click("text=TühistanSalvestan >> div")
-    # Click text=Frank
-    page.click("text=Frank")
-    # assert page.url == "http://localhost:4000/#/admin/index/101155069756422"
-    # Click button:has-text("Muudan")
-    page.click("button:has-text(\"Muudan\")")
-    # Click text=Indeksi nimetusKustutanTühistanSalvestan >> input
-    page.click("text=Indeksi nimetusKustutanTühistanSalvestan >> input")
-    # Fill text=Indeksi nimetusKustutanTühistanSalvestan >> input
-    page.fill("text=Indeksi nimetusKustutanTühistanSalvestan >> input", "Frank II")
-    # Click button:has-text("Salvestan")
-    page.click("button:has-text(\"Salvestan\")")
+    # Click div[role="presentation"] button:has-text("Today")
+    page.click("div[role=\"presentation\"] button:has-text(\"Today\")")
+    # Click button:has-text("Save")
+    page.click("button:has-text(\"Save\")")
+    # Click text=my lovely index
+    page.click("text=my lovely index")
+    # assert page.url == "http://localhost:4000/#/admin/index/87960930223112"
+    # Click button:has-text("Edit")
+    page.click("button:has-text(\"Edit\")")
+    # Click text=Index nameDeleteCancelSave >> input
+    page.click("text=Index nameDeleteCancelSave >> input")
+    # Click text=Index nameDeleteCancelSave >> input
+    page.click("text=Index nameDeleteCancelSave >> input")
+    # Press Home
+    page.press("text=Index nameDeleteCancelSave >> input", "Home")
+    # Fill text=Index nameDeleteCancelSave >> input
+    page.fill("text=Index nameDeleteCancelSave >> input", "your lovely index")
+    # Click button:has-text("Save")
+    page.click("button:has-text(\"Save\")")
+    # Click button:has-text("Edit index values")
+    page.click("button:has-text(\"Edit index values\")")
+    # Click button:has-text("Save")
+    page.click("button:has-text(\"Save\")")
+    # Click text=my fantastic index
+    page.click("text=my fantastic index")
+    # assert page.url == "http://localhost:4000/#/admin/index/74766790689801"
+    # Click button:has-text("Edit index values")
+    page.click("button:has-text(\"Edit index values\")")
+    # Click button:has-text("Save")
+    page.click("button:has-text(\"Save\")")
+    # Click text=your lovely index
+    page.click("text=your lovely index")
+    # assert page.url == "http://localhost:4000/#/admin/index/87960930223112"
+    # Click button:has-text("Edit")
+    page.click("button:has-text(\"Edit\")")
+    # Click button:has-text("Delete")
+    page.click("button:has-text(\"Delete\")")
+    # Click #confirmation-confirm
+    # with page.expect_navigation(url="http://localhost:4000/#/admin/indexes"):
+    with page.expect_navigation():
+        page.click("#confirmation-confirm")
+    # Click text=my fantastic index
+    page.click("text=my fantastic index")
+    # assert page.url == "http://localhost:4000/#/admin/index/74766790689801"
+    # Click button:has-text("Edit")
+    page.click("button:has-text(\"Edit\")")
+    # Click button:has-text("Delete")
+    page.click("button:has-text(\"Delete\")")
+    # Click #confirmation-confirm
+    # with page.expect_navigation(url="http://localhost:4000/#/admin/indexes"):
+    with page.expect_navigation():
+        page.click("#confirmation-confirm")
     # ---------------------
     context.close()
     browser.close()
