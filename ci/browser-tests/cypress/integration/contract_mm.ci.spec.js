cafe56d46a60dc8f0560aa0edc5f5230648cb840
Aleksei Velizanin <aleksei.velizanin@solita.fi>
2021-07-16 10:51:46 +0300

    Add cypress test for related contracts

--- ci/browser-tests/cypress/integration/contract_mm.ci.spec.js
+++ ci/browser-tests/cypress/integration/contract_mm.ci.spec.js
@@ -5,17 +5,7 @@ context("Contracts MM", () => {
     });
 
     it("finds contracts with search and navigates there + edits details + project link", () => {
-        cy.visit("#/contracts");
-        cy.get("[data-cy='search-shortcut-all-contracts']").click({force: true});
-        // cy.get("[data-cy='toggle-filters-visibility']").click({force: true}); // uncomment if default is hidden, has been in flux
-        cy.get("input[id='contract-filter-input/:contract-name']").type("TEPPO");
-        cy.get("[data-cy='contract-card']").first().should('be.visible');
-        cy.get("[data-cy='expand-contracts']").click();
-        // cy.get('h6:contains("Region")').should('be.visible'); // the above click doesn't work in cypress, but does in human use, and this fails.
-
-        cy.get("h4 + a[href^=\"#/contracts/\"").last().click({force: true});
-
-        cy.url().should('contain', 'contracts/');
+        findContract("TEPPO");
 
         cy.get('h1 + button').contains("Edit").click({force: true});
         cy.formInput(":thk.contract/number", "123",
@@ -29,6 +19,27 @@ context("Contracts MM", () => {
         cy.get("[data-cy='snackbar-success']");
         cy.get("[data-cy='contract-related-link']").click({force: true})
         cy.url().should('contain', 'projects/');
-    });
+    })
+
+    it("find related contracts and shows them in a table on contract view", () => {
+        findContract("TEPPO");
+        cy.get('h4').contains("Related contracts").should('be.visible');
+        cy.get("[data-cy='project-related-contract-link']").click({force: true});
+        cy.url().should('contain', 'contracts');
+    })
+
+    function findContract(contractName) {
+        cy.visit("#/contracts");
+        cy.get("[data-cy='search-shortcut-all-contracts']").click({force: true});
+        // cy.get("[data-cy='toggle-filters-visibility']").click({force: true}); // uncomment if default is hidden, has been in flux
+        cy.get("input[id='contract-filter-input/:contract-name']").type(contractName);
+        cy.get("[data-cy='contract-card']").first().should('be.visible');
+        cy.get("[data-cy='expand-contracts']").click();
+        // cy.get('h6:contains("Region")').should('be.visible'); // the above click doesn't work in cypress, but does in human use, and this fails.
+
+        cy.get("h4 + a[href^=\"#/contracts/\"").last().click({force: true});
+
+        cy.url().should('contain', 'contracts/');
+    }
 
 })
