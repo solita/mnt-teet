2e68f9dea3ad872f002d1af08d30d83d62e3f559
Ville Outamaa <ville.outamaa@solita.fi>
2021-07-29 09:36:37 +0300

    TEET-1860 Improve Contract MM browser test stability

--- ci/browser-tests/cypress/integration/contract_mm.ci.spec.js
+++ ci/browser-tests/cypress/integration/contract_mm.ci.spec.js
@@ -31,12 +31,7 @@ context("Contracts MM", () => {
         cy.get("[data-cy='search-shortcut-all-contracts']").click({force: true});
         // cy.get("[data-cy='toggle-filters-visibility']").click({force: true}); // uncomment if default is hidden, has been in flux
         cy.get("input[id='contract-filter-input/:contract-name']").type(contractName);
-        cy.get("[data-cy='contract-card']").first().should('be.visible');
-        cy.get("[data-cy='expand-contracts']").click();
-        // cy.get('h6:contains("Region")').should('be.visible'); // the above click doesn't work in cypress, but does in human use, and this fails.
-
-        cy.get("h4 + a[href^=\"#/contracts/\"").last().click({force: true});
-
+        cy.get("[data-cy='contract-card'] h4").contains(contractName, {matchCase: false}).parent().find("a[href^=\"#/contracts/\"]").click();
         cy.url().should('contain', 'contracts/');
     }
 
