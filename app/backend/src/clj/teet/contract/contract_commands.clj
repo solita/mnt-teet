f1516aefa0f8c2816251a6f25db4330bcf910000
Ville Outamaa <ville.outamaa@solita.fi>
2021-07-30 13:47:34 +0300

    TEET-1232 Prevent existing and previously added persons to be added as new persons to contract

--- app/backend/src/clj/teet/contract/contract_commands.clj
+++ app/backend/src/clj/teet/contract/contract_commands.clj
@@ -174,6 +174,10 @@
               {:db/id company-contract-eid
                :company-contract/employees "new-company-contract-employee"}]})
 
+(defn- form-value->person-id-eid [form-value]
+  [:user/person-id (-> form-value
+                       :user/person-id
+                       user-model/normalize-person-id)])
 
 (defcommand :thk.contract/add-new-contract-employee
   {:doc "Add a new contract employee"
@@ -181,7 +185,14 @@
              company-contract-eid :company-contract-eid}
    :context {:keys [user db]}
    :project-id nil
-   :authorization {:contracts/contract-editing {}}}
+   :authorization {:contracts/contract-editing {}}
+   :pre [^{:error :existing-teet-user}
+         (not (user-db/user-has-logged-in? db (form-value->person-id-eid form-value)))
+
+         ^{:error :employee-already-added-to-contract}
+         (not (contract-db/is-company-contract-employee? db
+                                                         company-contract-eid
+                                                         (form-value->person-id-eid form-value)))]}
   (let [employee-fields (-> (select-keys form-value [:user/given-name :user/family-name
                                                      :user/email :user/phone-number]))
         user-person-id (user-model/normalize-person-id (:user/person-id form-value))
