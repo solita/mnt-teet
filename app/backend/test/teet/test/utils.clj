48a9fe7d33b8884901f72b901c7b3f28f1429640
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2021-08-12 12:27:39 +0300

    Use new role

--- app/backend/test/teet/test/utils.clj
+++ app/backend/test/teet/test/utils.clj
@@ -49,7 +49,7 @@
     :user/family-name "Boss"
     :user/email "benjamin.boss@example.com"
     :user/permissions [{:db/id "boss-permission"
-                        :permission/role :manager
+                        :permission/role :ta-project-manager
                         :permission/valid-from (java.util.Date.)}]
     }
 
